db94518b0b154270de4c03a50c3a967b169c6c94
Viacheslav Kabanovich <scabanovich@exadel.com>
2007-07-12 15:15:44 +0000

    EXIN-218 classes annotated
    git-svn-id: http://svn.jboss.org/repos/jbosstools/trunk@2405 a97e2381-89e5-4abb-bab3-167db6db766c

--- seam/plugins/org.jboss.tools.seam.ui/src/org/jboss/tools/seam/ui/views/actions/SeamActionProvider.java
+++ seam/plugins/org.jboss.tools.seam.ui/src/org/jboss/tools/seam/ui/views/actions/SeamActionProvider.java
@@ -1,3 +1,13 @@
+/*******************************************************************************
+ * Copyright (c) 2007 Exadel, Inc. and Red Hat, Inc.
+ * Distributed under license by Red Hat, Inc. All rights reserved.
+ * This program is made available under the terms of the
+ * Eclipse Public License v1.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors:
+ *     Exadel, Inc. and Red Hat, Inc. - initial API and implementation
+ ******************************************************************************/ 
 package org.jboss.tools.seam.ui.views.actions;
 
 import org.eclipse.jface.action.IMenuManager;
@@ -8,6 +18,10 @@ import org.eclipse.ui.navigator.CommonActionProvider;
 import org.jboss.tools.seam.core.IOpenableElement;
 import org.jboss.tools.seam.core.ISeamElement;
 
+/**
+ * Action provider for Seam Components view.
+ * @author Viacheslav Kabanovich
+ */
 public class SeamActionProvider extends CommonActionProvider {
 	
 	public SeamActionProvider() {}
