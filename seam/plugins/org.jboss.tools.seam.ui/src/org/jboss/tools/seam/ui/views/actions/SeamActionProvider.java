24682b739be566f8a92f392b6aaf5773aaeb230c
Viacheslav Kabanovich <scabanovich@exadel.com>
2007-07-17 12:32:44 +0000

    EXIN-218 - double click implemented
    git-svn-id: http://svn.jboss.org/repos/jbosstools/trunk@2461 a97e2381-89e5-4abb-bab3-167db6db766c

--- seam/plugins/org.jboss.tools.seam.ui/src/org/jboss/tools/seam/ui/views/actions/SeamActionProvider.java
+++ seam/plugins/org.jboss.tools.seam.ui/src/org/jboss/tools/seam/ui/views/actions/SeamActionProvider.java
@@ -13,8 +13,10 @@ package org.jboss.tools.seam.ui.views.actions;
 import org.eclipse.jface.action.IMenuManager;
 import org.eclipse.jface.viewers.ISelection;
 import org.eclipse.jface.viewers.IStructuredSelection;
+import org.eclipse.ui.IActionBars;
 import org.eclipse.ui.actions.ActionContext;
 import org.eclipse.ui.navigator.CommonActionProvider;
+import org.eclipse.ui.navigator.ICommonActionConstants;
 import org.jboss.tools.seam.core.IOpenableElement;
 import org.jboss.tools.seam.core.ISeamElement;
 
@@ -42,4 +44,20 @@ public class SeamActionProvider extends CommonActionProvider {
 		}
     }
 
+    public void fillActionBars(IActionBars actionBars) {
+		ActionContext c = getContext();
+		ISelection s = c.getSelection();
+		if(s == null || s.isEmpty() || !(s instanceof IStructuredSelection)) return;
+		Object e = ((IStructuredSelection)s).getFirstElement();
+		if(e instanceof ISeamElement) {
+			ISeamElement element = (ISeamElement)e;
+			
+			if(element instanceof IOpenableElement) {
+				SeamOpenAction action = new SeamOpenAction((IOpenableElement)element);
+				actionBars.setGlobalActionHandler(ICommonActionConstants.OPEN, action);
+			}
+
+		}
+		
+    }
 }
