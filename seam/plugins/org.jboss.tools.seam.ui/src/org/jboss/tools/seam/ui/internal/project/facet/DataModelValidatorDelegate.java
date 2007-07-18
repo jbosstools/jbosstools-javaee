a105609c08533d6ef94ae1f47ed0a79d21829ad5
Viacheslav Kabanovich <scabanovich@exadel.com>
2007-07-18 08:30:07 +0000

    Fixed Invalid thread access, that occured because of setting data model property in SeamFacetInstallDelegete
    git-svn-id: http://svn.jboss.org/repos/jbosstools/trunk@2490 a97e2381-89e5-4abb-bab3-167db6db766c

--- seam/plugins/org.jboss.tools.seam.ui/src/org/jboss/tools/seam/ui/internal/project/facet/DataModelValidatorDelegate.java
+++ seam/plugins/org.jboss.tools.seam.ui/src/org/jboss/tools/seam/ui/internal/project/facet/DataModelValidatorDelegate.java
@@ -14,6 +14,7 @@ import java.util.HashMap;
 import java.util.Map;
 
 import org.eclipse.jface.wizard.WizardPage;
+import org.eclipse.swt.widgets.Display;
 import org.eclipse.wst.common.frameworks.datamodel.DataModelEvent;
 import org.eclipse.wst.common.frameworks.datamodel.IDataModel;
 import org.eclipse.wst.common.frameworks.datamodel.IDataModelListener;
@@ -65,7 +66,13 @@ public class DataModelValidatorDelegate implements IDataModelListener {
 	 * 
 	 */
 	public void propertyChanged(DataModelEvent event) {
-		validateUntillError();
+		//When SeamFacetInstallDelegete sets model property,
+		//it can result in Invalid thread access.
+		Display.getDefault().asyncExec(new Runnable() {
+			public void run() {
+				validateUntillError();
+			}
+		});
 	}
 	
 	/**
