d52dab01b93c091bc93693807a0547f402206522
Viacheslav Kabanovich <scabanovich@exadel.com>
2007-08-02 07:49:36 +0000

    Reference to obsolete class FileAntImpl removed
    git-svn-id: http://svn.jboss.org/repos/jbosstools/trunk@2830 a97e2381-89e5-4abb-bab3-167db6db766c

--- jsf/plugins/org.jboss.tools.jsf/src/org/jboss/tools/jsf/model/handlers/refactoring/JSFRenamePathDiagramChange.java
+++ jsf/plugins/org.jboss.tools.jsf/src/org/jboss/tools/jsf/model/handlers/refactoring/JSFRenamePathDiagramChange.java
@@ -14,8 +14,8 @@ import org.eclipse.core.resources.IResource;
 import org.eclipse.core.runtime.*;
 import org.eclipse.ltk.core.refactoring.*;
 
-import org.jboss.tools.common.ant.model.FileAntImpl;
 import org.jboss.tools.common.model.XModelObject;
+import org.jboss.tools.common.model.filesystems.impl.FileAnyImpl;
 import org.jboss.tools.common.model.util.EclipseResourceUtil;
 import org.jboss.tools.jsf.messages.JSFUIMessages;
 
@@ -23,8 +23,8 @@ public class JSFRenamePathDiagramChange extends CompositeChange {
 	
 	static String getName(XModelObject config) {
 		String n = config.getAttributeValue("name");
-		if(config instanceof FileAntImpl) {
-			n = FileAntImpl.toFileName(config);
+		if(config instanceof FileAnyImpl) {
+			n = FileAnyImpl.toFileName(config);
 		}
 		IResource r = EclipseResourceUtil.getResource(config);
 		if(r != null && r.getParent() != null) {
