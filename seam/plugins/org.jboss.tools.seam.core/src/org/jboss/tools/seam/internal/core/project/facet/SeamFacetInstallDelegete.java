c63b874a8063117c375c31102019bff925d80512
Max Rydahl Andersen <max@hibernate.org>
2007-08-09 01:07:40 +0000

    trying to auto deploy ds.xml
    git-svn-id: http://svn.jboss.org/repos/jbosstools/trunk@2990 a97e2381-89e5-4abb-bab3-167db6db766c

--- seam/plugins/org.jboss.tools.seam.core/src/org/jboss/tools/seam/internal/core/project/facet/SeamFacetInstallDelegete.java
+++ seam/plugins/org.jboss.tools.seam.core/src/org/jboss/tools/seam/internal/core/project/facet/SeamFacetInstallDelegete.java
@@ -12,6 +12,7 @@ package org.jboss.tools.seam.internal.core.project.facet;
 
 import java.io.File;
 import java.io.IOException;
+import java.util.Iterator;
 
 import org.apache.tools.ant.types.FilterSet;
 import org.apache.tools.ant.types.FilterSetCollection;
@@ -20,6 +21,7 @@ import org.eclipse.core.resources.IProject;
 import org.eclipse.core.resources.IResource;
 import org.eclipse.core.resources.ResourcesPlugin;
 import org.eclipse.core.runtime.CoreException;
+import org.eclipse.core.runtime.IPath;
 import org.eclipse.core.runtime.IProgressMonitor;
 import org.eclipse.core.runtime.IStatus;
 import org.eclipse.core.runtime.Path;
@@ -32,7 +34,16 @@ import org.eclipse.wst.common.componentcore.resources.IVirtualComponent;
 import org.eclipse.wst.common.componentcore.resources.IVirtualFolder;
 import org.eclipse.wst.common.frameworks.datamodel.IDataModel;
 import org.eclipse.wst.common.project.facet.core.IDelegate;
+import org.eclipse.wst.common.project.facet.core.IFacetedProject;
 import org.eclipse.wst.common.project.facet.core.IProjectFacetVersion;
+import org.eclipse.wst.common.project.facet.core.ProjectFacetsManager;
+import org.eclipse.wst.server.core.IModule;
+import org.eclipse.wst.server.core.IRuntime;
+import org.eclipse.wst.server.core.IServer;
+import org.eclipse.wst.server.core.ServerCore;
+import org.eclipse.wst.server.core.model.ServerBehaviourDelegate;
+import org.jboss.ide.eclipse.as.core.server.stripped.DeployableServerBehavior;
+import org.jboss.ide.eclipse.as.core.singledeployable.SingleDeployableFactory;
 import org.jboss.tools.common.model.util.EclipseResourceUtil;
 import org.jboss.tools.seam.core.ISeamProject;
 import org.jboss.tools.seam.core.SeamCorePlugin;
@@ -172,7 +183,7 @@ public class SeamFacetInstallDelegete extends Object implements IDelegate {
 		WebArtifactEdit edit = WebArtifactEdit.getWebArtifactEditForRead(project);
 		IVirtualComponent com = ComponentCore.createComponent(project);
 		IVirtualFolder webRootFolder = com.getRootFolder().getFolder(new Path("/"));
-		IVirtualFolder srcRootFolder = com.getRootFolder().getFolder(new Path("/WEB-INF/classes"));
+		final IVirtualFolder srcRootFolder = com.getRootFolder().getFolder(new Path("/WEB-INF/classes"));
 		IContainer folder = webRootFolder.getUnderlyingFolder();
 		edit.dispose();
 		model.setProperty(ISeamFacetDataModelProperties.SEAM_PROJECT_NAME, project.getName());
@@ -296,6 +307,68 @@ public class SeamFacetInstallDelegete extends Object implements IDelegate {
 			if(model.getProperty(ISeamFacetDataModelProperties.JDBC_DRIVER_JAR_PATH)!=null)
 				AntCopyUtils.copyFiles((String[])model.getProperty(ISeamFacetDataModelProperties.JDBC_DRIVER_JAR_PATH), webLibFolder);
 
+			Job create = new Job("Deploying datasource to server") {
+				@Override
+				protected IStatus run(IProgressMonitor monitor) {
+
+					IFacetedProject facetedProject;
+					try {
+						facetedProject = ProjectFacetsManager.create(project);
+					} catch (CoreException e) {
+						return new Status(Status.WARNING, SeamCorePlugin.PLUGIN_ID, "No server selected to deploy datasource to");
+					}
+					org.eclipse.wst.common.project.facet.core.runtime.IRuntime primaryRuntime = facetedProject.getPrimaryRuntime();
+					IServer s = null;
+					IServer[] servers = ServerCore.getServers();
+					for (IServer server : servers) {
+						String primaryName = primaryRuntime.getName();
+						IRuntime runtime = server.getRuntime();
+						if(runtime!=null) {
+							String serverName = runtime.getName();
+							if(primaryName.equals(serverName)) {
+								s = server;
+							}
+						}
+					}
+					
+					
+					
+					 
+			         
+					
+					if(s==null) {
+						return new Status(Status.WARNING, SeamCorePlugin.PLUGIN_ID, "No server selected to deploy datasource to");							
+					} 
+					
+			        // convert it to a DeployableServer instance
+                    DeployableServerBehavior deployer =  (DeployableServerBehavior) s.loadAdapter(DeployableServerBehavior.class, null);
+
+                    // if its not null, the adaptation worked.
+                    if( deployer == null ) {
+                    	return new Status(Status.WARNING, SeamCorePlugin.PLUGIN_ID, "Server did not support deploy of datasource.");
+                    }
+ 
+			        IContainer underlyingFolder = srcRootFolder.getUnderlyingFolder();
+					
+					IPath projectPath = new Path("/"+underlyingFolder.getProject().getName());
+					IPath projectRelativePath = underlyingFolder.getProjectRelativePath();
+					
+					IPath append = projectPath.append(projectRelativePath).append(project.getName()+"-ds.xml");
+					
+					if(SingleDeployableFactory.makeDeployable(append)) {
+					
+						IModule module = SingleDeployableFactory.findModule(append);
+						
+				         // custom API to deploy / publish only one module.
+						IStatus t = deployer.publishOneModule(IServer.PUBLISH_FULL, new IModule[] { module}, ServerBehaviourDelegate.ADDED, monitor);
+				        return 	t;				
+					} else {
+						return new Status(Status.WARNING, SeamCorePlugin.PLUGIN_ID, "Could not deploy datasource " + append);
+					}
+				}
+			};
+			create.setRule(ResourcesPlugin.getWorkspace().getRoot());
+			create.schedule();
 			
 		} else {
 			
