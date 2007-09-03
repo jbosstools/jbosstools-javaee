6c36fa97ab4aec6c6580e549fa29479d10151e45
Daniel Azarov <daniel@exadel.com>
2007-09-03 16:33:58 +0000

    http://jira.jboss.com/jira/browse/EXIN-337
    git-svn-id: http://svn.jboss.org/repos/jbosstools/trunk@3428 a97e2381-89e5-4abb-bab3-167db6db766c

--- seam/tests/org.jboss.tools.seam.ui.test/src/org/jboss/tools/seam/ui/test/view/SeamComponentsViewTest.java
+++ seam/tests/org.jboss.tools.seam.ui.test/src/org/jboss/tools/seam/ui/test/view/SeamComponentsViewTest.java
@@ -11,20 +11,30 @@
 ******************************************************************************/ 
 package org.jboss.tools.seam.ui.test.view;
 
+import java.util.Collection;
+import java.util.Iterator;
+import java.util.List;
+
 import junit.framework.TestCase;
 
+import org.eclipse.core.resources.IFile;
 import org.eclipse.core.resources.IProject;
 import org.eclipse.core.resources.IResource;
 import org.eclipse.core.resources.ResourcesPlugin;
 import org.eclipse.core.runtime.CoreException;
 import org.eclipse.core.runtime.NullProgressMonitor;
 import org.eclipse.jface.viewers.IStructuredContentProvider;
+import org.eclipse.swt.widgets.Tree;
+import org.eclipse.swt.widgets.TreeItem;
 import org.eclipse.ui.IViewPart;
 import org.eclipse.ui.IWorkbenchPage;
 import org.eclipse.ui.navigator.CommonNavigator;
 import org.jboss.tools.common.model.XJob;
 import org.jboss.tools.common.test.util.TestProjectProvider;
 import org.jboss.tools.jst.web.ui.RedHat4WebPerspectiveFactory;
+import org.jboss.tools.seam.core.ISeamComponent;
+import org.jboss.tools.seam.core.ISeamPackage;
+import org.jboss.tools.seam.core.ISeamScope;
 import org.jboss.tools.seam.ui.ISeamUiConstants;
 import org.jboss.tools.test.util.JUnitUtils;
 import org.jboss.tools.test.util.WorkbenchUtils;
@@ -44,7 +54,41 @@ public class SeamComponentsViewTest extends TestCase {
 				RedHat4WebPerspectiveFactory.PERSPECTIVE_ID,
 				WorkbenchUtils.getWorkbench().getActiveWorkbenchWindow());
 		}
-
+	
+	public void testComponentViewTree(){
+		System.out.println("testAComponentViewTree!");
+		TestProjectProvider provider=null;
+		try {
+			provider = new TestProjectProvider("org.jboss.tools.seam.ui.test", null, "TestComponentView", true);
+		} catch (Exception e1) {
+			JUnitUtils.fail("Cannot create Project Provider", e1);
+		} 
+		IProject project = provider.getProject();
+		try {
+			project.refreshLocal(IResource.DEPTH_INFINITE, new NullProgressMonitor());
+		} catch (Exception e) {
+			JUnitUtils.fail("Cannot refresh created test Project", e);
+		}
+		try {
+			XJob.waitForJob();
+		} catch (InterruptedException e) {
+			JUnitUtils.fail(e.getMessage(),e);
+		}
+		System.out.println("Refresh is DONE!");
+		
+		IFile f = project.getFile("WebContent/WEB-INF/components.xml");
+		assertTrue("Cannot find components.xml in test project", f != null && f.exists());
+		
+		CommonNavigator navigator = getSeamComponentsView();
+		navigator.getCommonViewer().expandAll();
+		Tree tree = navigator.getCommonViewer().getTree();
+		
+		System.out.println("tree.getItemCount() - "+tree.getItemCount());
+		for(int i=0;i<tree.getItemCount();i++){
+			showTreeItem(tree.getItem(i),0);
+		}
+	}
+	
 	/**
 	 * 
 	 */
@@ -57,7 +101,7 @@ public class SeamComponentsViewTest extends TestCase {
 	public void testCreatedProjectIsShownOnTree() {
 		TestProjectProvider provider=null;
 		try {
-			provider = new TestProjectProvider("org.jboss.tools.seam.core.test", null, "TestScanner", true);
+			provider = new TestProjectProvider("org.jboss.tools.seam.ui.test", null, "TestComponentView", true);
 		} catch (Exception e1) {
 			JUnitUtils.fail("Cannot create Project Provider", e1);
 		} 
@@ -80,7 +124,7 @@ public class SeamComponentsViewTest extends TestCase {
 	
 	public void testThatDeletedProjectIsDisappearedFromTree() {
 		try {
-			ResourcesPlugin.getWorkspace().getRoot().findMember("TestScanner").delete(true, new NullProgressMonitor());
+			ResourcesPlugin.getWorkspace().getRoot().findMember("TestComponentView").delete(true, new NullProgressMonitor());
 		} catch (CoreException e) {
 			JUnitUtils.fail(e.getMessage(),e);
 		}	
@@ -93,5 +137,48 @@ public class SeamComponentsViewTest extends TestCase {
 		IWorkbenchPage page  = WorkbenchUtils.getWorkbenchActivePage();
 		CommonNavigator part = (CommonNavigator)page.findView(ISeamUiConstants.SEAM_COMPONENTS_VIEW_ID);
 		return part;
+	}
+	
+	
+	
+	private void showTreeItem(TreeItem item, int level){
+		for(int i=0;i<level;i++)
+			System.out.print("-");
+		
+		System.out.print(item.getText());
+		System.out.println(" "+item.getData());
+		if(item.getData() instanceof ISeamScope){
+			ISeamScope scope = (ISeamScope)item.getData();
+			Collection packages = scope.getPackages();
+			System.out.println(" packages - "+packages.size());
+			Iterator iter = packages.iterator();
+			while(iter.hasNext())
+				showSeamPackage((ISeamPackage)iter.next(), level++);
+			
+			
+			List components = scope.getComponents();
+			for(int i=0;i<components.size();i++)
+				showSeamComponent((ISeamComponent)components.get(i), level++);
+		}
+		
+		for(int i=0;i<item.getItemCount();i++){
+			showTreeItem(item.getItem(i),level++);
+		}
+	}
+	
+	private void showSeamComponent(ISeamComponent component, int level){
+		for(int i=0;i<level;i++)
+			System.out.print("-");
+		
+		System.out.println("Component - "+component.getName());
+		
+	}
+
+	private void showSeamPackage(ISeamPackage seamPackage, int level){
+		for(int i=0;i<level;i++)
+			System.out.print("-");
+		
+		System.out.println("Package - "+seamPackage.getName());
+		
 	}
 }
