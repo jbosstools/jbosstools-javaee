f7c6e53b92d26d5bbfc9c87625ff2f84e7bf0c6f
Evgeny Zheleznyakov <ezheleznyakov@exadel.com>
2007-10-05 16:19:48 +0000

    http://jira.jboss.com/jira/browse/JBIDE-847 Create templates for <rich:panelMenu/>, <rich:panelMenuGroup/> and <rich:panelMenuItem> components.
    git-svn-id: http://svn.jboss.org/repos/jbosstools/trunk@4038 a97e2381-89e5-4abb-bab3-167db6db766c

---
+++ jsf/plugins/org.jboss.tools.jsf.vpe.richfaces/src/org/jboss/tools/jsf/vpe/richfaces/template/RichFacesPanelMenuItem.java
@@ -0,0 +1,16 @@
+package org.jboss.tools.jsf.vpe.richfaces.template;
+
+import org.jboss.tools.vpe.editor.context.VpePageContext;
+import org.jboss.tools.vpe.editor.template.VpeAbstractTemplate;
+import org.jboss.tools.vpe.editor.template.VpeCreationData;
+import org.w3c.dom.Document;
+import org.w3c.dom.Node;
+
+public class RichFacesPanelMenuItem extends VpeAbstractTemplate {
+
+	public VpeCreationData create(VpePageContext pageContext, Node sourceNode,
+			Document visualDocument) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+}
