d0952f55ed00ddc6abeb53687373b0aaa360b62e
Viacheslav Kabanovich <scabanovich@exadel.com>
2007-10-05 15:15:44 +0000

    JBIDE-1029
    git-svn-id: http://svn.jboss.org/repos/jbosstools/trunk@4035 a97e2381-89e5-4abb-bab3-167db6db766c

--- seam/plugins/org.jboss.tools.seam.xml/src/org/jboss/tools/seam/xml/components/model/SeamComponentConstants.java
+++ seam/plugins/org.jboss.tools.seam.xml/src/org/jboss/tools/seam/xml/components/model/SeamComponentConstants.java
@@ -14,14 +14,16 @@ public interface SeamComponentConstants {
 	public String PUBLIC_ID_11 = "-//JBoss/Seam Component Configuration DTD 1.1//EN"; //$NON-NLS-1$
 	public String SYSTEM_ID_11 = "http://jboss.com/products/seam/components-1.1.dtd"; //$NON-NLS-1$
 	
+	public String SUFF_20 = "20"; //$NON-NLS-1$
 	public String ENT_SEAM_COMPONENTS = "FileSeamComponents"; //$NON-NLS-1$
 	public String ENT_SEAM_COMPONENTS_11 = ENT_SEAM_COMPONENTS + "11"; //$NON-NLS-1$
 	public String ENT_SEAM_COMPONENTS_12 = ENT_SEAM_COMPONENTS + "12"; //$NON-NLS-1$
-	public String ENT_SEAM_COMPONENT_12 = "FileSeamComponent" + "12"; //$NON-NLS-1$ //$NON-NLS-2$
-	public String ENT_SEAM_COMPONENTS_20 = ENT_SEAM_COMPONENTS + "20"; //$NON-NLS-1$
-	public String ENT_SEAM_COMPONENT_20 = "FileSeamComponent" + "20"; //$NON-NLS-1$ //$NON-NLS-2$
+	public String ENT_SEAM_COMPONENT_12 = "FileSeamComponent" + "12"; //$NON-NLS-1$  //$NON-NLS-2$
+	public String ENT_SEAM_COMPONENTS_20 = ENT_SEAM_COMPONENTS + SUFF_20;
+	public String ENT_SEAM_COMPONENT_20 = "FileSeamComponent" + SUFF_20;  //$NON-NLS-1$
 	public String ENT_SEAM_COMPONENT = "SeamComponent"; //$NON-NLS-1$
-	public String ENT_SEAM_FACTORY = "SeamFactory"; //$NON-NLS-1$
+	public String ENT_SEAM_FACTORY = "SeamFactory";  //$NON-NLS-1$
+	public String ENT_SEAM_FACTORY_20 = ENT_SEAM_FACTORY + SUFF_20;
 	
 	public String ENT_SEAM_PROPERTY = "SeamProperty"; //$NON-NLS-1$
 	public String ENT_SEAM_PROPERTY_LIST = "SeamPropertyList"; //$NON-NLS-1$
@@ -32,11 +34,16 @@ public interface SeamComponentConstants {
 	
 	public String ENT_SEAM_EVENT = "SeamEvent"; //$NON-NLS-1$
 	public String ENT_SEAM_ACTION = "SeamAction"; //$NON-NLS-1$
-	
+	public String ENT_SEAM_EVENT_20 = ENT_SEAM_EVENT + SUFF_20;
+	public String ENT_SEAM_ACTION_20 = ENT_SEAM_ACTION + SUFF_20;
+
+	public String ENT_SEAM_IMPORT = "SeamImport"; //$NON-NLS-1$
+
 	public String ATTR_NAME = "name"; //$NON-NLS-1$
 	public String ATTR_KEY = "key"; //$NON-NLS-1$
 	public String ATTR_VALUE = "value"; //$NON-NLS-1$
 	public String ATTR_TYPE = "type"; //$NON-NLS-1$
 	public String ATTR_EXPR = "expression"; //$NON-NLS-1$
+	public String ATTR_EXEC = "execute"; //$NON-NLS-1$
 
 }
