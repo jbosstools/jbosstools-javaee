cbab8a176224ced9640b73f752dfd04be464302e
Viacheslav Kabanovich <scabanovich@exadel.com>
2007-10-18 10:27:46 +0000

    JBIDE-800
    git-svn-id: http://svn.jboss.org/repos/jbosstools/trunk@4318 a97e2381-89e5-4abb-bab3-167db6db766c

--- jsf/plugins/org.jboss.tools.jsf.ui/src/org/jboss/tools/jsf/ui/action/JSFMultiPageContributor.java
+++ jsf/plugins/org.jboss.tools.jsf.ui/src/org/jboss/tools/jsf/ui/action/JSFMultiPageContributor.java
@@ -132,11 +132,10 @@ public class JSFMultiPageContributor extends AbstractMultiPageContributor {
 		updateStatus();
 	}
 
-	public static void registerKeyBindings(IHandlerService handler, String[] actions, ActionRegistry registry) {
+	public void registerKeyBindings(IHandlerService handler, String[] actions, ActionRegistry registry) {
 		for (int i = 0; i < actions.length; i++) {
 			IAction action = registry.getAction(actions[i]);
-			if(action == null) continue;
-			handler.activateHandler(actions[i], new ActionHandler(action));
+			registerKeyBinding(handler, actions[i], action);
 		}
 	}
 
