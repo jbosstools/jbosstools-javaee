f972fa1cf4c05eedb0239eccd7827fcf7aad6b7b
Daniel Azarov <daniel@exadel.com>
2007-10-22 11:18:04 +0000

    http://jira.jboss.com/jira/browse/JBIDE-1121
    git-svn-id: http://svn.jboss.org/repos/jbosstools/trunk@4403 a97e2381-89e5-4abb-bab3-167db6db766c

--- seam/plugins/org.jboss.tools.seam.ui/src/org/jboss/tools/seam/ui/SeamPerspectiveFactory.java
+++ seam/plugins/org.jboss.tools.seam.ui/src/org/jboss/tools/seam/ui/SeamPerspectiveFactory.java
@@ -1,5 +1,6 @@
 package org.jboss.tools.seam.ui;
 
+import org.eclipse.debug.ui.IDebugUIConstants;
 import org.eclipse.jdt.ui.JavaUI;
 import org.eclipse.ui.IFolderLayout;
 import org.eclipse.ui.IPageLayout;
@@ -40,6 +41,7 @@ public class SeamPerspectiveFactory implements IPerspectiveFactory {
 		IFolderLayout rightBottom = layout.createFolder("rightBottom", IPageLayout.BOTTOM, (float)0.64, "right"); //$NON-NLS-1$ //$NON-NLS-2$
 		rightBottom.addView(IPageLayout.ID_OUTLINE);
 
+		layout.addActionSet(IDebugUIConstants.LAUNCH_ACTION_SET);
 		layout.addActionSet(JavaUI.ID_ACTION_SET);
 		layout.addActionSet(JavaUI.ID_ELEMENT_CREATION_ACTION_SET);
 		layout.addActionSet(IPageLayout.ID_NAVIGATE_ACTION_SET);
