e161f8dd16b5ceab5a27eb94a24aa8969bbc5b99
Marshall Culpepper <mculpepper@jboss.com>
2007-11-30 17:32:11 +0000

    retagging seam for GA
    git-svn-id: http://svn.jboss.org/repos/jbosstools/tags/jbosstools-2.0.0.GA@5177 a97e2381-89e5-4abb-bab3-167db6db766c

--- seam/plugins/org.jboss.ide.seam.gen/src/org/jboss/ide/seam/gen/actions/RestartAction.java
+++
@@ -1,9 +0,0 @@
-package org.jboss.ide.seam.gen.actions;
-
-public class RestartAction extends SeamGenAction {
-
-	protected String getTarget() {
-		return "restart";
-	}
-
-}
--- seam/plugins/org.jboss.tools.seam.core/src/org/jboss/tools/seam/core/ISeamContextShortVariable.java
+++
@@ -1,5 +0,0 @@
-package org.jboss.tools.seam.core;
-
-public interface ISeamContextShortVariable extends ISeamContextVariable {
-	public ISeamContextVariable getOriginal();
-}
