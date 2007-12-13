8bb7e2153285fc757772280df7d13e3e52bdc034
Dzmitry Sakovich <dsakovich@exadel.com>
2007-12-13 17:14:17 +0000

    http://jira.jboss.com/jira/browse/JBIDE-1491
    git-svn-id: http://svn.jboss.org/repos/jbosstools/trunk@5290 a97e2381-89e5-4abb-bab3-167db6db766c

---
+++ jsf/tests/org.jboss.tools.jsf.vpe.seam.test/src/org/jboss/tools/jsf/vpe/seam/test/SeamAllTests.java
@@ -0,0 +1,32 @@
+/*******************************************************************************
+ * Copyright (c) 2007 Exadel, Inc. and Red Hat, Inc.
+ * Distributed under license by Red Hat, Inc. All rights reserved.
+ * This program is made available under the terms of the
+ * Eclipse Public License v1.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors:
+ *     Exadel, Inc. and Red Hat, Inc. - initial API and implementation
+ ******************************************************************************/
+package org.jboss.tools.jsf.vpe.seam.test;
+
+import junit.framework.Test;
+import junit.framework.TestSuite;
+
+/**
+ * Class for testing all Seam components
+ * 
+ * @author dsakovich@exadel.com
+ * 
+ */
+
+public class SeamAllTests {
+    public static Test suite() {
+	TestSuite suite = new TestSuite("Tests for Vpe Seam components"); // $NON-NLS-1$
+	// $JUnit-BEGIN$
+	suite.addTestSuite(SeamComponentTest.class);
+	// $JUnit-END$
+	return suite;
+
+    }
+}
