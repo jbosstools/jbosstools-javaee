e661f88be0aa835ad016d18d68f2f4911c39b22d
Victor Rubezhny <ruby@exadel.com>
2007-12-26 14:51:28 +0000

    http://jira.jboss.org/jira/browse/JBIDE-1437 Ctrl+Click/F3 navigation isn't supported for s:button@view and s:link@view attributes
    Issue is fixed
    
    git-svn-id: http://svn.jboss.org/repos/jbosstools/trunk@5449 a97e2381-89e5-4abb-bab3-167db6db766c

---
+++ seam/plugins/org.jboss.tools.seam.text.ext/src/org/jboss/tools/seam/text/ext/hyperlink/SeamViewHyperlink.java
@@ -0,0 +1,63 @@
+/*******************************************************************************
+ * Copyright (c) 2007 Red Hat, Inc.
+ * Distributed under license by Red Hat, Inc. All rights reserved.
+ * This program is made available under the terms of the
+ * Eclipse Public License v1.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributor:
+ *     Red Hat, Inc. - initial API and implementation
+ ******************************************************************************/
+
+package org.jboss.tools.seam.text.ext.hyperlink;
+
+import java.util.Iterator;
+import java.util.List;
+
+import org.eclipse.core.resources.IFile;
+import org.eclipse.core.resources.IProject;
+import org.jboss.tools.common.model.XModel;
+import org.jboss.tools.common.model.project.IModelNature;
+import org.jboss.tools.common.model.util.EclipseResourceUtil;
+import org.jboss.tools.common.text.ext.hyperlink.LinkHyperlink;
+import org.jboss.tools.jst.web.project.list.WebPromptingProvider;
+import org.jboss.tools.seam.text.ext.SeamExtPlugin;
+
+public class SeamViewHyperlink extends LinkHyperlink {
+	
+	protected String updateFilenameForModel(String filename, IProject project) {
+		if (filename == null ||
+				!filename.trim().startsWith("/"))
+			return null;
+		WebPromptingProvider provider = WebPromptingProvider.getInstance();
+
+		IModelNature n = EclipseResourceUtil.getModelNature(project);
+		XModel xModel = n == null ? null : n.getModel();
+		
+		if (xModel != null) {
+			List list = provider.getList(xModel, WebPromptingProvider.JSF_GET_PATH, filename, null);
+			if (list != null && list.size() > 0) {
+				for (Iterator i = list.iterator(); i.hasNext();) {
+					Object o = i.next();
+					if (o instanceof String) {
+						return (String)o;
+					}
+				}
+			}
+		}
+		return filename;
+	}
+	
+	protected IFile getFileFromProject(String fileName) {
+		IFile documentFile = getFile();
+		
+		try {	
+			IProject project = documentFile.getProject();
+			return super.getFileFromProject(updateFilenameForModel(fileName, project));
+		} catch (Exception x) {
+			SeamExtPlugin.getPluginLog().logError(x);
+			return null;
+		}
+	}
+	
+}
