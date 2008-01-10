e84c1ded877a253ba036ea3a3c4a4c9174242bb5
Dzmitry Sakovich <dsakovich@exadel.com>
2008-01-10 20:59:39 +0000

    http://jira.jboss.com/jira/browse/JBIDE-1504
    git-svn-id: http://svn.jboss.org/repos/jbosstools/trunk@5599 a97e2381-89e5-4abb-bab3-167db6db766c

--- jsf/tests/org.jboss.tools.jsf.vpe.facelets.test/src/org/jboss/tools/jsf/vpe/facelets/test/FaceletsComponentTest.java
+++ jsf/tests/org.jboss.tools.jsf.vpe.facelets.test/src/org/jboss/tools/jsf/vpe/facelets/test/FaceletsComponentTest.java
@@ -23,9 +23,10 @@ import org.jboss.tools.vpe.ui.test.VpeTest;
 import org.mozilla.interfaces.nsIDOMDocument;
 import org.mozilla.interfaces.nsIDOMElement;
 import org.mozilla.interfaces.nsIDOMNode;
+import org.mozilla.interfaces.nsIDOMText;
 
 /**
- * Class for testing all Seam components
+ * Class for testing all Facelets components
  * 
  * @author dsakovich@exadel.com
  * 
@@ -37,20 +38,251 @@ public class FaceletsComponentTest extends VpeTest {
 
     public FaceletsComponentTest(String name) {
 	super(name);
+	setCheckWarning(false);
     }
 
+    /**
+     * Test for ui:debug
+     * 
+     * @throws Throwable
+     */
     public void testDebug() throws Throwable {
-	performTestForVpeComponent((IFile) TestUtil.getComponentPath("components/debug.xhtml",IMPORT_PROJECT_NAME)); // $NON-NLS-1$
+	performTestForVpeComponent((IFile) TestUtil.getComponentPath(
+		"components/debug.xhtml", IMPORT_PROJECT_NAME)); // $NON-NLS-1$
     }
 
+    /**
+     * Test for ui:define
+     * 
+     * @throws Throwable
+     */
     public void testDefine() throws Throwable {
+
+	nsIDOMElement element = performTestForFaceletComponent("components/define.xhtml");
+	nsIDOMNode node = (nsIDOMNode) element
+		.queryInterface(nsIDOMNode.NS_IDOMNODE_IID);
+
+	List<nsIDOMNode> elements = new ArrayList<nsIDOMNode>();
+
+	// find "span" elements
+	TestUtil.findElementsByName(node, elements, HTML.TAG_SPAN);
+
+	assertEquals(2, elements.size());
+	nsIDOMElement elementSpan0 = (nsIDOMElement) elements.get(0)
+		.queryInterface(nsIDOMElement.NS_IDOMELEMENT_IID);
+	nsIDOMElement elementSpan1 = (nsIDOMElement) elements.get(1)
+		.queryInterface(nsIDOMElement.NS_IDOMELEMENT_IID);
+
+	nsIDOMText text0 = (nsIDOMText) elementSpan0.getFirstChild()
+		.queryInterface(nsIDOMText.NS_IDOMTEXT_IID);
+	nsIDOMText text1 = (nsIDOMText) elementSpan1.getFirstChild()
+		.queryInterface(nsIDOMText.NS_IDOMTEXT_IID);
+
+	assertEquals(text0.getNodeValue(), "Greeting Page");
+	assertEquals(text1.getNodeValue(), "#{person.name}!");
+
+	if (getException() != null) {
+	    throw getException();
+	}
+    }
+
+    /**
+     * Test for ui:composition
+     * 
+     * @throws Throwable
+     */
+    public void testComposition() throws Throwable {
+
+	// check absolute path
+	nsIDOMElement element = performTestForFaceletComponent("components/composition_absolute.xhtml");
+	nsIDOMNode node = (nsIDOMNode) element
+		.queryInterface(nsIDOMNode.NS_IDOMNODE_IID);
+
+	List<nsIDOMNode> elements = new ArrayList<nsIDOMNode>();
+
+	// find "table" elements
+	TestUtil.findElementsByName(node, elements, HTML.TAG_TABLE);
+
+	assertEquals(1, elements.size());
+
+	// check related path
+	element = performTestForFaceletComponent("components/composition_related.xhtml");
+	node = (nsIDOMNode) element.queryInterface(nsIDOMNode.NS_IDOMNODE_IID);
+
+	elements = new ArrayList<nsIDOMNode>();
+
+	// find "table" elements
+	TestUtil.findElementsByName(node, elements, HTML.TAG_TABLE);
+
+	assertEquals(1, elements.size());
+
+	if (getException() != null) {
+	    throw getException();
+	}
+    }
+
+    /**
+     * Test for ui:component
+     * 
+     * @throws Throwable
+     */
+    public void testComponent() throws Throwable {
+
+	nsIDOMElement element = performTestForFaceletComponent("components/component.xhtml");
+	nsIDOMNode node = (nsIDOMNode) element
+		.queryInterface(nsIDOMNode.NS_IDOMNODE_IID);
+
+	List<nsIDOMNode> elements = new ArrayList<nsIDOMNode>();
+
+	// find "div" elements
+	TestUtil.findElementsByName(node, elements, HTML.TAG_DIV);
+	assertEquals(1, elements.size());
+	nsIDOMElement divElement = (nsIDOMElement) elements.get(0)
+		.queryInterface(nsIDOMElement.NS_IDOMELEMENT_IID);
+
+	nsIDOMElement divBody = (nsIDOMElement) divElement.getLastChild()
+		.queryInterface(nsIDOMElement.NS_IDOMELEMENT_IID);
+
+	nsIDOMElement div = (nsIDOMElement) divBody.getLastChild()
+		.queryInterface(nsIDOMElement.NS_IDOMELEMENT_IID);
+
+	String title = div.getAttribute("title");
+
+	assertEquals(title.replaceAll("\\s+", ""),
+		"ui:componentbinding:#{backingBean.menu}");
+	if (getException() != null) {
+	    throw getException();
+	}
+    }
+
+    /**
+     * Test for ui:remove
+     * 
+     * @throws Throwable
+     */
+    public void testRemove() throws Throwable {
+	performTestForVpeComponent((IFile) TestUtil.getComponentPath(
+		"components/remove.xhtml", IMPORT_PROJECT_NAME)); // $NON-NLS-1$
+	// TODO check that content in ui:remove isn't shown in VPE
+	assertTrue("Content inside ui:remove tag shouldn't be shown", false);
+    }
+
+    /**
+     * Test for ui:decorate
+     * 
+     * @throws Throwable
+     */
+    public void testDecorate() throws Throwable {
+	// check absolute path
+	nsIDOMElement element = performTestForFaceletComponent("components/decorate_absolute.xhtml");
+	nsIDOMNode node = (nsIDOMNode) element
+		.queryInterface(nsIDOMNode.NS_IDOMNODE_IID);
+
+	List<nsIDOMNode> elements = new ArrayList<nsIDOMNode>();
+
+	// find "table" elements
+	TestUtil.findElementsByName(node, elements, HTML.TAG_TABLE);
+
+	assertEquals(1, elements.size());
+
+	// check related path
+	element = performTestForFaceletComponent("components/decorate_related.xhtml");
+	node = (nsIDOMNode) element.queryInterface(nsIDOMNode.NS_IDOMNODE_IID);
+
+	elements = new ArrayList<nsIDOMNode>();
+
+	// find "table" elements
+	TestUtil.findElementsByName(node, elements, HTML.TAG_TABLE);
+
+	assertEquals(1, elements.size());
+
+	if (getException() != null) {
+	    throw getException();
+	}
+    }
+
+    /**
+     * Test for ui:repeat
+     * 
+     * @throws Throwable
+     */
+    public void testRepeat() throws Throwable {
+	performTestForVpeComponent((IFile) TestUtil.getComponentPath(
+		"components/repeat.xhtml", IMPORT_PROJECT_NAME)); // $NON-NLS-1$
+	assertTrue("Component's content is not shown", false);
+    }
+
+    /**
+     * Test for ui:include
+     * 
+     * @throws Throwable
+     */
+    public void testInclude() throws Throwable {
+	// check absolute path
+	nsIDOMElement element = performTestForFaceletComponent("components/include_absolute.xhtml");
+	nsIDOMNode node = (nsIDOMNode) element
+		.queryInterface(nsIDOMNode.NS_IDOMNODE_IID);
+
+	List<nsIDOMNode> elements = new ArrayList<nsIDOMNode>();
+
+	// find "table" elements
+	TestUtil.findElementsByName(node, elements, HTML.TAG_TABLE);
+
+	assertEquals(1, elements.size());
+
+	// check related path
+	element = performTestForFaceletComponent("components/include_related.xhtml");
+	node = (nsIDOMNode) element.queryInterface(nsIDOMNode.NS_IDOMNODE_IID);
+
+	elements = new ArrayList<nsIDOMNode>();
+
+	// find "table" elements
+	TestUtil.findElementsByName(node, elements, HTML.TAG_TABLE);
+
+	assertEquals(1, elements.size());
+
+	if (getException() != null) {
+	    throw getException();
+	}
+    }
+
+    /**
+     * Test for ui:fragment
+     * 
+     * @throws Throwable
+     */
+    public void testFragment() throws Throwable {
+	performTestForVpeComponent((IFile) TestUtil.getComponentPath(
+		"components/fragment.xhtml", IMPORT_PROJECT_NAME)); // $NON-NLS-1$
+	// TODO check that fragment's content is showed
+	assertTrue("Fragment's content is not sown", false);
+    }
+
+    /**
+     * Test for ui:insert
+     * 
+     * @throws Throwable
+     */
+    public void testInsert() throws Throwable {
+	performTestForVpeComponent((IFile) TestUtil.getComponentPath(
+		"components/insert.xhtml", IMPORT_PROJECT_NAME)); // $NON-NLS-1$
+    }
+
+    /**
+     * 
+     * @param componentPage
+     * @return
+     * @throws Throwable
+     */
+    private nsIDOMElement performTestForFaceletComponent(String componentPage)
+	    throws Throwable {
 	TestUtil.waitForJobs();
 	// set exception
 	setException(null);
 
 	// get test page path
-	IFile file = (IFile) TestUtil.getComponentPath(
-		"components/define.xhtml", IMPORT_PROJECT_NAME);
+	IFile file = (IFile) TestUtil.getComponentPath(componentPage,
+		IMPORT_PROJECT_NAME);
 
 	assertNotNull("Could not open specified file " + file.getFullPath(),
 		file);
@@ -68,40 +300,7 @@ public class FaceletsComponentTest extends VpeTest {
 	// check that element is not null
 	assertNotNull(element);
 
-	// get root node
-	nsIDOMNode node = (nsIDOMNode) element
-		.queryInterface(nsIDOMNode.NS_IDOMNODE_IID);
-
-	List<nsIDOMNode> elements = new ArrayList<nsIDOMNode>();
-
-	// find "span" elements
-	TestUtil.findElementsByName(node, elements, HTML.TAG_SPAN);
-
-	assertEquals(2, elements.size());
-
-	nsIDOMElement elementSpan0 = (nsIDOMElement) elements.get(0)
-		.queryInterface(nsIDOMElement.NS_IDOMELEMENT_IID);
-	nsIDOMElement elementSpan1 = (nsIDOMElement) elements.get(1)
-		.queryInterface(nsIDOMElement.NS_IDOMELEMENT_IID);
-
-	/*
-	 * nsIDOMText text0 = (nsIDOMText) elementSpan0.getFirstChild();
-	 * nsIDOMText text1 = (nsIDOMText) elementSpan1.getFirstChild();
-	 * System.out.println(text0.getNodeValue());
-	 * assertEquals(elementInput0.getAttribute("value"), "");
-	 * assertNotNull(elementInput1.getAttribute("value"), "");
-	 * assertNotNull(elementInput2.getAttribute("value"), "test");
-	 */
-
-	// TODO Dzmitry Sakovich Test not complete
-	assertTrue("Defined content is not shown", false);
-	if (getException() != null) {
-	    throw getException();
-	}
-    }
-
-    public void testInsert() throws Throwable {
-	performTestForVpeComponent((IFile) TestUtil.getComponentPath("components/insert.xhtml",IMPORT_PROJECT_NAME)); // $NON-NLS-1$
+	return element;
     }
 
 }
