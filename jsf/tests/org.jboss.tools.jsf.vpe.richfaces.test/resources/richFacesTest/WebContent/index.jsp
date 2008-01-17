36409e04830fc98470f9aa0fed18b96cc8a97e1c
Dzmitry Sakovich <dsakovich@exadel.com>
2008-01-17 15:19:49 +0000

    Add junit test for http://jira.jboss.com/jira/browse/JBIDE-1614
    git-svn-id: http://svn.jboss.org/repos/jbosstools/trunk@5768 a97e2381-89e5-4abb-bab3-167db6db766c

--- jsf/tests/org.jboss.tools.jsf.vpe.richfaces.test/resources/richFacesTest/WebContent/index.jsp
+++ jsf/tests/org.jboss.tools.jsf.vpe.richfaces.test/resources/richFacesTest/WebContent/index.jsp
@@ -1,7 +1,7 @@
 <!doctype html public "-//w3c//dtd html 4.0 transitional//en">
+
 <html>
-<head></head>
 	<body>
-		<jsp:forward page="pages/richFacesTest.jsf" />
+		<jsp:forward page="/pages/inputUserName.jsf" />
 	</body>
 </html>
--- jsf/tests/org.jboss.tools.jsf.vpe.richfaces.test/src/org/jboss/tools/jsf/vpe/richfaces/test/RichFacesAllTests.java
+++ jsf/tests/org.jboss.tools.jsf.vpe.richfaces.test/src/org/jboss/tools/jsf/vpe/richfaces/test/RichFacesAllTests.java
@@ -18,9 +18,9 @@ import junit.framework.TestSuite;
 import org.jboss.tools.jsf.vpe.richfaces.test.jbide.JBIDE1579Test;
 import org.jboss.tools.jsf.vpe.richfaces.test.jbide.JBIDE1613Test;
 import org.jboss.tools.jsf.vpe.richfaces.test.jbide.Jbide1580Test;
+import org.jboss.tools.jsf.vpe.richfaces.test.jbide.Jbide1614Test;
 import org.jboss.tools.vpe.ui.test.VpeTestSetup;
 import org.jboss.tools.vpe.ui.test.beans.ImportBean;
-
 
 /**
  * Class for testing all RichFaces components
@@ -29,29 +29,30 @@ import org.jboss.tools.vpe.ui.test.beans.ImportBean;
  * 
  */
 
-public class RichFacesAllTests  {
+public class RichFacesAllTests {
 
-	public static Test suite() {
-		TestSuite suite = new TestSuite("Tests for Vpe RichFaces components"); // $NON-NLS-1$
-		// $JUnit-BEGIN$
+    public static Test suite() {
+	TestSuite suite = new TestSuite("Tests for Vpe RichFaces components"); // $NON-NLS-1$
+	// $JUnit-BEGIN$
 
-		suite.addTestSuite(RichFacesComponentTest.class);
-		suite.addTestSuite(JBIDE1579Test.class);
-		suite.addTestSuite(Jbide1580Test.class);
-		suite.addTestSuite(JBIDE1613Test.class);
+	suite.addTestSuite(RichFacesComponentTest.class);
+	suite.addTestSuite(JBIDE1579Test.class);
+	suite.addTestSuite(Jbide1580Test.class);
+	suite.addTestSuite(JBIDE1613Test.class);
+	suite.addTestSuite(Jbide1614Test.class);
 
-		// $JUnit-END$
+	// $JUnit-END$
 
-		List<ImportBean> projectToImport = new ArrayList<ImportBean>();
-		ImportBean importBean = new ImportBean();
-		importBean
-				.setImportProjectName(RichFacesComponentTest.IMPORT_PROJECT_NAME);
-		importBean.setImportProjectPath(RichFacesTestPlugin
-				.getPluginResourcePath());
-		projectToImport.add(importBean);
+	List<ImportBean> projectToImport = new ArrayList<ImportBean>();
+	ImportBean importBean = new ImportBean();
+	importBean
+		.setImportProjectName(RichFacesComponentTest.IMPORT_PROJECT_NAME);
+	importBean.setImportProjectPath(RichFacesTestPlugin
+		.getPluginResourcePath());
+	projectToImport.add(importBean);
 
-		return new VpeTestSetup(suite, projectToImport);
+	return new VpeTestSetup(suite, projectToImport);
 
-	}
+    }
 
 }
