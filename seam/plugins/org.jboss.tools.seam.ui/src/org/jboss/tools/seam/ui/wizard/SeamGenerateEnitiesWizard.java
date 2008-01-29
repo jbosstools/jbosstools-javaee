31b166ed0b33faf1347bf58ff7ed2d2e274c23ce
Denis Golovin <dgolovin@exadel.com>
2008-01-29 09:35:30 +0000

    fix seam.ui compilation error :(
    javac] 215. ERROR in /home/hudson/jbosstools-build/eclipse/plugins/org.jboss.tools.seam.ui/src/org/jboss/tools/seam/ui/wizard/SeamGenerateEnitiesWizard.java (at line 313)
         [java]     [javac] 	SeamRuntime seamRt = SeamRuntimeManager.getDefaultRuntimeForProject(project);
         [java]     [javac] 	                                        ^^^^^^^^^^^^^^^^^^^^^^^^^^^
         [java]     [javac] The method getDefaultRuntimeForProject(IProject) is undefined for the type SeamRuntimeManager
    
    git-svn-id: http://svn.jboss.org/repos/jbosstools/trunk@6006 a97e2381-89e5-4abb-bab3-167db6db766c

--- seam/plugins/org.jboss.tools.seam.ui/src/org/jboss/tools/seam/ui/wizard/SeamGenerateEnitiesWizard.java
+++ seam/plugins/org.jboss.tools.seam.ui/src/org/jboss/tools/seam/ui/wizard/SeamGenerateEnitiesWizard.java
@@ -310,7 +310,7 @@ public class SeamGenerateEnitiesWizard extends SeamBaseWizard implements INewWiz
 		if(project==null) {
 			return null;
 		}
-		SeamRuntime seamRt = SeamRuntimeManager.getDefaultRuntimeForProject(project);
+		SeamRuntime seamRt = SeamRuntimeManager.getInstance().getRuntimeForProject(project);
 
 		String runtimeName = SeamCorePlugin.getSeamPreferences(project)
 			.get(ISeamFacetDataModelProperties.SEAM_RUNTIME_NAME,""); //$NON-NLS-1$
