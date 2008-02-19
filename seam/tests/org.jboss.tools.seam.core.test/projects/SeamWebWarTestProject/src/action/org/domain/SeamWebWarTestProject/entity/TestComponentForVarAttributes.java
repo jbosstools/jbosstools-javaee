be6163ad7133a5affa9eabdd5e0c8522b26ca9d1
Alexey Kazakov <akazakov@exadel.com>
2008-02-19 16:03:04 +0000

    JBIDE-999 Improved JUnitTest
    git-svn-id: http://svn.jboss.org/repos/jbosstools/trunk@6433 a97e2381-89e5-4abb-bab3-167db6db766c

--- seam/tests/org.jboss.tools.seam.core.test/projects/SeamWebWarTestProject/src/action/org/domain/SeamWebWarTestProject/entity/TestComponentForVarAttributes.java
+++ seam/tests/org.jboss.tools.seam.core.test/projects/SeamWebWarTestProject/src/action/org/domain/SeamWebWarTestProject/entity/TestComponentForVarAttributes.java
@@ -1,15 +1,34 @@
 package org.domain.SeamWebWarTestProject.entity;
 
-import java.io.Serializable;
-import org.jboss.seam.ScopeType;
-import org.jboss.seam.annotations.Name;
-import org.jboss.seam.annotations.Scope;
 import java.util.List;
+import java.util.Map;
+import java.util.Set;
 
-@Scope(ScopeType.EVENT)
+import org.jboss.seam.annotations.Name;
+import org.jboss.seam.annotations.datamodel.DataModel;
+
 @Name("testComponentForVars")
-public class TestComponentForVarAttributes implements Serializable {
-	static final long serialVersionUID = 1001;
+public class TestComponentForVarAttributes {
+
+    @DataModel
+	private Set<Value> setItems;
+
+    @DataModel
+	private List<Value> listItems;
+
+    @DataModel
+	private Map<String, Test> mapItems;
+
+    @DataModel
+	private Value[] arrayItems;
+
+    public Test getTest() {
+    	return null;
+    }
+
+	public List<Value> getList() {
+	    return null;
+	}
 
 	public static class Value {
 		public String getName() {
@@ -17,7 +36,9 @@ public class TestComponentForVarAttributes implements Serializable {
 		}
 	}
 
-	public List<Value> getList() {
-	    return null;
+	public static class Test {
+		public List<Value> getItems() {
+			return null;
+		}
 	}
 }
