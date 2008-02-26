a9d417236f3fe3e86e49e5de3c5af615cc87dae9
Sergey Dzmitrovich <sdzmitrovich@gmail.com>
2008-02-26 18:03:50 +0000

    http://jira.jboss.com/jira/browse/JBIDE-1718
    git-svn-id: http://svn.jboss.org/repos/jbosstools/trunk@6584 a97e2381-89e5-4abb-bab3-167db6db766c

---
+++ jsf/plugins/org.jboss.tools.jsf.vpe.jsf/src/org/jboss/tools/jsf/vpe/jsf/template/AbstractOutputJsfTemplate.java
@@ -0,0 +1,92 @@
+package org.jboss.tools.jsf.vpe.jsf.template;
+
+import org.eclipse.wst.xml.core.internal.provisional.document.IDOMAttr;
+import org.jboss.tools.vpe.editor.bundle.BundleMap;
+import org.jboss.tools.vpe.editor.context.VpePageContext;
+import org.jboss.tools.vpe.editor.util.HTML;
+import org.mozilla.interfaces.nsIDOMElement;
+import org.w3c.dom.Attr;
+import org.w3c.dom.Element;
+
+public abstract class AbstractOutputJsfTemplate extends AbstractJsfTemplate {
+
+	/**
+	 * name of "value" attribute
+	 */
+	protected static final String VALUE_ATTR_NAME = "value";
+
+	/**
+	 * name of "binding" attribute
+	 */
+	protected static final String BINDING_ATTR_NAME = "binding";
+
+	/**
+	 * name of "escape" attribute
+	 */
+	protected static final String ESCAPE_ATTR_NAME = "escape";
+
+	/**
+	 * name of "dir" attribute
+	 */
+	protected static final String DIR_ATTR_NAME = "dir";
+
+	/**
+	 * 
+	 * @param element
+	 * @return
+	 */
+	protected Attr getOutputAttributeNode(Element element) {
+
+		if (element.hasAttribute(VALUE_ATTR_NAME))
+			return element.getAttributeNode(VALUE_ATTR_NAME);
+		else if (element.hasAttribute(BINDING_ATTR_NAME))
+			return element.getAttributeNode(BINDING_ATTR_NAME);
+
+		return null;
+
+	}
+
+	/**
+	 * get bundle
+	 * 
+	 * @param pageContext
+	 * @param attr
+	 * @return
+	 */
+	protected String getBundleValue(VpePageContext pageContext, Attr attr) {
+
+		return getBundleValue(pageContext, attr.getNodeValue(),
+				((IDOMAttr) attr).getValueRegionStartOffset());
+
+	}
+
+	/**
+	 * 
+	 * @param pageContext
+	 * @param value
+	 * @param offfset
+	 * @return
+	 */
+	protected String getBundleValue(VpePageContext pageContext, String value,
+			int offfset) {
+
+		BundleMap bundle = pageContext.getBundle();
+
+		return bundle.getBundleValue(value, offfset);
+
+	}
+
+	/**
+	 * copy outputAttributes
+	 * 
+	 * @param visualElement
+	 * @param sourceElement
+	 */
+	protected void copyOutputJsfAttributes(nsIDOMElement visualElement,
+			Element sourceElement) {
+		copyGeneralJsfAttributes(visualElement, sourceElement);
+		copyAttribute(visualElement, sourceElement, DIR_ATTR_NAME,
+				HTML.ATTR_DIR);
+
+	}
+}
