fd81566fbb9cb72778e8ef0a7613bc1474fd7f9b
Sergey Dzmitrovich <sdzmitrovich@gmail.com>
2008-02-28 12:02:59 +0000

    http://jira.jboss.com/jira/browse/JBIDE-1718
    created tests
    
    git-svn-id: http://svn.jboss.org/repos/jbosstools/trunk@6636 a97e2381-89e5-4abb-bab3-167db6db766c

--- jsf/tests/org.jboss.tools.jsf.vpe.jsf.test/src/org/jboss/tools/jsf/vpe/jsf/test/JsfAllTests.java
+++ jsf/tests/org.jboss.tools.jsf.vpe.jsf.test/src/org/jboss/tools/jsf/vpe/jsf/test/JsfAllTests.java
@@ -28,6 +28,7 @@ import org.jboss.tools.jsf.vpe.jsf.test.jbide.JBIDE788Test;
 import org.jboss.tools.jsf.vpe.jsf.test.jbide.JsfJbide1467Test;
 import org.jboss.tools.jsf.vpe.jsf.test.jbide.JsfJbide1501Test;
 import org.jboss.tools.jsf.vpe.jsf.test.jbide.JsfJbide1568Test;
+import org.jboss.tools.jsf.vpe.jsf.test.jbide.JsfJbide1718Test;
 import org.jboss.tools.vpe.ui.test.VpeTestSetup;
 import org.jboss.tools.vpe.ui.test.beans.ImportBean;
 
@@ -57,7 +58,7 @@ public class JsfAllTests {
 		suite.addTestSuite(JBIDE675Test.class);
 		suite.addTestSuite(JBIDE1460Test.class);
 		suite.addTestSuite(JBIDE1720Test.class);
-		
+		suite.addTestSuite(JsfJbide1718Test.class) ;
 		// $JUnit-END$
 		//added by Max Areshkau
 		//add here projects which should be imported for junit tests
