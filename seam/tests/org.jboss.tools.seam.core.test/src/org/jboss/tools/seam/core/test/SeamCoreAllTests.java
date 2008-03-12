05dff80ba7dc6cd7be8bc318361bec45507984cf
Viacheslav Kabanovich <scabanovich@exadel.com>
2008-03-12 14:56:11 +0000

    JBIDE-1785 Test added
    git-svn-id: http://svn.jboss.org/repos/jbosstools/trunk@6894 a97e2381-89e5-4abb-bab3-167db6db766c

--- seam/tests/org.jboss.tools.seam.core.test/src/org/jboss/tools/seam/core/test/SeamCoreAllTests.java
+++ seam/tests/org.jboss.tools.seam.core.test/src/org/jboss/tools/seam/core/test/SeamCoreAllTests.java
@@ -28,6 +28,7 @@ public class SeamCoreAllTests {
 		suite.setName("All tests for " + PLUGIN_ID);
 		suite.addTestSuite(ScannerTest.class);
 		suite.addTestSuite(SerializationTest.class);
+		suite.addTestSuite(SeamBigProjectTest.class);
 		suite.addTestSuite(SeamEARTest.class);
 		suite.addTestSuite(SeamRuntimeListConverterTest.class);
 		suite.addTestSuite(SeamRuntimeManagerTest.class);
