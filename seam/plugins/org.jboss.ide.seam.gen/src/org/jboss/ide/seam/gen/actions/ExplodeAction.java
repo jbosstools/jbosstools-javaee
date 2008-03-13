a343a3307672e6982169f5512ca79ce013cb40e9
Marshall Culpepper <mculpepper@jboss.com>
2008-03-13 21:47:30 +0000

    retagging seam for 2.0.1
    git-svn-id: http://svn.jboss.org/repos/jbosstools/tags/jbosstools-2.0.1.GA@6923 a97e2381-89e5-4abb-bab3-167db6db766c

--- seam/plugins/org.jboss.ide.seam.gen/src/org/jboss/ide/seam/gen/actions/ExplodeAction.java
+++
@@ -1,9 +0,0 @@
-package org.jboss.ide.seam.gen.actions;
-
-public class ExplodeAction extends SeamGenAction {
-
-	protected String getTarget() {
-		return "explode";
-	}
-
-}
--- seam/plugins/org.jboss.ide.seam.gen/src/org/jboss/ide/seam/gen/actions/UndeployAction.java
+++
@@ -1,9 +0,0 @@
-package org.jboss.ide.seam.gen.actions;
-
-public class UndeployAction extends SeamGenAction {
-
-	protected String getTarget() {
-		return "undeploy";
-	}
-
-}
