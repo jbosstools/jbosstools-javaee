dd5986a7a2188b84b89ac6d3127638bee4103a11
Sergey Dzmitrovich <sdzmitrovich@gmail.com>
2008-04-03 10:29:19 +0000

    http://jira.jboss.com/jira/browse/JBIDE-1373
    git-svn-id: http://svn.jboss.org/repos/jbosstools/trunk@7278 a97e2381-89e5-4abb-bab3-167db6db766c

---
+++ jsf/plugins/org.jboss.tools.jsf.vpe.jsf/src/org/jboss/tools/jsf/vpe/jsf/template/JsfVerbatim.java
@@ -0,0 +1,83 @@
+/*******************************************************************************
+ * Copyright (c) 2007-2008 Red Hat, Inc.
+ * Distributed under license by Red Hat, Inc. All rights reserved.
+ * This program is made available under the terms of the
+ * Eclipse Public License v1.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributor:
+ *     Red Hat, Inc. - initial API and implementation
+ ******************************************************************************/
+package org.jboss.tools.jsf.vpe.jsf.template;
+
+import org.eclipse.wst.xml.core.internal.provisional.document.IDOMNode;
+import org.jboss.tools.vpe.editor.context.VpePageContext;
+import org.jboss.tools.vpe.editor.template.EditableTemplateAdapter;
+import org.jboss.tools.vpe.editor.template.VpeCreationData;
+import org.jboss.tools.vpe.editor.util.HTML;
+import org.jboss.tools.vpe.editor.util.TemplateManagingUtil;
+import org.mozilla.interfaces.nsIDOMDocument;
+import org.mozilla.interfaces.nsIDOMElement;
+import org.w3c.dom.Element;
+import org.w3c.dom.Node;
+import org.w3c.dom.NodeList;
+
+/**
+ * @author Sergey Dzmitrovich
+ * 
+ */
+public class JsfVerbatim extends EditableTemplateAdapter {
+
+	/**
+	 * Instantiates a new jsf view.
+	 */
+	public JsfVerbatim() {
+	}
+
+	/*
+	 * (non-Javadoc)
+	 * 
+	 * @see org.jboss.tools.vpe.editor.template.VpeTemplate#create(org.jboss.tools.vpe.editor.context.VpePageContext,
+	 *      org.w3c.dom.Node, org.mozilla.interfaces.nsIDOMDocument)
+	 */
+	public VpeCreationData create(VpePageContext pageContext, Node sourceNode,
+			nsIDOMDocument visualDocument) {
+
+		Element element = (Element) sourceNode;
+
+		nsIDOMElement span = visualDocument.createElement(HTML.TAG_SPAN);
+
+		NodeList list = element.getChildNodes();
+
+		if (list.getLength() != 0) {
+			Node firstNode = list.item(0);
+			Node lastNode = list.item(list.getLength() - 1);
+
+			String text = TemplateManagingUtil.getSourceText(pageContext,
+					((IDOMNode) firstNode).getStartOffset(),
+					((IDOMNode) lastNode).getEndOffset() - 1);
+
+			span.appendChild(visualDocument.createTextNode(text));
+
+		}
+
+		VpeCreationData creationData = new VpeCreationData(span);
+
+		return creationData;
+	}
+
+	/*
+	 * (non-Javadoc)
+	 * 
+	 * @see org.jboss.tools.vpe.editor.template.VpeAbstractTemplate#isRecreateAtAttrChange(org.jboss.tools.vpe.editor.context.VpePageContext,
+	 *      org.w3c.dom.Element, org.mozilla.interfaces.nsIDOMDocument,
+	 *      org.mozilla.interfaces.nsIDOMElement, java.lang.Object,
+	 *      java.lang.String, java.lang.String)
+	 */
+	public boolean isRecreateAtAttrChange(VpePageContext pageContext,
+			Element sourceElement, nsIDOMDocument visualDocument,
+			nsIDOMElement visualNode, Object data, String name, String value) {
+		return true;
+	}
+
+}
