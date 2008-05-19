51d4b444a5e35cd03769b8655bac3b4030eff4d7
Daniel Azarov <daniel@exadel.com>
2008-05-19 14:30:08 +0000

    http://jira.jboss.com/jira/browse/JBIDE-2004
    git-svn-id: http://svn.jboss.org/repos/jbosstools/trunk@8180 a97e2381-89e5-4abb-bab3-167db6db766c

--- seam/tests/org.jboss.tools.seam.ui.test/src/org/jboss/tools/seam/ui/test/wizard/Seam20EARNewOperationTest.java
+++ seam/tests/org.jboss.tools.seam.ui.test/src/org/jboss/tools/seam/ui/test/wizard/Seam20EARNewOperationTest.java
@@ -1,5 +1,8 @@
 package org.jboss.tools.seam.ui.test.wizard;
 
 public class Seam20EARNewOperationTest extends Seam12EARNewOperationTest {
-
+	@Override
+	void setUpSeamProjects() {
+		setUpSeamProject(earProject, AbstractSeamNewOperationTest.SEAM_1_2);
+	}
 }
