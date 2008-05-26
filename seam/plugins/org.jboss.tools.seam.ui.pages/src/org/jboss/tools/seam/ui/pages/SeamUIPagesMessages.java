54e6407be60bb3bfbbaebcf7590c07e9892828cc
Daniel Azarov <daniel@exadel.com>
2008-05-26 16:09:30 +0000

    http://jira.jboss.com/jira/browse/JBIDE-1189
    git-svn-id: http://svn.jboss.org/repos/jbosstools/trunk@8334 a97e2381-89e5-4abb-bab3-167db6db766c

--- seam/plugins/org.jboss.tools.seam.ui.pages/src/org/jboss/tools/seam/ui/pages/SeamUIPagesMessages.java
+++ seam/plugins/org.jboss.tools.seam.ui.pages/src/org/jboss/tools/seam/ui/pages/SeamUIPagesMessages.java
@@ -1,22 +1,34 @@
 package org.jboss.tools.seam.ui.pages;
 
+import java.util.Locale;
 import java.util.MissingResourceException;
 import java.util.ResourceBundle;
 
-public class SeamUIPagesMessages {
+import org.eclipse.osgi.util.NLS;
+
+public class SeamUIPagesMessages extends NLS{
 	private static final String BUNDLE_NAME = "org.jboss.tools.seam.ui.pages.messages"; //$NON-NLS-1$
 
-	private static final ResourceBundle RESOURCE_BUNDLE = ResourceBundle
-			.getBundle(BUNDLE_NAME);
-
-	private SeamUIPagesMessages() {
+	private static ResourceBundle fResourceBundle; 
+	static {
+		// load message values from bundle file
+		NLS.initializeMessages(BUNDLE_NAME, SeamUIPagesMessages.class);		
 	}
-
-	public static String getString(String key) {
+	
+	public static String PAGESDIAGRAM_SELECT;
+	public static String PAGESDIAGRAM_MARQUEE;
+	public static String PAGESDIAGRAM_CREATE_NEW_CONNECTION;
+	
+	public static ResourceBundle getResourceBundle() {
 		try {
-			return RESOURCE_BUNDLE.getString(key);
-		} catch (MissingResourceException e) {
-			return '!' + key + '!';
+			if (fResourceBundle == null)
+				fResourceBundle = ResourceBundle.getBundle(BUNDLE_NAME, Locale.getDefault());
 		}
-	}
+		catch (MissingResourceException x) {
+			SeamUiPagesPlugin.log(x);
+			fResourceBundle = null;
+		}
+		return fResourceBundle;
+	}	
+
 }
