2b1f151052336b6b05e62c03a80989c68a8a1b99
Daniel Azarov <daniel@exadel.com>
2008-05-27 11:41:44 +0000

    http://jira.jboss.com/jira/browse/JBIDE-1189
    git-svn-id: http://svn.jboss.org/repos/jbosstools/trunk@8367 a97e2381-89e5-4abb-bab3-167db6db766c

--- seam/plugins/org.jboss.tools.seam.ui.pages/src/org/jboss/tools/seam/ui/pages/editor/model/IPagesModel.java
+++
@@ -1,6 +0,0 @@
-package org.jboss.tools.seam.ui.pages.editor.model;
-
-public interface IPagesModel {
-	public void addModelListener(IPagesModelListener listener);
-	public void removeModelListener(IPagesModelListener listener);
-}
--- seam/plugins/org.jboss.tools.seam.ui.pages/src/org/jboss/tools/seam/ui/pages/editor/model/IPagesModelListener.java
+++
@@ -1,5 +0,0 @@
-package org.jboss.tools.seam.ui.pages.editor.model;
-
-public interface IPagesModelListener {
-
-}
