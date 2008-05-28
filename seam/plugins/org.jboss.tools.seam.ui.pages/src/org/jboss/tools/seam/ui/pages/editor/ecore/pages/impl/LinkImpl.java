f7ea69c8e4c5256a48e223141e3784a956a61996
Daniel Azarov <daniel@exadel.com>
2008-05-28 12:15:38 +0000

    http://jira.jboss.com/jira/browse/JBIDE-1189
    git-svn-id: http://svn.jboss.org/repos/jbosstools/trunk@8399 a97e2381-89e5-4abb-bab3-167db6db766c

---
+++ seam/plugins/org.jboss.tools.seam.ui.pages/src/org/jboss/tools/seam/ui/pages/editor/ecore/pages/impl/LinkImpl.java
@@ -0,0 +1,424 @@
+/**
+ * <copyright>
+ * </copyright>
+ *
+ * $Id$
+ */
+package org.jboss.tools.seam.ui.pages.editor.ecore.pages.impl;
+
+import org.eclipse.emf.common.notify.Notification;
+import org.eclipse.emf.common.notify.NotificationChain;
+
+import org.eclipse.emf.ecore.EClass;
+import org.eclipse.emf.ecore.InternalEObject;
+
+import org.eclipse.emf.ecore.impl.ENotificationImpl;
+import org.eclipse.emf.ecore.impl.EObjectImpl;
+
+import org.jboss.tools.seam.ui.pages.editor.ecore.pages.Link;
+import org.jboss.tools.seam.ui.pages.editor.ecore.pages.PagesElement;
+import org.jboss.tools.seam.ui.pages.editor.ecore.pages.PagesPackage;
+
+/**
+ * <!-- begin-user-doc -->
+ * An implementation of the model object '<em><b>Link</b></em>'.
+ * <!-- end-user-doc -->
+ * <p>
+ * The following features are implemented:
+ * <ul>
+ *   <li>{@link org.jboss.tools.seam.ui.pages.editor.ecore.pages.impl.LinkImpl#getName <em>Name</em>}</li>
+ *   <li>{@link org.jboss.tools.seam.ui.pages.editor.ecore.pages.impl.LinkImpl#getFromElement <em>From Element</em>}</li>
+ *   <li>{@link org.jboss.tools.seam.ui.pages.editor.ecore.pages.impl.LinkImpl#getToElement <em>To Element</em>}</li>
+ *   <li>{@link org.jboss.tools.seam.ui.pages.editor.ecore.pages.impl.LinkImpl#isShortcut <em>Shortcut</em>}</li>
+ * </ul>
+ * </p>
+ *
+ * @generated
+ */
+public class LinkImpl extends EObjectImpl implements Link {
+	/**
+	 * The default value of the '{@link #getName() <em>Name</em>}' attribute.
+	 * <!-- begin-user-doc -->
+	 * <!-- end-user-doc -->
+	 * @see #getName()
+	 * @generated
+	 * @ordered
+	 */
+	protected static final String NAME_EDEFAULT = null;
+
+	/**
+	 * The cached value of the '{@link #getName() <em>Name</em>}' attribute.
+	 * <!-- begin-user-doc -->
+	 * <!-- end-user-doc -->
+	 * @see #getName()
+	 * @generated
+	 * @ordered
+	 */
+	protected String name = NAME_EDEFAULT;
+
+	/**
+	 * The cached value of the '{@link #getFromElement() <em>From Element</em>}' reference.
+	 * <!-- begin-user-doc -->
+	 * <!-- end-user-doc -->
+	 * @see #getFromElement()
+	 * @generated
+	 * @ordered
+	 */
+	protected PagesElement fromElement;
+
+	/**
+	 * The cached value of the '{@link #getToElement() <em>To Element</em>}' reference.
+	 * <!-- begin-user-doc -->
+	 * <!-- end-user-doc -->
+	 * @see #getToElement()
+	 * @generated
+	 * @ordered
+	 */
+	protected PagesElement toElement;
+
+	/**
+	 * The default value of the '{@link #isShortcut() <em>Shortcut</em>}' attribute.
+	 * <!-- begin-user-doc -->
+	 * <!-- end-user-doc -->
+	 * @see #isShortcut()
+	 * @generated
+	 * @ordered
+	 */
+	protected static final boolean SHORTCUT_EDEFAULT = false;
+
+	/**
+	 * The cached value of the '{@link #isShortcut() <em>Shortcut</em>}' attribute.
+	 * <!-- begin-user-doc -->
+	 * <!-- end-user-doc -->
+	 * @see #isShortcut()
+	 * @generated
+	 * @ordered
+	 */
+	protected boolean shortcut = SHORTCUT_EDEFAULT;
+
+	/**
+	 * <!-- begin-user-doc -->
+	 * <!-- end-user-doc -->
+	 * @generated
+	 */
+	protected LinkImpl() {
+		super();
+	}
+
+	/**
+	 * <!-- begin-user-doc -->
+	 * <!-- end-user-doc -->
+	 * @generated
+	 */
+	@Override
+	protected EClass eStaticClass() {
+		return PagesPackage.Literals.LINK;
+	}
+
+	/**
+	 * <!-- begin-user-doc -->
+	 * <!-- end-user-doc -->
+	 * @generated
+	 */
+	public String getName() {
+		return name;
+	}
+
+	/**
+	 * <!-- begin-user-doc -->
+	 * <!-- end-user-doc -->
+	 * @generated
+	 */
+	public void setName(String newName) {
+		String oldName = name;
+		name = newName;
+		if (eNotificationRequired())
+			eNotify(new ENotificationImpl(this, Notification.SET, PagesPackage.LINK__NAME, oldName, name));
+	}
+
+	/**
+	 * <!-- begin-user-doc -->
+	 * <!-- end-user-doc -->
+	 * @generated
+	 */
+	public PagesElement getFromElement() {
+		if (fromElement != null && fromElement.eIsProxy()) {
+			InternalEObject oldFromElement = (InternalEObject)fromElement;
+			fromElement = (PagesElement)eResolveProxy(oldFromElement);
+			if (fromElement != oldFromElement) {
+				if (eNotificationRequired())
+					eNotify(new ENotificationImpl(this, Notification.RESOLVE, PagesPackage.LINK__FROM_ELEMENT, oldFromElement, fromElement));
+			}
+		}
+		return fromElement;
+	}
+
+	/**
+	 * <!-- begin-user-doc -->
+	 * <!-- end-user-doc -->
+	 * @generated
+	 */
+	public PagesElement basicGetFromElement() {
+		return fromElement;
+	}
+
+	/**
+	 * <!-- begin-user-doc -->
+	 * <!-- end-user-doc -->
+	 * @generated
+	 */
+	public NotificationChain basicSetFromElement(PagesElement newFromElement, NotificationChain msgs) {
+		PagesElement oldFromElement = fromElement;
+		fromElement = newFromElement;
+		if (eNotificationRequired()) {
+			ENotificationImpl notification = new ENotificationImpl(this, Notification.SET, PagesPackage.LINK__FROM_ELEMENT, oldFromElement, newFromElement);
+			if (msgs == null) msgs = notification; else msgs.add(notification);
+		}
+		return msgs;
+	}
+
+	/**
+	 * <!-- begin-user-doc -->
+	 * <!-- end-user-doc -->
+	 * @generated
+	 */
+	public void setFromElement(PagesElement newFromElement) {
+		if (newFromElement != fromElement) {
+			NotificationChain msgs = null;
+			if (fromElement != null)
+				msgs = ((InternalEObject)fromElement).eInverseRemove(this, PagesPackage.PAGES_ELEMENT__OUTPUT_LINKS, PagesElement.class, msgs);
+			if (newFromElement != null)
+				msgs = ((InternalEObject)newFromElement).eInverseAdd(this, PagesPackage.PAGES_ELEMENT__OUTPUT_LINKS, PagesElement.class, msgs);
+			msgs = basicSetFromElement(newFromElement, msgs);
+			if (msgs != null) msgs.dispatch();
+		}
+		else if (eNotificationRequired())
+			eNotify(new ENotificationImpl(this, Notification.SET, PagesPackage.LINK__FROM_ELEMENT, newFromElement, newFromElement));
+	}
+
+	/**
+	 * <!-- begin-user-doc -->
+	 * <!-- end-user-doc -->
+	 * @generated
+	 */
+	public PagesElement getToElement() {
+		if (toElement != null && toElement.eIsProxy()) {
+			InternalEObject oldToElement = (InternalEObject)toElement;
+			toElement = (PagesElement)eResolveProxy(oldToElement);
+			if (toElement != oldToElement) {
+				if (eNotificationRequired())
+					eNotify(new ENotificationImpl(this, Notification.RESOLVE, PagesPackage.LINK__TO_ELEMENT, oldToElement, toElement));
+			}
+		}
+		return toElement;
+	}
+
+	/**
+	 * <!-- begin-user-doc -->
+	 * <!-- end-user-doc -->
+	 * @generated
+	 */
+	public PagesElement basicGetToElement() {
+		return toElement;
+	}
+
+	/**
+	 * <!-- begin-user-doc -->
+	 * <!-- end-user-doc -->
+	 * @generated
+	 */
+	public NotificationChain basicSetToElement(PagesElement newToElement, NotificationChain msgs) {
+		PagesElement oldToElement = toElement;
+		toElement = newToElement;
+		if (eNotificationRequired()) {
+			ENotificationImpl notification = new ENotificationImpl(this, Notification.SET, PagesPackage.LINK__TO_ELEMENT, oldToElement, newToElement);
+			if (msgs == null) msgs = notification; else msgs.add(notification);
+		}
+		return msgs;
+	}
+
+	/**
+	 * <!-- begin-user-doc -->
+	 * <!-- end-user-doc -->
+	 * @generated
+	 */
+	public void setToElement(PagesElement newToElement) {
+		if (newToElement != toElement) {
+			NotificationChain msgs = null;
+			if (toElement != null)
+				msgs = ((InternalEObject)toElement).eInverseRemove(this, PagesPackage.PAGES_ELEMENT__INPUT_LINKS, PagesElement.class, msgs);
+			if (newToElement != null)
+				msgs = ((InternalEObject)newToElement).eInverseAdd(this, PagesPackage.PAGES_ELEMENT__INPUT_LINKS, PagesElement.class, msgs);
+			msgs = basicSetToElement(newToElement, msgs);
+			if (msgs != null) msgs.dispatch();
+		}
+		else if (eNotificationRequired())
+			eNotify(new ENotificationImpl(this, Notification.SET, PagesPackage.LINK__TO_ELEMENT, newToElement, newToElement));
+	}
+
+	/**
+	 * <!-- begin-user-doc -->
+	 * <!-- end-user-doc -->
+	 * @generated
+	 */
+	public boolean isShortcut() {
+		return shortcut;
+	}
+
+	/**
+	 * <!-- begin-user-doc -->
+	 * <!-- end-user-doc -->
+	 * @generated
+	 */
+	public void setShortcut(boolean newShortcut) {
+		boolean oldShortcut = shortcut;
+		shortcut = newShortcut;
+		if (eNotificationRequired())
+			eNotify(new ENotificationImpl(this, Notification.SET, PagesPackage.LINK__SHORTCUT, oldShortcut, shortcut));
+	}
+
+	/**
+	 * <!-- begin-user-doc -->
+	 * <!-- end-user-doc -->
+	 * @generated
+	 */
+	@Override
+	public NotificationChain eInverseAdd(InternalEObject otherEnd, int featureID, NotificationChain msgs) {
+		switch (featureID) {
+			case PagesPackage.LINK__FROM_ELEMENT:
+				if (fromElement != null)
+					msgs = ((InternalEObject)fromElement).eInverseRemove(this, PagesPackage.PAGES_ELEMENT__OUTPUT_LINKS, PagesElement.class, msgs);
+				return basicSetFromElement((PagesElement)otherEnd, msgs);
+			case PagesPackage.LINK__TO_ELEMENT:
+				if (toElement != null)
+					msgs = ((InternalEObject)toElement).eInverseRemove(this, PagesPackage.PAGES_ELEMENT__INPUT_LINKS, PagesElement.class, msgs);
+				return basicSetToElement((PagesElement)otherEnd, msgs);
+		}
+		return super.eInverseAdd(otherEnd, featureID, msgs);
+	}
+
+	/**
+	 * <!-- begin-user-doc -->
+	 * <!-- end-user-doc -->
+	 * @generated
+	 */
+	@Override
+	public NotificationChain eInverseRemove(InternalEObject otherEnd, int featureID, NotificationChain msgs) {
+		switch (featureID) {
+			case PagesPackage.LINK__FROM_ELEMENT:
+				return basicSetFromElement(null, msgs);
+			case PagesPackage.LINK__TO_ELEMENT:
+				return basicSetToElement(null, msgs);
+		}
+		return super.eInverseRemove(otherEnd, featureID, msgs);
+	}
+
+	/**
+	 * <!-- begin-user-doc -->
+	 * <!-- end-user-doc -->
+	 * @generated
+	 */
+	@Override
+	public Object eGet(int featureID, boolean resolve, boolean coreType) {
+		switch (featureID) {
+			case PagesPackage.LINK__NAME:
+				return getName();
+			case PagesPackage.LINK__FROM_ELEMENT:
+				if (resolve) return getFromElement();
+				return basicGetFromElement();
+			case PagesPackage.LINK__TO_ELEMENT:
+				if (resolve) return getToElement();
+				return basicGetToElement();
+			case PagesPackage.LINK__SHORTCUT:
+				return isShortcut() ? Boolean.TRUE : Boolean.FALSE;
+		}
+		return super.eGet(featureID, resolve, coreType);
+	}
+
+	/**
+	 * <!-- begin-user-doc -->
+	 * <!-- end-user-doc -->
+	 * @generated
+	 */
+	@Override
+	public void eSet(int featureID, Object newValue) {
+		switch (featureID) {
+			case PagesPackage.LINK__NAME:
+				setName((String)newValue);
+				return;
+			case PagesPackage.LINK__FROM_ELEMENT:
+				setFromElement((PagesElement)newValue);
+				return;
+			case PagesPackage.LINK__TO_ELEMENT:
+				setToElement((PagesElement)newValue);
+				return;
+			case PagesPackage.LINK__SHORTCUT:
+				setShortcut(((Boolean)newValue).booleanValue());
+				return;
+		}
+		super.eSet(featureID, newValue);
+	}
+
+	/**
+	 * <!-- begin-user-doc -->
+	 * <!-- end-user-doc -->
+	 * @generated
+	 */
+	@Override
+	public void eUnset(int featureID) {
+		switch (featureID) {
+			case PagesPackage.LINK__NAME:
+				setName(NAME_EDEFAULT);
+				return;
+			case PagesPackage.LINK__FROM_ELEMENT:
+				setFromElement((PagesElement)null);
+				return;
+			case PagesPackage.LINK__TO_ELEMENT:
+				setToElement((PagesElement)null);
+				return;
+			case PagesPackage.LINK__SHORTCUT:
+				setShortcut(SHORTCUT_EDEFAULT);
+				return;
+		}
+		super.eUnset(featureID);
+	}
+
+	/**
+	 * <!-- begin-user-doc -->
+	 * <!-- end-user-doc -->
+	 * @generated
+	 */
+	@Override
+	public boolean eIsSet(int featureID) {
+		switch (featureID) {
+			case PagesPackage.LINK__NAME:
+				return NAME_EDEFAULT == null ? name != null : !NAME_EDEFAULT.equals(name);
+			case PagesPackage.LINK__FROM_ELEMENT:
+				return fromElement != null;
+			case PagesPackage.LINK__TO_ELEMENT:
+				return toElement != null;
+			case PagesPackage.LINK__SHORTCUT:
+				return shortcut != SHORTCUT_EDEFAULT;
+		}
+		return super.eIsSet(featureID);
+	}
+
+	/**
+	 * <!-- begin-user-doc -->
+	 * <!-- end-user-doc -->
+	 * @generated
+	 */
+	@Override
+	public String toString() {
+		if (eIsProxy()) return super.toString();
+
+		StringBuffer result = new StringBuffer(super.toString());
+		result.append(" (name: ");
+		result.append(name);
+		result.append(", shortcut: ");
+		result.append(shortcut);
+		result.append(')');
+		return result.toString();
+	}
+
+} //LinkImpl
---
+++ seam/plugins/org.jboss.tools.seam.ui.pages/src/org/jboss/tools/seam/ui/pages/editor/ecore/pages/impl/PagesModelImpl.java
@@ -0,0 +1,43 @@
+/**
+ * <copyright>
+ * </copyright>
+ *
+ * $Id$
+ */
+package org.jboss.tools.seam.ui.pages.editor.ecore.pages.impl;
+
+import org.eclipse.emf.ecore.EClass;
+
+import org.jboss.tools.seam.ui.pages.editor.ecore.pages.PagesModel;
+import org.jboss.tools.seam.ui.pages.editor.ecore.pages.PagesPackage;
+
+/**
+ * <!-- begin-user-doc -->
+ * An implementation of the model object '<em><b>Model</b></em>'.
+ * <!-- end-user-doc -->
+ * <p>
+ * </p>
+ *
+ * @generated
+ */
+public class PagesModelImpl extends PagesElementImpl implements PagesModel {
+	/**
+	 * <!-- begin-user-doc -->
+	 * <!-- end-user-doc -->
+	 * @generated
+	 */
+	protected PagesModelImpl() {
+		super();
+	}
+
+	/**
+	 * <!-- begin-user-doc -->
+	 * <!-- end-user-doc -->
+	 * @generated
+	 */
+	@Override
+	protected EClass eStaticClass() {
+		return PagesPackage.Literals.PAGES_MODEL;
+	}
+
+} //PagesModelImpl
