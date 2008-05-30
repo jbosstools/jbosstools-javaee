e0bdd635bfa63049dc0f5b02a9f56f5df2e56c81
Viacheslav Kabanovich <scabanovich@exadel.com>
2008-05-30 14:03:04 +0000

    JBIDE-830
    git-svn-id: http://svn.jboss.org/repos/jbosstools/trunk@8477 a97e2381-89e5-4abb-bab3-167db6db766c

--- jsf/plugins/org.jboss.tools.jsf/src/org/jboss/tools/jsf/project/JSFAutoLoad.java
+++ jsf/plugins/org.jboss.tools.jsf/src/org/jboss/tools/jsf/project/JSFAutoLoad.java
@@ -1,15 +1,21 @@
 package org.jboss.tools.jsf.project;
 
+import java.io.IOException;
 import java.util.Properties;
 
 import org.eclipse.core.resources.IProject;
+import org.eclipse.core.resources.IResource;
+import org.eclipse.core.resources.ResourcesPlugin;
+import org.eclipse.core.runtime.IPath;
 import org.jboss.tools.common.model.XModel;
 import org.jboss.tools.common.model.XModelConstants;
 import org.jboss.tools.common.model.XModelObject;
 import org.jboss.tools.common.model.filesystems.FileSystemsHelper;
 import org.jboss.tools.common.model.filesystems.impl.FileSystemImpl;
 import org.jboss.tools.common.model.project.IAutoLoad;
+import org.jboss.tools.common.model.project.ProjectHome;
 import org.jboss.tools.common.model.util.EclipseResourceUtil;
+import org.jboss.tools.common.model.util.XModelObjectUtil;
 
 public class JSFAutoLoad implements IAutoLoad {
 
@@ -35,10 +41,13 @@ public class JSFAutoLoad implements IAutoLoad {
 		webinf.setAttributeValue("name", "WEB-INF"); //$NON-NLS-1$ //$NON-NLS-2$
 		webinf.setAttributeValue("location", XModelConstants.WORKSPACE_REF); //$NON-NLS-1$
 		fs.addChild(webinf);
+
+		String webInfLocation = XModelObjectUtil.expand(XModelConstants.WORKSPACE_REF, model, null);
+		String webRootLocation = getWebRootPath(project, webInfLocation);
 		
 		XModelObject webroot = model.createModelObject("FileSystemFolder", null); //$NON-NLS-1$
 		webroot.setAttributeValue("name", "WEB-ROOT"); //$NON-NLS-1$ //$NON-NLS-2$
-		webroot.setAttributeValue("location", XModelConstants.WORKSPACE_REF + "/.."); //$NON-NLS-1$ //$NON-NLS-2$
+		webroot.setAttributeValue("location", webRootLocation); //$NON-NLS-1$ //$NON-NLS-2$
 		fs.addChild(webroot);
 		
 		XModelObject lib = model.createModelObject("FileSystemFolder", null); //$NON-NLS-1$
@@ -46,6 +55,34 @@ public class JSFAutoLoad implements IAutoLoad {
 		lib.setAttributeValue("location", XModelConstants.WORKSPACE_REF + "/lib"); //$NON-NLS-1$ //$NON-NLS-2$
 		fs.addChild(lib);		
 		
+	}
+
+	static String getWebRootPath(IProject project, String webInfLocation) {
+		String webRootLocation = XModelConstants.WORKSPACE_REF + "/..";
+		
+		IPath wrp = ProjectHome.getFirstWebContentPath(project);
+		IPath wip = ProjectHome.getWebInfPath(project);
+
+		if(wrp == null || wip == null) {
+			return webRootLocation;
+		}
+		
+		IResource wrpc = ResourcesPlugin.getWorkspace().getRoot().findMember(wrp);
+		IResource wipc = ResourcesPlugin.getWorkspace().getRoot().findMember(wip);
+		if(wrpc != null && wipc != null && wipc.isLinked()) {
+			IPath p = wrpc.getLocation();
+			if(p != null) {
+				try {
+					webRootLocation = p.toFile().getCanonicalPath().replace('\\', '/');
+				} catch (IOException e) {
+				}
+				String relative = org.jboss.tools.common.util.FileUtil.getRelativePath(webInfLocation, webRootLocation);
+				if(relative != null) {
+					webRootLocation = XModelConstants.WORKSPACE_REF + relative;
+				}
+			}
+		}
+		return webRootLocation;
 	}
 
 }
