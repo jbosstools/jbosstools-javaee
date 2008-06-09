1a8fe8f1956d656808c057cc637b2fccbcf522ac
Viacheslav Kabanovich <scabanovich@exadel.com>
2008-06-09 09:55:05 +0000

    JBIDE-2285
    git-svn-id: http://svn.jboss.org/repos/jbosstools/trunk@8639 a97e2381-89e5-4abb-bab3-167db6db766c

--- seam/plugins/org.jboss.tools.seam.pages.xml/src/org/jboss/tools/seam/pages/xml/model/handlers/SelectOnDiagramHandler.java
+++ seam/plugins/org.jboss.tools.seam.pages.xml/src/org/jboss/tools/seam/pages/xml/model/handlers/SelectOnDiagramHandler.java
@@ -18,47 +18,67 @@ import org.jboss.tools.jst.web.model.ReferenceObject;
 import org.jboss.tools.seam.pages.xml.model.SeamPagesConstants;
 import org.jboss.tools.seam.pages.xml.model.helpers.SeamPagesDiagramStructureHelper;
 
+/**
+ * 
+ * @author Viacheslav Kabanovich
+ *
+ */
 public class SelectOnDiagramHandler extends AbstractHandler implements SeamPagesConstants {
 
-    public SelectOnDiagramHandler() {}
+	public SelectOnDiagramHandler() {}
 
-    public boolean isEnabled(XModelObject object) {
-        return (object != null && object.isActive());
-    }
+	public boolean isEnabled(XModelObject object) {
+		return (object != null && object.isActive());
+	}
 
-    public void executeHandler(XModelObject object, Properties p) throws XModelException {
-    	if(object == null) return;
-    	String entity = object.getModelEntity().getName();
-    	if(entity.startsWith(SeamPagesConstants.ENT_NAVIGATION_RULE)
-    			|| entity.startsWith(SeamPagesConstants.ENT_RULE)) {
-    		object = object.getChildByPath("target");
-    	}
-        XModelObject item = getItemOnDiagram(object);
-        if(item == null) return;
-        FindObjectHelper.findModelObject(item, FindObjectHelper.IN_EDITOR_ONLY, "Diagram");
-        FindObjectHelper.findModelObject(object, FindObjectHelper.IN_EDITOR_ONLY);
-    }
+	public void executeHandler(XModelObject object, Properties p) throws XModelException {
+		if(object == null) return;
+		XModelObject item = getItemOnDiagram(object);
+		if(item == null) return;
+		FindObjectHelper.findModelObject(item, FindObjectHelper.IN_EDITOR_ONLY, "Diagram");
+		FindObjectHelper.findModelObject(object, FindObjectHelper.IN_EDITOR_ONLY);
+	}
 
-    public static XModelObject getItemOnDiagram(XModelObject object) {
-        if(object == null) return null;
-        XModelObject diagram = SeamPagesDiagramStructureHelper.instance.getProcess(object);
-        return (diagram == null) ? null : getItemOnDiagram(diagram, object);
-    }
+	/**
+	 *
+	 * @param object
+	 * @return the best match in diagram XML for argument file XML object. 
+	 */
+	public static XModelObject getItemOnDiagram(XModelObject object) {
+		if(object == null) return null;
+		String entity = object.getModelEntity().getName();
+		if(entity.startsWith(SeamPagesConstants.ENT_NAVIGATION_RULE)
+				|| entity.startsWith(SeamPagesConstants.ENT_RULE)) {
+			object = object.getChildByPath("target");
+		} else if(entity.startsWith(SeamPagesConstants.ENT_NAVIGATION)) {
+			XModelObject[] cs = object.getChildren();
+			if(cs.length != 1) {
+				object = object.getParent();
+			} else {
+				object = cs[0].getChildByPath("target");
+			}
+			if(object == null) return null;
+		} else if(entity.startsWith(SeamPagesConstants.ENT_PARAM)) {
+			//or should we return object itself, if params are displayed by diagram?
+			return getItemOnDiagram(object.getParent());
+		}
+		XModelObject diagram = SeamPagesDiagramStructureHelper.instance.getProcess(object);
+		return (diagram == null) ? null : getItemOnDiagram(diagram, object);
+	}
 
-    public static XModelObject getItemOnDiagram(XModelObject diagramObject, XModelObject object) {
-        if(diagramObject instanceof ReferenceObject) {
-            if(((ReferenceObject)diagramObject).getReference() == object) return diagramObject;
-        }
-        return getItemOnDiagram(diagramObject.getChildren(), object);
-    }
+	private static XModelObject getItemOnDiagram(XModelObject diagramObject, XModelObject object) {
+		if(diagramObject instanceof ReferenceObject) {
+			if(((ReferenceObject)diagramObject).getReference() == object) return diagramObject;
+		}
+		return getItemOnDiagram(diagramObject.getChildren(), object);
+	}
 
-    public static XModelObject getItemOnDiagram(XModelObject[] diagramObjects, XModelObject object) {
-        for (int i = 0; i < diagramObjects.length; i++) {
-            XModelObject cr = getItemOnDiagram(diagramObjects[i], object);
-            if(cr != null) return cr;
-        }
-    	return null;
-    }
+	private static XModelObject getItemOnDiagram(XModelObject[] diagramObjects, XModelObject object) {
+		for (int i = 0; i < diagramObjects.length; i++) {
+			XModelObject cr = getItemOnDiagram(diagramObjects[i], object);
+			if(cr != null) return cr;
+		}
+		return null;
+	}
 
 }
-
