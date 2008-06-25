b5d3dce4145bed37c297b52fb4a43815eb182447
Alexey Kazakov <akazakov@exadel.com>
2008-06-25 13:40:26 +0000

    http://jira.jboss.com/jira/browse/JBIDE-2429
    git-svn-id: http://svn.jboss.org/repos/jbosstools/trunk@8940 a97e2381-89e5-4abb-bab3-167db6db766c

--- seam/tests/org.jboss.tools.seam.core.test/src/org/jboss/tools/seam/core/test/SeamValidatorsTest.java
+++ seam/tests/org.jboss.tools.seam.core.test/src/org/jboss/tools/seam/core/test/SeamValidatorsTest.java
@@ -285,7 +285,7 @@ public class SeamValidatorsTest extends AbstractResourceMarkerTest {
 		assertFalse("Problem marker 'Component class does not contain setter for property' not found' not found' not found", number == 0);
 		
 		messages = getMarkersMessage(componentsFile);
-		assertTrue("Problem marker 'Component class does not have a setter or a field for the property' not found", "Class \"StatefulComponent\" of component \"statefulComponent\" does not have a setter or a field for the property \"abc\"".equals(messages[0]));
+		assertEquals("Problem marker 'Component class does not have a setter or a field for the property' not found", "Class \"StatefulComponent\" of component \"statefulComponent\" does not have a setter or a field for the property \"abc\"", messages[0]);
 		
 		lineNumbers = getMarkersNumbersOfLine(componentsFile);
 		
