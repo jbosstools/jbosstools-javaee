1a42bd163af221471e443d11e80515f42f37639e
Sergey Dzmitrovich <sdzmitrovich@gmail.com>
2008-07-08 16:14:04 +0000

    http://jira.jboss.com/jira/browse/JBIDE-2170
    git-svn-id: http://svn.jboss.org/repos/jbosstools/trunk@9095 a97e2381-89e5-4abb-bab3-167db6db766c

--- jsf/tests/org.jboss.tools.jsf.vpe.jsf.test/src/org/jboss/tools/jsf/vpe/jsf/test/jbide/JsfJbide2170Test.java
+++ jsf/tests/org.jboss.tools.jsf.vpe.jsf.test/src/org/jboss/tools/jsf/vpe/jsf/test/jbide/JsfJbide2170Test.java
@@ -30,13 +30,17 @@ import org.mozilla.interfaces.nsIDOMDocumentType;
  */
 public class JsfJbide2170Test extends VpeTest {
 
-	public static final String IMPORT_PROJECT_NAME = "jsfTest";
+	public static final String IMPORT_PROJECT_NAME = "jsfTest"; //$NON-NLS-1$
 
-	private static final String WITHOUT_DOCTYPE_TEST_PAGE_NAME = "JBIDE/2170/there_is_not_doctype.xhtml";
+	private static final String WITHOUT_DOCTYPE_TEST_PAGE_NAME = "JBIDE/2170/there_is_not_doctype.xhtml"; //$NON-NLS-1$
 
-	private static final String WITH_DOCTYPE_TEST_PAGE_NAME = "JBIDE/2170/there_is_doctype.xhtml";
+	private static final String WITH_DOCTYPE_TEST_PAGE_NAME = "JBIDE/2170/there_is_doctype.xhtml"; //$NON-NLS-1$
 
-	private static final String TEMPLATE_DOCTYPE_TEST_PAGE_NAME = "JBIDE/2170/template_doctype_test.xhtml";
+	private static final String TEMPLATE_DOCTYPE_TEST_PAGE_NAME = "JBIDE/2170/template_doctype_test.xhtml"; //$NON-NLS-1$
+
+	private static final String COMPLEX_DOCTYPE_TEST_PAGE_NAME = "JBIDE/2170/complex_doctype_test.xhtml"; //$NON-NLS-1$
+
+	private static final String CORRECT_NAME = "html3"; //$NON-NLS-1$
 
 	public JsfJbide2170Test(String name) {
 		super(name);
@@ -177,4 +181,51 @@ public class JsfJbide2170Test extends VpeTest {
 		}
 
 	}
+
+	/**
+	 * 
+	 * @throws Throwable
+	 */
+	public void testComplexDoctypePage() throws Throwable {
+
+		// wait
+		TestUtil.waitForJobs();
+		// set exception
+		setException(null);
+
+		// get test page path
+		IFile file = (IFile) TestUtil.getComponentPath(
+				COMPLEX_DOCTYPE_TEST_PAGE_NAME, IMPORT_PROJECT_NAME);
+
+		IEditorInput input = new FileEditorInput(file);
+
+		// open and get editor
+		JSPMultiPageEditor part = openEditor(input);
+
+		// get doctype from opened page
+		String basicDoctypeString = DocTypeUtil.getDoctype(input);
+
+		// doctype must be not null
+		assertNotNull(basicDoctypeString);
+		// length of doctype must be more than 0
+		assertEquals(true, basicDoctypeString.length() > 0);
+
+		// get dom document
+		nsIDOMDocument document = getVpeVisualDocument(part);
+		assertNotNull(document);
+
+		// get doctype
+		nsIDOMDocumentType vpeDocumentType = document.getDoctype();
+
+		// doctype must be not null
+		assertNotNull(vpeDocumentType);
+
+		assertEquals(CORRECT_NAME, vpeDocumentType.getName());
+		// check exception
+		if (getException() != null) {
+			throw getException();
+		}
+
+	}
+
 }
