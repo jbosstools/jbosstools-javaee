05a55695521e8cecbdb62267951250d625446d03
Viacheslav Kabanovich <scabanovich@exadel.com>
2008-08-13 15:41:01 +0000

    JBIDE-2611
    git-svn-id: http://svn.jboss.org/repos/jbosstools/trunk@9704 a97e2381-89e5-4abb-bab3-167db6db766c

--- seam/plugins/org.jboss.tools.seam.pages.xml/src/org/jboss/tools/seam/pages/xml/model/helpers/autolayout/SeamPagesItems.java
+++ seam/plugins/org.jboss.tools.seam.pages.xml/src/org/jboss/tools/seam/pages/xml/model/helpers/autolayout/SeamPagesItems.java
@@ -1,7 +1,24 @@
 package org.jboss.tools.seam.pages.xml.model.helpers.autolayout;
 
 import org.jboss.tools.jst.web.model.helpers.autolayout.Items;
+import org.jboss.tools.jst.web.model.helpers.autolayout.LayuotConstants;
 
 public class SeamPagesItems extends Items {
+
+	public boolean isZigzagging() {
+		return false;
+	}
+
+    protected LayuotConstants createConstants() {
+    	return new SeamLayoutConstants();
+    }
+
+}
+
+class SeamLayoutConstants extends LayuotConstants {
+
+	public void update() {
+		deltaX = 320;
+	}
 
 }
