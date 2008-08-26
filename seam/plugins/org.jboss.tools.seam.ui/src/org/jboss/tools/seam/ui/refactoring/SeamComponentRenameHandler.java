6f13062eb9f8d94023d696c07c40133e9010f2c5
Alexey Kazakov <akazakov@exadel.com>
2008-08-26 15:36:02 +0000

    https://jira.jboss.org/jira/browse/JBIDE-1077
    git-svn-id: http://svn.jboss.org/repos/jbosstools/trunk@9906 a97e2381-89e5-4abb-bab3-167db6db766c

--- seam/plugins/org.jboss.tools.seam.ui/src/org/jboss/tools/seam/ui/refactoring/SeamComponentRenameHandler.java
+++ seam/plugins/org.jboss.tools.seam.ui/src/org/jboss/tools/seam/ui/refactoring/SeamComponentRenameHandler.java
@@ -10,33 +10,45 @@
   ******************************************************************************/
 package org.jboss.tools.seam.ui.refactoring;
 
+import java.util.Set;
+
 import org.eclipse.core.commands.AbstractHandler;
 import org.eclipse.core.commands.ExecutionEvent;
 import org.eclipse.core.commands.ExecutionException;
-import org.eclipse.jface.viewers.ISelection;
+import org.eclipse.core.resources.IFile;
+import org.eclipse.core.resources.IProject;
+import org.eclipse.ui.IEditorInput;
 import org.eclipse.ui.IEditorPart;
-import org.eclipse.ui.IWorkbenchPart;
 import org.eclipse.ui.handlers.HandlerUtil;
+import org.jboss.tools.seam.core.ISeamComponent;
+import org.jboss.tools.seam.core.ISeamProject;
+import org.jboss.tools.seam.core.SeamCorePlugin;
 
 /**
  * @author Alexey Kazakov
  *
  */
-public class SeamComponentRefactoringHandler extends AbstractHandler {
+public class SeamComponentRenameHandler extends AbstractHandler {
 
 	/* (non-Javadoc)
 	 * @see org.eclipse.core.commands.IHandler#execute(org.eclipse.core.commands.ExecutionEvent)
 	 */
 	public Object execute(ExecutionEvent event) throws ExecutionException {
-		IWorkbenchPart part = HandlerUtil.getActivePart(event);
 		IEditorPart editor = HandlerUtil.getActiveEditor(event);
-		ISelection selection = HandlerUtil.getCurrentSelection(event);
 
-//		Object context = event.getApplicationContext();
-//		if (context instanceof IEvaluationContext) {
-//			IEvaluationContext appContext = (IEvaluationContext) context;
-//			
-//		}
+		IEditorInput input = editor.getEditorInput();
+		if(input instanceof IFile) {
+			IFile file = (IFile)input;
+			IProject project = file.getProject();
+			ISeamProject seamProject = SeamCorePlugin.getSeamProject(project, true);
+			if(seamProject!=null) {
+				Set<ISeamComponent> components = seamProject.getComponentsByPath(file.getFullPath());
+				if(components.size() > 0) {
+					ISeamComponent component = components.iterator().next();
+					// TODO this is a component which we want to rename.
+				}
+			}
+		}
 		return null;
 	}
 }
