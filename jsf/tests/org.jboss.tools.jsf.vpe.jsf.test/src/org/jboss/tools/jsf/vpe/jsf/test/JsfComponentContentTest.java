b3aa9d264d103452c250b13dcd0cebf15e7c225a
Sergey Dzmitrovich <sdzmitrovich@gmail.com>
2008-09-18 14:51:55 +0000

    https://jira.jboss.org/jira/browse/JBIDE-2781
    git-svn-id: http://svn.jboss.org/repos/jbosstools/trunk@10359 a97e2381-89e5-4abb-bab3-167db6db766c

--- jsf/tests/org.jboss.tools.jsf.vpe.jsf.test/src/org/jboss/tools/jsf/vpe/jsf/test/JsfComponentContentTest.java
+++ jsf/tests/org.jboss.tools.jsf.vpe.jsf.test/src/org/jboss/tools/jsf/vpe/jsf/test/JsfComponentContentTest.java
@@ -10,10 +10,6 @@
  ******************************************************************************/
 package org.jboss.tools.jsf.vpe.jsf.test;
 
-import java.io.File;
-import java.util.HashMap;
-import java.util.Map;
-
 import org.jboss.tools.vpe.ui.test.ComponentContentTest;
 
 /**
@@ -24,134 +20,106 @@ import org.jboss.tools.vpe.ui.test.ComponentContentTest;
  */
 public class JsfComponentContentTest extends ComponentContentTest {
 
-	// import project name
-	public static final String IMPORT_PROJECT_NAME = "jsfTest"; //$NON-NLS-1$
-
-	public static final String TESTS_ROOT_NAME = "tests"; //$NON-NLS-1$
-
 	public JsfComponentContentTest(String name) {
 		super(name);
 		setCheckWarning(false);
 	}
 
 	public void testCommandButton() throws Throwable {
-
-		Map<String, String> mapIds = new HashMap<String, String>();
-		mapIds.put("commandButton1", "commandButton1");
-		mapIds.put("commandButton2", "commandButton2");
-		mapIds.put("commandButton3", "commandButton3");
-		mapIds.put("commandButton4", "commandButton4");
-		mapIds.put("commandButton5", "commandButton5");
-		performContentTest("components/commandButton.jsp", //$NON-NLS-1$ 
-				"commandButton.xml", mapIds); //$NON-NLS-1$
+		performContentTest("components/commandButton.jsp"); //$NON-NLS-1$
 	}
 
 	public void testCommandLink() throws Throwable {
-		Map<String, String> mapIds = new HashMap<String, String>();
-		mapIds.put("commandLink1", "commandLink1");
-		mapIds.put("commandLink2", "commandLink2");
-		performContentTest("components/commandLink.jsp", //$NON-NLS-1$ 
-				"commandLink.xml", mapIds); //$NON-NLS-1$
+		performContentTest("components/commandLink.jsp"); //$NON-NLS-1$
 	}
 
 	public void testDataTable() throws Throwable {
-		performContentTest("components/dataTable.jsp", "dataTable",
-				"dataTable.xml");
+		performContentTest("components/dataTable.jsp"); //$NON-NLS-1$
 	}
 
 	public void testForm() throws Throwable {
-		performContentTest("components/form.jsp", "form", "form.xml");
+		performContentTest("components/form.jsp"); //$NON-NLS-1$
 	}
 
 	public void testGraphicImage() throws Throwable {
-		Map<String, String> mapIds = new HashMap<String, String>();
-		mapIds.put("graphicImage1", "graphicImage1");
-		mapIds.put("graphicImage2", "graphicImage2");
-		performContentTest("components/graphicImage.jsp", //$NON-NLS-1$ 
-				"graphicImage.xml", mapIds); //$NON-NLS-1$
+		performContentTest("components/graphicImage.jsp"); //$NON-NLS-1$
 	}
 
 	public void testInputHidden() throws Throwable {
-		assertTrue("it is necessery to add body of the test", false);
+		assertTrue("it is necessery to add body of the test", false); //$NON-NLS-1$
 	}
 
 	public void testInputSecret() throws Throwable {
-		performContentTest("components/inputSecret.jsp", "inputSecret",
-				"inputSecret.xml");
+		performContentTest("components/inputSecret.jsp"); //$NON-NLS-1$
 	}
 
 	public void testInputText() throws Throwable {
-
-		performContentTest("components/inputText.jsp", "inputText", //$NON-NLS-1$ //$NON-NLS-2$
-				"inputText.xml"); //$NON-NLS-1$
+		performContentTest("components/inputText.jsp"); //$NON-NLS-1$
 
 	}
 
 	public void testInputTextArea() throws Throwable {
-		performContentTest("components/inputTextArea.jsp", "inputTextArea", //$NON-NLS-1$ //$NON-NLS-2$
-				"inputTextArea.xml"); //$NON-NLS-1$
+		performContentTest("components/inputTextArea.jsp"); //$NON-NLS-1$
 	}
 
 	public void testMessage() throws Throwable {
-		performContentTest("components/message.jsp", "message", //$NON-NLS-1$ //$NON-NLS-2$
-				"message.xml"); //$NON-NLS-1$
+		performContentTest("components/message.jsp"); //$NON-NLS-1$
 	}
 
 	public void testMessages() throws Throwable {
-		performContentTest("components/messages.jsp", "messages", //$NON-NLS-1$ //$NON-NLS-2$
-		"messages.xml"); //$NON-NLS-1$
+		performContentTest("components/messages.jsp"); //$NON-NLS-1$
 	}
 
 	public void testOutputFormat() throws Throwable {
-		assertTrue("it is necessery to add body of the test", false);
+		assertTrue("it is necessery to add body of the test", false); //$NON-NLS-1$
 	}
 
 	public void testOutputLabel() throws Throwable {
-		assertTrue("it is necessery to add body of the test", false);
+		assertTrue("it is necessery to add body of the test", false); //$NON-NLS-1$
 	}
 
 	public void testOutputLink() throws Throwable {
-		assertTrue("it is necessery to add body of the test", false);
+		assertTrue("it is necessery to add body of the test", false); //$NON-NLS-1$
 	}
 
 	public void testOutputText() throws Throwable {
-		assertTrue("it is necessery to add body of the test", false);
+		assertTrue("it is necessery to add body of the test", false); //$NON-NLS-1$
 	}
 
 	public void testPanelGrid() throws Throwable {
-		assertTrue("it is necessery to add body of the test", false);
+		assertTrue("it is necessery to add body of the test", false); //$NON-NLS-1$
 	}
 
 	public void testPanelGroup() throws Throwable {
-		assertTrue("it is necessery to add body of the test", false);
+		assertTrue("it is necessery to add body of the test", false); //$NON-NLS-1$
 	}
 
 	public void testSelectBooleanCheckbox() throws Throwable {
-		assertTrue("it is necessery to add body of the test", false);
+		assertTrue("it is necessery to add body of the test", false); //$NON-NLS-1$
 	}
 
 	public void testSelectManyCheckbox() throws Throwable {
-		assertTrue("it is necessery to add body of the test", false);
+		assertTrue("it is necessery to add body of the test", false); //$NON-NLS-1$
 	}
 
 	public void testSelectManyListbox() throws Throwable {
-		assertTrue("it is necessery to add body of the test", false);
+		assertTrue("it is necessery to add body of the test", false); //$NON-NLS-1$
 	}
 
 	public void testSelectManyMenu() throws Throwable {
-		assertTrue("it is necessery to add body of the test", false);
+		assertTrue("it is necessery to add body of the test", false); //$NON-NLS-1$
 	}
 
 	public void testSelectOneListbox() throws Throwable {
-		assertTrue("it is necessery to add body of the test", false);
+		assertTrue("it is necessery to add body of the test", false); //$NON-NLS-1$
 	}
 
 	public void testSelectOneMenu() throws Throwable {
-		assertTrue("it is necessery to add body of the test", false);
+		assertTrue("it is necessery to add body of the test", false); //$NON-NLS-1$
 	}
 
 	public void _testSelectOneRadio() throws Throwable {
-		assertTrue("it is necessery to add body of the test", false);
+		assertTrue("it is necessery to add body of the test", false); //$NON-NLS-1$
 	}
 
 	/*
@@ -159,92 +127,87 @@ public class JsfComponentContentTest extends ComponentContentTest {
 	 */
 
 	public void testActionListener() throws Throwable {
-		assertTrue("it is necessery to add body of the test", false);
+		assertTrue("it is necessery to add body of the test", false); //$NON-NLS-1$
 	}
 
 	public void testAttribute() throws Throwable {
-		assertTrue("it is necessery to add body of the test", false);
+		assertTrue("it is necessery to add body of the test", false); //$NON-NLS-1$
 	}
 
 	public void testConvertDateTime() throws Throwable {
-		assertTrue("it is necessery to add body of the test", false);
+		assertTrue("it is necessery to add body of the test", false); //$NON-NLS-1$
 	}
 
 	public void testConvertNumber() throws Throwable {
-		assertTrue("it is necessery to add body of the test", false);
+		assertTrue("it is necessery to add body of the test", false); //$NON-NLS-1$
 	}
 
 	public void testConverter() throws Throwable {
-		assertTrue("it is necessery to add body of the test", false);
+		assertTrue("it is necessery to add body of the test", false); //$NON-NLS-1$
 	}
 
 	public void testFacet() throws Throwable {
-		assertTrue("it is necessery to add body of the test", false);
+		assertTrue("it is necessery to add body of the test", false); //$NON-NLS-1$
 	}
 
 	public void testLoadBundle() throws Throwable {
-		assertTrue("it is necessery to add body of the test", false);
+		assertTrue("it is necessery to add body of the test", false); //$NON-NLS-1$
 	}
 
 	public void testParam() throws Throwable {
-		assertTrue("it is necessery to add body of the test", false);
+		assertTrue("it is necessery to add body of the test", false); //$NON-NLS-1$
 	}
 
 	public void testPhaseListener() throws Throwable {
-		assertTrue("it is necessery to add body of the test", false);
+		assertTrue("it is necessery to add body of the test", false); //$NON-NLS-1$
 	}
 
 	public void testSelectItem() throws Throwable {
-		assertTrue("it is necessery to add body of the test", false);
+		assertTrue("it is necessery to add body of the test", false); //$NON-NLS-1$
 	}
 
 	public void testSelectItems() throws Throwable {
-		assertTrue("it is necessery to add body of the test", false);
+		assertTrue("it is necessery to add body of the test", false); //$NON-NLS-1$
 	}
 
 	public void testSetPropertyActionListener() throws Throwable {
-		assertTrue("it is necessery to add body of the test", false);
+		assertTrue("it is necessery to add body of the test", false); //$NON-NLS-1$
 	}
 
 	public void testSubview() throws Throwable {
-		assertTrue("it is necessery to add body of the test", false);
+		assertTrue("it is necessery to add body of the test", false); //$NON-NLS-1$
 	}
 
 	public void testValidateDoubleRange() throws Throwable {
-		assertTrue("it is necessery to add body of the test", false);
+		assertTrue("it is necessery to add body of the test", false); //$NON-NLS-1$
 	}
 
 	public void testValidateLength() throws Throwable {
-		assertTrue("it is necessery to add body of the test", false);
+		assertTrue("it is necessery to add body of the test", false); //$NON-NLS-1$
 	}
 
 	public void testValidateLongRange() throws Throwable {
-		assertTrue("it is necessery to add body of the test", false);
+		assertTrue("it is necessery to add body of the test", false); //$NON-NLS-1$
 	}
 
 	public void testValidator() throws Throwable {
-		assertTrue("it is necessery to add body of the test", false);
+		assertTrue("it is necessery to add body of the test", false); //$NON-NLS-1$
 	}
 
 	public void testValueChangeListener() throws Throwable {
-		assertTrue("it is necessery to add body of the test", false);
+		assertTrue("it is necessery to add body of the test", false); //$NON-NLS-1$
 	}
 
 	public void testVerbatim() throws Throwable {
-		assertTrue("it is necessery to add body of the test", false);
+		assertTrue("it is necessery to add body of the test", false); //$NON-NLS-1$
 	}
 
 	public void testView() throws Throwable {
-		assertTrue("it is necessery to add body of the test", false);
+		assertTrue("it is necessery to add body of the test", false); //$NON-NLS-1$
 	}
 
 	protected String getTestProjectName() {
-		return IMPORT_PROJECT_NAME;
+		return JsfAllTests.IMPORT_PROJECT_NAME;
 	}
 
-	@Override
-	protected String getTestsRoot() {
-		return JsfTestPlugin.getPluginResourcePath() + File.separator
-				+ TESTS_ROOT_NAME;
-	}
 }
