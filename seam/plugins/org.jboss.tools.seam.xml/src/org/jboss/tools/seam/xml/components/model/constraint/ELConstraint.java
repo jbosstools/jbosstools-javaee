9e8c8908cf8c2b7d21319ba43ca6ca49b1766f71
Viacheslav Kabanovich <scabanovich@exadel.com>
2008-10-07 15:56:50 +0000

    JBIDE-2855
    git-svn-id: http://svn.jboss.org/repos/jbosstools/trunk@10715 a97e2381-89e5-4abb-bab3-167db6db766c

---
+++ seam/plugins/org.jboss.tools.seam.xml/src/org/jboss/tools/seam/xml/components/model/constraint/ELConstraint.java
@@ -0,0 +1,40 @@
+package org.jboss.tools.seam.xml.components.model.constraint;
+
+import org.jboss.tools.common.meta.constraint.XAttributeConstraint;
+import org.jboss.tools.common.meta.constraint.impl.XAttributeConstraintImpl;
+
+public class ELConstraint extends XAttributeConstraintImpl {
+	public static ELConstraint INSTANCE = new ELConstraint();
+
+	public static XAttributeConstraint getInstance() {
+		return INSTANCE;
+	}
+
+    public boolean accepts(String value) {
+    	if(value != null) {
+    		if(value.startsWith("#{") && value.endsWith("}")) {
+    			return true;
+    		}
+    		if(value.length() >= 2 && value.startsWith("@") && value.endsWith("@")) {
+    			return true;
+    		}
+    	}
+        return false;
+    }
+
+    /**
+     * Returns not null only if value starts with EL tokens.
+     */
+    public String getError(String value) {
+    	if(accepts(value)) return null;
+    	if(value.startsWith("#{")) {
+    		return "value is not a correct EL."; 
+    	}
+    	if(value.startsWith("@")) {
+    		return "value is not a correct property."; 
+    	}
+    	
+        return super.getError(value);
+    }
+
+}
---
+++ seam/plugins/org.jboss.tools.seam.xml/src/org/jboss/tools/seam/xml/components/model/constraint/ListELConstraint.java
@@ -0,0 +1,23 @@
+package org.jboss.tools.seam.xml.components.model.constraint;
+
+import org.jboss.tools.common.meta.constraint.impl.XAttributeConstraintList;
+
+public class ListELConstraint extends XAttributeConstraintList {
+
+    public boolean accepts(String value) {
+    	if(ELConstraint.getInstance().accepts(value)) {
+    		return true;
+    	}
+        return super.accepts(value);
+    }
+
+    public String getError(String value) {
+    	if(accepts(value)) return null;
+    	String error = ELConstraint.getInstance().getError(value);
+    	if(error != null) {
+    		return error;
+    	}
+        return super.getError(value);
+    }
+
+}
