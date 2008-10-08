02a1584e7d5c8fa56faff9c51ce841248c0c2768
Denis Golovin <dgolovin@exadel.com>
2008-10-08 17:35:10 +0000

    Package name changed for jst.text.ext tests to exclude from coverage report
    git-svn-id: http://svn.jboss.org/repos/jbosstools/trunk@10752 a97e2381-89e5-4abb-bab3-167db6db766c

--- jsf/tests/org.jboss.tools.jsf.text.ext.test/src/org/jboss/tools/jsf/text/ext/test/ELExprPartitionerTest.java
+++ jsf/tests/org.jboss.tools.jsf.text.ext.test/src/org/jboss/tools/jsf/text/ext/test/ELExprPartitionerTest.java
@@ -8,7 +8,7 @@
  * Contributors:
  *     Exadel, Inc. and Red Hat, Inc. - initial API and implementation
  ******************************************************************************/ 
-package org.jboss.tools.jsf.text.ext.tests;
+package org.jboss.tools.jsf.text.ext.test;
 
 import java.util.ArrayList;
 import java.util.HashMap;
