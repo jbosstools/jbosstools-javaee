2eed7b4c958ea01d5014ef28f31a609b585b890a
Maxim Areshkau <mareshkau@exadel.com>
2008-11-03 14:01:32 +0000

    Junit test for JBIDE-2979
    git-svn-id: http://svn.jboss.org/repos/jbosstools/trunk@11488 a97e2381-89e5-4abb-bab3-167db6db766c

--- jsf/tests/org.jboss.tools.jsf.vpe.jsf.test/src/org/jboss/tools/jsf/vpe/jsf/test/JsfAllTests.java
+++ jsf/tests/org.jboss.tools.jsf.vpe.jsf.test/src/org/jboss/tools/jsf/vpe/jsf/test/JsfAllTests.java
@@ -39,6 +39,7 @@ import org.jboss.tools.jsf.vpe.jsf.test.jbide.JBIDE2594Test;
 import org.jboss.tools.jsf.vpe.jsf.test.jbide.JBIDE2624Test;
 import org.jboss.tools.jsf.vpe.jsf.test.jbide.JBIDE2774Test;
 import org.jboss.tools.jsf.vpe.jsf.test.jbide.JBIDE2828Test;
+import org.jboss.tools.jsf.vpe.jsf.test.jbide.JBIDE2979Test;
 import org.jboss.tools.jsf.vpe.jsf.test.jbide.JBIDE3030Test;
 import org.jboss.tools.jsf.vpe.jsf.test.jbide.JBIDE675Test;
 import org.jboss.tools.jsf.vpe.jsf.test.jbide.JBIDE788Test;
@@ -105,6 +106,7 @@ public class JsfAllTests {
 		suite.addTestSuite(JBIDE2774Test.class);
 		suite.addTestSuite(JBIDE2828Test.class);
 		suite.addTestSuite(JBIDE3030Test.class);
+		suite.addTestSuite(JBIDE2979Test.class);
 		// $JUnit-END$
 		// added by Max Areshkau
 		// add here projects which should be imported for junit tests
