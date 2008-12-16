c3fc0327c75878a3d7e0b4c3f18fd9a243bca704
Maxim Areshkau <mareshkau@exadel.com>
2008-12-16 17:30:32 +0000

    https://jira.jboss.org/jira/browse/JBIDE-3396, junit test
    git-svn-id: http://svn.jboss.org/repos/jbosstools/trunk@12627 a97e2381-89e5-4abb-bab3-167db6db766c

--- jsf/tests/org.jboss.tools.jsf.vpe.jsf.test/src/org/jboss/tools/jsf/vpe/jsf/test/JsfAllTests.java
+++ jsf/tests/org.jboss.tools.jsf.vpe.jsf.test/src/org/jboss/tools/jsf/vpe/jsf/test/JsfAllTests.java
@@ -45,6 +45,7 @@ import org.jboss.tools.jsf.vpe.jsf.test.jbide.JBIDE3127Test;
 import org.jboss.tools.jsf.vpe.jsf.test.jbide.JBIDE3144Test;
 import org.jboss.tools.jsf.vpe.jsf.test.jbide.JBIDE3163Test;
 import org.jboss.tools.jsf.vpe.jsf.test.jbide.JBIDE3376Test;
+import org.jboss.tools.jsf.vpe.jsf.test.jbide.JBIDE3396Test;
 import org.jboss.tools.jsf.vpe.jsf.test.jbide.JBIDE675Test;
 import org.jboss.tools.jsf.vpe.jsf.test.jbide.JBIDE788Test;
 import org.jboss.tools.jsf.vpe.jsf.test.jbide.JBIDE924Test;
@@ -115,6 +116,7 @@ public class JsfAllTests {
 		suite.addTestSuite(JBIDE2354Test.class);
 		suite.addTestSuite(JBIDE3163Test.class);
 		suite.addTestSuite(JBIDE3376Test.class);
+		suite.addTestSuite(JBIDE3396Test.class);
 		
 		// $JUnit-END$
 		// added by Max Areshkau
