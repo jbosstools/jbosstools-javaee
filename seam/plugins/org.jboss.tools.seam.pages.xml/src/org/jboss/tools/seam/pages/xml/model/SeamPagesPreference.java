2ccb5410a7c6d972b60c6e547c232a1a198a7374
Viacheslav Kabanovich <scabanovich@exadel.com>
2008-12-16 11:15:52 +0000

    JBIDE-3409
    git-svn-id: http://svn.jboss.org/repos/jbosstools/trunk@12613 a97e2381-89e5-4abb-bab3-167db6db766c

---
+++ seam/plugins/org.jboss.tools.seam.pages.xml/src/org/jboss/tools/seam/pages/xml/model/SeamPagesPreference.java
@@ -0,0 +1,17 @@
+package org.jboss.tools.seam.pages.xml.model;
+
+import org.jboss.tools.common.model.options.Preference;
+import org.jboss.tools.jst.web.WebPreference;
+
+public class SeamPagesPreference extends WebPreference {
+	public static String SEAM_PAGES_EDITOR_PATH = "%Options%/Struts Studio/Editors/Seam Pages Diagram";
+
+	public static final Preference ENABLE_CONTROL_MODE_ON_TRANSITION_COMPLETED = new SeamPagesPreference(SEAM_PAGES_EDITOR_PATH, "enableControlModeOnTransitionCompleted");
+	public static final Preference SHOW_SHORTCUT_ICON = new SeamPagesPreference(SEAM_PAGES_EDITOR_PATH, "showShortcutIcon");
+	public static final Preference SHOW_SHORTCUT_PATH = new SeamPagesPreference(SEAM_PAGES_EDITOR_PATH, "showShortcutPath");
+
+	protected SeamPagesPreference(String optionPath, String attributeName) {
+		super(optionPath, attributeName);
+	}
+
+}
