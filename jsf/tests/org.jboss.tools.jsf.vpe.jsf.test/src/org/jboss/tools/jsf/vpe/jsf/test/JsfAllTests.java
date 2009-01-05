ba90f1ff5c367a7301de93ef0eed12eee1319b4d
Yahor Radtsevich <yradtsevich@exadel.com>
2009-01-05 17:49:39 +0000

    CODING IN PROGRESS - issue JBIDE-3441: VPE - Preferences - change size of VE pane - press OK - Preview will show empty page. https://jira.jboss.org/jira/browse/JBIDE-3441
    JUnit test of JBIDE-3441 has been moved to the project org.jboss.tools.jsf.vpe.jsf.test .
    
    git-svn-id: http://svn.jboss.org/repos/jbosstools/trunk@12885 a97e2381-89e5-4abb-bab3-167db6db766c

--- jsf/tests/org.jboss.tools.jsf.vpe.jsf.test/src/org/jboss/tools/jsf/vpe/jsf/test/JsfAllTests.java
+++ jsf/tests/org.jboss.tools.jsf.vpe.jsf.test/src/org/jboss/tools/jsf/vpe/jsf/test/JsfAllTests.java
@@ -46,6 +46,7 @@ import org.jboss.tools.jsf.vpe.jsf.test.jbide.JBIDE3144Test;
 import org.jboss.tools.jsf.vpe.jsf.test.jbide.JBIDE3163Test;
 import org.jboss.tools.jsf.vpe.jsf.test.jbide.JBIDE3376Test;
 import org.jboss.tools.jsf.vpe.jsf.test.jbide.JBIDE3396Test;
+import org.jboss.tools.jsf.vpe.jsf.test.jbide.JBIDE3441Test;
 import org.jboss.tools.jsf.vpe.jsf.test.jbide.JBIDE675Test;
 import org.jboss.tools.jsf.vpe.jsf.test.jbide.JBIDE788Test;
 import org.jboss.tools.jsf.vpe.jsf.test.jbide.JBIDE924Test;
@@ -76,6 +77,7 @@ public class JsfAllTests {
 		// $JUnit-BEGIN$
 		suite.addTestSuite(JsfComponentTest.class);
 		suite.addTestSuite(Jsf20ComponentContentTest.class);
+		suite.addTestSuite(JBIDE3441Test.class);
 		suite.addTestSuite(JsfJbide1467Test.class);
 		suite.addTestSuite(JsfJbide1501Test.class);
 		suite.addTestSuite(JBIDE1484Test.class);
