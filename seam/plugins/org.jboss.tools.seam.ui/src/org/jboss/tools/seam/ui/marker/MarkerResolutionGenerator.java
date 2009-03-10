68989b302c6b45ec33ed9698b2a15d2ec9efbe47
Snjezana Peco <snjezana.peco@redhat.com>
2009-03-10 21:58:14 +0000

    JBIDE-3979 Seam markers have invalid type
    git-svn-id: http://svn.jboss.org/repos/jbosstools/trunk@14160 a97e2381-89e5-4abb-bab3-167db6db766c

--- seam/plugins/org.jboss.tools.seam.ui/src/org/jboss/tools/seam/ui/marker/MarkerResolutionGenerator.java
+++ seam/plugins/org.jboss.tools.seam.ui/src/org/jboss/tools/seam/ui/marker/MarkerResolutionGenerator.java
@@ -3,11 +3,23 @@ package org.jboss.tools.seam.ui.marker;
 import org.eclipse.core.resources.IMarker;
 import org.eclipse.ui.IMarkerResolution;
 import org.eclipse.ui.IMarkerResolutionGenerator2;
+import org.jboss.tools.seam.internal.core.validation.SeamProjectPropertyValidator;
 
 public class MarkerResolutionGenerator implements IMarkerResolutionGenerator2 {
 
+	/**  Copied from org.eclipse.wst.validation.internal.TaskListUtility */
+	private static final String VALIDATION_MARKER_MESSAGEID = "messageId"; //$NON-NLS-1$
+	
+
 	public boolean hasResolutions(IMarker marker) {
-		return true;
+		if (marker == null) {
+			return false;
+		}
+		String runtime = marker.getAttribute(VALIDATION_MARKER_MESSAGEID, "");
+		if (SeamProjectPropertyValidator.INVALID_SEAM_RUNTIME.equals(runtime)) {
+			return true;
+		}
+		return false;
 	}
 
 	public IMarkerResolution[] getResolutions(IMarker marker) {
