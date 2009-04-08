36972b34db60c7def5360288e392cac69c9d38c5
Denis Maliarevich <dmaliarevich@exadel.com>
2009-04-08 15:21:35 +0000

    https://jira.jboss.org/jira/browse/JBIDE-4109, style and styleClass attributes were added.
    git-svn-id: http://svn.jboss.org/repos/jbosstools/trunk@14608 a97e2381-89e5-4abb-bab3-167db6db766c

--- jsf/plugins/org.jboss.tools.jsf.vpe.seam/src/org/jboss/tools/jsf/vpe/seam/template/SeamDecorateTemplate.java
+++ jsf/plugins/org.jboss.tools.jsf.vpe.seam/src/org/jboss/tools/jsf/vpe/seam/template/SeamDecorateTemplate.java
@@ -1,20 +1,46 @@
 package org.jboss.tools.jsf.vpe.seam.template;
 
+import org.jboss.tools.jsf.vpe.jsf.template.util.ComponentUtil;
+import org.jboss.tools.jsf.vpe.seam.template.util.Seam;
 import org.jboss.tools.vpe.editor.context.VpePageContext;
 import org.jboss.tools.vpe.editor.template.VpeCreationData;
 import org.jboss.tools.vpe.editor.template.VpeDefineContainerTemplate;
+import org.jboss.tools.vpe.editor.util.HTML;
 import org.mozilla.interfaces.nsIDOMDocument;
+import org.mozilla.interfaces.nsIDOMElement;
 import org.w3c.dom.Element;
 import org.w3c.dom.Node;
 
 public class SeamDecorateTemplate extends VpeDefineContainerTemplate {
 
-    private final String ATTR_TEMPLATE = "template"; //$NON-NLS-1$
-    
     public VpeCreationData create(VpePageContext pageContext, Node sourceNode,
 	    nsIDOMDocument visualDocument) {
-	String fileName = ((Element) sourceNode).getAttribute(ATTR_TEMPLATE);
-	return createTemplate(fileName, pageContext, sourceNode, visualDocument);
+	
+	Element sourceElement = (Element) sourceNode;
+	
+	/*
+	 * Reading attributes
+	 */
+	String fileName = sourceElement.getAttribute(Seam.ATTR_TEMPLATE);
+	String styleClass = sourceElement.getAttribute(Seam.ATTR_STYLE_CLASS);
+	String style = sourceElement.getAttribute(HTML.ATTR_STYLE);
+	
+	/*
+	 * Creating template
+	 */
+	VpeCreationData creationData = createTemplate(fileName, pageContext, sourceNode, visualDocument);
+	
+	/*
+	 * Setting style and class attributes
+	 */
+	if (ComponentUtil.isNotBlank(style)) {
+	    ((nsIDOMElement)creationData.getNode()).setAttribute(HTML.ATTR_STYLE, style);
+	}
+	if (ComponentUtil.isNotBlank(styleClass)) {
+	    ((nsIDOMElement)creationData.getNode()).setAttribute(HTML.ATTR_CLASS, styleClass);
+	}
+	
+	return creationData;
     }
 
 }
