eb829fc9915fe6fb38c5335b5ae06f759bd6bc92
Daniel Azarov <daniel@exadel.com>
2009-04-09 12:32:58 +0000

    https://jira.jboss.org/jira/browse/JBIDE-1077
    git-svn-id: http://svn.jboss.org/repos/jbosstools/trunk@14631 a97e2381-89e5-4abb-bab3-167db6db766c

--- seam/plugins/org.jboss.tools.seam.core/src/org/jboss/tools/seam/internal/core/refactoring/RenameComponentProcessor.java
+++ seam/plugins/org.jboss.tools.seam.core/src/org/jboss/tools/seam/internal/core/refactoring/RenameComponentProcessor.java
@@ -322,21 +322,7 @@ public class RenameComponentProcessor extends RenameProcessor {
 	}
 
 	private void scanProperties(IFile file, String content){
-		//System.out.println("ScanProperties "+file.getName());
-//		try{
-//			FileInputStream fis = new FileInputStream(new File(file.getLocationURI()));
-//			PropertyResourceBundle rb = new PropertyResourceBundle(fis);
-//			for(String key : rb.keySet()){
-//				//System.out.println("Key - "+key+" Val - "+rb.getString(key));
-//				String value = rb.getString(key);
-//				if(value.indexOf('{')>-1)
-//					scanString(file, value, 0);
-//			}
-//		}catch(FileNotFoundException ex){
-//			SeamCorePlugin.getDefault().logError(ex);
-//		}catch(IOException ex){
-//			SeamCorePlugin.getDefault().logError(ex);
-//		}
+		scanString(file, content, 0);
 	}
 
 	/*
