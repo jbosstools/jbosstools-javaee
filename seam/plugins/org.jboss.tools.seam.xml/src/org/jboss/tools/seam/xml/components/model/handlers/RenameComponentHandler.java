fdc338ebea3813945713e29439de802fd3457a55
Viacheslav Kabanovich <scabanovich@exadel.com>
2009-04-23 13:12:04 +0000

    https://jira.jboss.org/jira/browse/JBIDE-1077
    git-svn-id: http://svn.jboss.org/repos/jbosstools/trunk@14870 a97e2381-89e5-4abb-bab3-167db6db766c

---
+++ seam/plugins/org.jboss.tools.seam.xml/src/org/jboss/tools/seam/xml/components/model/handlers/RenameComponentHandler.java
@@ -0,0 +1,28 @@
+package org.jboss.tools.seam.xml.components.model.handlers;
+
+import java.util.Properties;
+
+import org.jboss.tools.common.meta.action.SpecialWizard;
+import org.jboss.tools.common.meta.action.SpecialWizardFactory;
+import org.jboss.tools.common.meta.action.impl.AbstractHandler;
+import org.jboss.tools.common.model.XModelException;
+import org.jboss.tools.common.model.XModelObject;
+
+public class RenameComponentHandler extends AbstractHandler {
+
+	public RenameComponentHandler() {
+	}
+
+	public boolean isEnabled(XModelObject object) {
+		return object != null && object.isObjectEditable();
+	}
+
+	public void executeHandler(XModelObject object, Properties p) throws XModelException {
+		if(!isEnabled(object)) return;
+		
+		SpecialWizard wizard = SpecialWizardFactory.createSpecialWizard("org.jboss.tools.seam.ui.views.actions.RenameComponentAction");
+		wizard.setObject(object);
+		wizard.execute();
+	}
+
+}
