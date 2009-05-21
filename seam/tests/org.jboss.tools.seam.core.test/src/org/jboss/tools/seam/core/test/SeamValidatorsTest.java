88cd2abd8f31efa708faf74a953e149dbe8e1f75
Alexey Kazakov <akazakov@exadel.com>
2009-05-21 13:49:14 +0000

    https://jira.jboss.org/jira/browse/JBIDE-4351
    git-svn-id: http://svn.jboss.org/repos/jbosstools/trunk@15387 a97e2381-89e5-4abb-bab3-167db6db766c

--- seam/tests/org.jboss.tools.seam.core.test/src/org/jboss/tools/seam/core/test/SeamValidatorsTest.java
+++ seam/tests/org.jboss.tools.seam.core.test/src/org/jboss/tools/seam/core/test/SeamValidatorsTest.java
@@ -638,7 +638,7 @@ public class SeamValidatorsTest extends AbstractResourceMarkerTest {
 		
 		assertEquals("Not all problem markers 'Duplicate variable name' was found", 2, messages.length);
 		
-		for(int i=0;i<4;i++)
+		for(int i=0;i<2;i++)
 			assertEquals("Problem marker 'Duplicate factory name' not found", "Duplicate factory name: \"messageList\"", messages[i]);
 		
 		int[] lineNumbers = getMarkersNumbersOfLine(contextVariableTestFile);
