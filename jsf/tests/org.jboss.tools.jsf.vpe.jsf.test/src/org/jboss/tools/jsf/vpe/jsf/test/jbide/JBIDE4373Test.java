593dd4953445b807ffae9694e618c08ece4d2e0c
Maxim Areshkau <mareshkau@exadel.com>
2009-06-10 13:05:32 +0000

    revert changes which was in r15848, uncomment failed junit tests
    git-svn-id: http://svn.jboss.org/repos/jbosstools/trunk@15855 a97e2381-89e5-4abb-bab3-167db6db766c

--- jsf/tests/org.jboss.tools.jsf.vpe.jsf.test/src/org/jboss/tools/jsf/vpe/jsf/test/jbide/JBIDE4373Test.java
+++ jsf/tests/org.jboss.tools.jsf.vpe.jsf.test/src/org/jboss/tools/jsf/vpe/jsf/test/jbide/JBIDE4373Test.java
@@ -40,8 +40,7 @@ public class JBIDE4373Test extends VpeTest{
 	/**
 	 * OpenOn test for Custom elements
 	 */
-	//TODO Maksim Areshkau, uncoment when JBIDE-4213 will be fixed 
-	public void _testCorrectCustomElements() throws CoreException {
+	public void testCorrectCustomElements() throws CoreException {
 
 		VpeController vpeController =	openInVpe(JsfAllTests.IMPORT_CUSTOM_FACELETS_PROJECT, "correctCustomTags.xhtml"); //$NON-NLS-1$
 		int position = TestUtil.getLinePositionOffcet(vpeController.getSourceEditor().getTextViewer(), 13, 8);
@@ -62,8 +61,7 @@ public class JBIDE4373Test extends VpeTest{
 	/**
 	 * test openOn for undefined template
 	 */
-	//TODO Maksim Areshkau, uncoment when JBIDE-4213 will be fixed 
-	public void _testIncorrectCustomElements() throws CoreException {
+	public void testIncorrectCustomElements() throws CoreException {
 		VpeController vpeController =	openInVpe(JsfAllTests.IMPORT_CUSTOM_FACELETS_PROJECT, "incorrectCustomTags.xhtml"); //$NON-NLS-1$
 		int position = TestUtil.getLinePositionOffcet(vpeController.getSourceEditor().getTextViewer(), 9, 6);
 		
