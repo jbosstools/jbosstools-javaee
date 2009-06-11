0e6d93851b6f3f2327185d067450e9e490e5e687
Viacheslav Kabanovich <scabanovich@exadel.com>
2009-06-11 16:27:01 +0000

    https://jira.jboss.org/jira/browse/JBIDE-4464
    git-svn-id: http://svn.jboss.org/repos/jbosstools/trunk@15880 a97e2381-89e5-4abb-bab3-167db6db766c

---
+++ jsf/plugins/org.jboss.tools.jsf/src/org/jboss/tools/jsf/model/pv/handler/CreateBeanRedirectHandler.java
@@ -0,0 +1,44 @@
+/*******************************************************************************
+ * Copyright (c) 2007 Red Hat, Inc.
+ * Distributed under license by Red Hat, Inc. All rights reserved.
+ * This program is made available under the terms of the
+ * Eclipse Public License v1.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributor:
+ *     Red Hat, Inc. - initial API and implementation
+ ******************************************************************************/
+package org.jboss.tools.jsf.model.pv.handler;
+
+import org.jboss.tools.common.meta.action.impl.handlers.DefaultRedirectHandler;
+import org.jboss.tools.common.model.XModel;
+import org.jboss.tools.common.model.XModelObject;
+import org.jboss.tools.jsf.model.JSFConstants;
+import org.jboss.tools.jsf.model.pv.JSFProjectsTree;
+import org.jboss.tools.jst.web.model.pv.WebProjectNode;
+
+/**
+ * 
+ * @author Viacheslav Kabanovich
+ *
+ */
+public class CreateBeanRedirectHandler extends DefaultRedirectHandler {
+
+	protected XModelObject getTrueSource(XModelObject source) {
+		if(source == null) return null;
+		XModel model = source.getModel();
+		WebProjectNode r = JSFProjectsTree.getProjectsRoot(model);
+		if(r == null) return null;
+		WebProjectNode n = (WebProjectNode)r.getChildByPath("Configuration");
+		if(n == null) return null;
+		XModelObject[] fs = n.getTreeChildren();
+		for (XModelObject f: fs) {
+			String entity = f.getModelEntity().getName();
+			if(!entity.startsWith(JSFConstants.ENT_FACESCONFIG)) continue;
+			if(!f.isObjectEditable()) continue;
+			return f.getChildByPath("Managed Beans");
+		}
+		return null;
+	}
+
+}
