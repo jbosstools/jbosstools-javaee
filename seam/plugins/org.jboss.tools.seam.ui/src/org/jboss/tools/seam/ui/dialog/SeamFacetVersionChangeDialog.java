4db61bc64f94ba708b0f63da4ba83b0ed05b156b
Nick Boldt <nboldt@redhat.com>
2009-06-19 18:04:07 +0000

    https://jira.jboss.org/jira/browse/JBIDE-4488 switch to "JBoss by Red Hat" and update (c) to 2009 as appropriate
    git-svn-id: http://svn.jboss.org/repos/jbosstools/trunk@16082 a97e2381-89e5-4abb-bab3-167db6db766c

--- seam/plugins/org.jboss.tools.seam.ui/src/org/jboss/tools/seam/ui/dialog/SeamFacetVersionChangeDialog.java
+++ seam/plugins/org.jboss.tools.seam.ui/src/org/jboss/tools/seam/ui/dialog/SeamFacetVersionChangeDialog.java
@@ -1,12 +1,12 @@
 /*************************************************************************************
- * Copyright (c) 2008 JBoss, a division of Red Hat and others.
+ * Copyright (c) 2008-2009 JBoss by Red Hat and others.
  * All rights reserved. This program and the accompanying materials 
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
  * http://www.eclipse.org/legal/epl-v10.html
  * 
  * Contributors:
- *     JBoss, a division of Red Hat - Initial implementation.
+ *     JBoss by Red Hat - Initial implementation.
  ************************************************************************************/
 package org.jboss.tools.seam.ui.dialog;
 
