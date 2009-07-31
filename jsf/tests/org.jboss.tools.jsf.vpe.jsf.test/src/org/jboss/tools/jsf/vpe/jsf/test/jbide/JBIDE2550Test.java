69e882fc4fec598fe18b6102bd6679a010e65396
Maxim Areshkau <mareshkau@exadel.com>
2009-07-31 13:02:46 +0000

    https://jira.jboss.org/jira/browse/JBIDE-2550, processing from jar files resources was added
    git-svn-id: http://svn.jboss.org/repos/jbosstools/trunk@16928 a97e2381-89e5-4abb-bab3-167db6db766c

--- jsf/tests/org.jboss.tools.jsf.vpe.jsf.test/src/org/jboss/tools/jsf/vpe/jsf/test/jbide/JBIDE2550Test.java
+++ jsf/tests/org.jboss.tools.jsf.vpe.jsf.test/src/org/jboss/tools/jsf/vpe/jsf/test/jbide/JBIDE2550Test.java
@@ -26,7 +26,7 @@ public class JBIDE2550Test extends ComponentContentTest {
 	public void testJBIDE2550TestResourceFromWebRoot() throws Throwable{
 		performContentTest("JBIDE/2550/jbide2550.xhtml"); //$NON-NLS-1$
 	}
-
+	
 	@Override
 	protected String getTestProjectName() {
 		return JsfAllTests.IMPORT_JSF_20_PROJECT_NAME;
