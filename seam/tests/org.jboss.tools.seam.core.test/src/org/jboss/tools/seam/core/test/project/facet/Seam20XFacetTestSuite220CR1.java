75cb5602bb494f879062d38b6b61b03f6162f0fa
Denis Golovin <dgolovin@exadel.com>
2009-08-10 17:11:28 +0000

    tests suites for seam 2.2.0.CR1 added
    git-svn-id: http://svn.jboss.org/repos/jbosstools/branches/jbosstools-3.0.x@17037 a97e2381-89e5-4abb-bab3-167db6db766c

---
+++ seam/tests/org.jboss.tools.seam.core.test/src/org/jboss/tools/seam/core/test/project/facet/Seam20XFacetTestSuite220CR1.java
@@ -0,0 +1,5 @@
+package org.jboss.tools.seam.core.test.project.facet;
+
+public class Seam20XFacetTestSuite220CR1 extends Seam20XFacetTestSuite201GA {
+
+}
---
+++ seam/tests/org.jboss.tools.seam.ui.test/src/org/jboss/tools/seam/ui/test/wizard/Seam20XOperationsTestSuite220CR1.java
@@ -0,0 +1,5 @@
+package org.jboss.tools.seam.ui.test.wizard;
+
+public class Seam20XOperationsTestSuite220CR1 extends
+		Seam20XOperationsTestSuite201GA {
+}
