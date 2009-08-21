d0f9ae34630917aad2116de6404439c5497793a4
Yahor Radtsevich <yradtsevich@exadel.com>
2009-08-21 12:54:34 +0000

    JBIDE-1460: https://jira.jboss.org/jira/browse/JBIDE-1460 - JUnit fixed
    git-svn-id: http://svn.jboss.org/repos/jbosstools/trunk@17218 a97e2381-89e5-4abb-bab3-167db6db766c

--- jsf/tests/org.jboss.tools.jsf.vpe.jsf.test/src/org/jboss/tools/jsf/vpe/jsf/test/jbide/JBIDE1460Test.java
+++ jsf/tests/org.jboss.tools.jsf.vpe.jsf.test/src/org/jboss/tools/jsf/vpe/jsf/test/jbide/JBIDE1460Test.java
@@ -10,81 +10,32 @@
  ******************************************************************************/
 package org.jboss.tools.jsf.vpe.jsf.test.jbide;
 
-import java.util.ArrayList;
-import java.util.List;
-
-import org.eclipse.core.resources.IFile;
-import org.eclipse.ui.IEditorInput;
-import org.eclipse.ui.part.FileEditorInput;
 import org.jboss.tools.jsf.vpe.jsf.test.JsfAllTests;
-import org.jboss.tools.jst.jsp.jspeditor.JSPMultiPageEditor;
-import org.jboss.tools.vpe.editor.util.HTML;
-import org.jboss.tools.vpe.ui.test.TestUtil;
-import org.jboss.tools.vpe.ui.test.VpeTest;
-import org.mozilla.interfaces.nsIDOMDocument;
-import org.mozilla.interfaces.nsIDOMElement;
-import org.mozilla.interfaces.nsIDOMNode;
+import org.jboss.tools.vpe.ui.test.ComponentContentTest;
 
 /**
  * Test JBIDE-1460
  * 
  * @author dsakovich@exadel.com
+ * @author yradtsevich
  * 
  */
-public class JBIDE1460Test extends VpeTest {
-
-    private static final String TEST_PAGE_NAME = "JBIDE/1460/JBIDE-1460.xhtml"; //$NON-NLS-1$
-
+public class JBIDE1460Test extends ComponentContentTest {
     public JBIDE1460Test(String name) {
-	super(name);
+    	super(name);
     }
 
     // test method for JBIDE 1460
     public void testJBIDE_1460() throws Throwable {
-	// wait
-	TestUtil.waitForJobs();
-	// set exception
-	setException(null);
-
-	// get test page path
-	IFile file = (IFile) TestUtil.getComponentPath(TEST_PAGE_NAME,
-			JsfAllTests.IMPORT_PROJECT_NAME);
-
-	assertNotNull("Could not open specified file. componentPage = " + TEST_PAGE_NAME//$NON-NLS-1$ 
-			+ ";projectName = " + JsfAllTests.IMPORT_PROJECT_NAME, file);//$NON-NLS-1$
-
-	IEditorInput input = new FileEditorInput(file);
-
-	assertNotNull("Editor input is null", input); //$NON-NLS-1$
-	// open and get editor
-	JSPMultiPageEditor part = openEditor(input);
-
-	// get dom document
-	nsIDOMDocument document = TestUtil.getVpeVisualDocument(part);
-	nsIDOMElement element = document.getDocumentElement();
-
-	// check that element is not null
-	assertNotNull(element);
-
-	// get root node
-	nsIDOMNode node = (nsIDOMNode) element
-		.queryInterface(nsIDOMNode.NS_IDOMNODE_IID);
-
-	List<nsIDOMNode> elements = new ArrayList<nsIDOMNode>();
-
-	// find "span" elements
-	TestUtil.findElementsByName(node, elements, HTML.TAG_SPAN);
-
-	assertEquals(3, elements.size());
-
-	nsIDOMNode span = elements.get(0);
-
-	nsIDOMNode text = span.getFirstChild();
-	assertEquals(text.getNodeValue().trim(), "#{item.nodes}"); //$NON-NLS-1$
-
-	if (getException() != null) {
-	    throw getException();
-	}
+    	performContentTest( "JBIDE/1460/JBIDE-1460.xhtml"); //$NON-NLS-1$
     }
+
+	/* (non-Javadoc)
+	 * @see org.jboss.tools.vpe.ui.test.ComponentContentTest#getTestProjectName()
+	 */
+	@Override
+	protected String getTestProjectName() {
+		return JsfAllTests.IMPORT_PROJECT_NAME;
+	}
 
 }
