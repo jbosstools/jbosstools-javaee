c7d678d27433af51d016231999ac89dff9d6ffc1
Alexey Kazakov <akazakov@exadel.com>
2009-09-14 19:49:56 +0000

    Share project "org.jboss.tools.cdi.core.test" into "https://svn.jboss.org/repos/jbosstools"
    git-svn-id: http://svn.jboss.org/repos/jbosstools/trunk@17562 a97e2381-89e5-4abb-bab3-167db6db766c

---
+++ cdi/tests/org.jboss.tools.cdi.core.test/src/org/jboss/tools/cdi/core/test/CDICoreAllTests.java
@@ -0,0 +1,33 @@
+/******************************************************************************* 
+ * Copyright (c) 2009 Red Hat, Inc. 
+ * Distributed under license by Red Hat, Inc. All rights reserved. 
+ * This program is made available under the terms of the 
+ * Eclipse Public License v1.0 which accompanies this distribution, 
+ * and is available at http://www.eclipse.org/legal/epl-v10.html 
+ * 
+ * Contributors: 
+ * Red Hat, Inc. - initial API and implementation 
+ ******************************************************************************/ 
+package org.jboss.tools.cdi.core.test;
+
+import junit.framework.Test;
+import junit.framework.TestSuite;
+
+import org.jboss.tools.tests.AbstractPluginsLoadTest;
+
+/**
+ * @author Alexey Kazakov
+ */
+public class CDICoreAllTests {
+
+	public static Test suite() {
+		TestSuite suite = new TestSuite("CDI Core Tests");
+		return suite;
+	}
+
+	public class CDIPluginsLoadTest extends AbstractPluginsLoadTest {
+		public void testBundlesAreLoadedForSeamFeature(){
+			testBundlesAreLoadedFor("org.jboss.tools.cdi.feature");
+		}
+	}
+}
