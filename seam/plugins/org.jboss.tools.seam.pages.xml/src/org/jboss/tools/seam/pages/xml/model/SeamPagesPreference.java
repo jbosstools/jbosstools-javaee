cbe6e39fc2c32d290f9764f14aa9a5d3daf1be89
Viacheslav Kabanovich <scabanovich@exadel.com>
2009-09-17 12:14:52 +0000

    https://jira.jboss.org/jira/browse/JBIDE-4896 https://jira.jboss.org/jira/browse/JBIDE-4885
    git-svn-id: http://svn.jboss.org/repos/jbosstools/trunk@17632 a97e2381-89e5-4abb-bab3-167db6db766c

--- seam/plugins/org.jboss.tools.seam.pages.xml/src/org/jboss/tools/seam/pages/xml/model/SeamPagesPreference.java
+++ seam/plugins/org.jboss.tools.seam.pages.xml/src/org/jboss/tools/seam/pages/xml/model/SeamPagesPreference.java
@@ -5,7 +5,7 @@ import org.jboss.tools.common.model.options.Preference;
 import org.jboss.tools.jst.web.WebPreference;
 
 public class SeamPagesPreference extends WebPreference {
-	public static String SEAM_PAGES_EDITOR_PATH = "%Options%/Struts Studio/Editors/Seam Pages Diagram";
+	public static String SEAM_PAGES_EDITOR_PATH = Preference.EDITOR_PATH + "/Seam Pages Diagram";
 
 	public static final Preference ENABLE_CONTROL_MODE_ON_TRANSITION_COMPLETED = new SeamPagesPreference(SEAM_PAGES_EDITOR_PATH, "enableControlModeOnTransitionCompleted");
 	public static final Preference SHOW_SHORTCUT_ICON = new SeamPagesPreference(SEAM_PAGES_EDITOR_PATH, "showShortcutIcon");
