ddc25e7c66fe581029987cbef2253b3bdd5686a4
Viacheslav Kabanovich <scabanovich@exadel.com>
2009-10-09 10:37:30 +0000

    https://jira.jboss.org/jira/browse/JBIDE-4999
    git-svn-id: http://svn.jboss.org/repos/jbosstools/trunk@18001 a97e2381-89e5-4abb-bab3-167db6db766c

--- jsf/plugins/org.jboss.tools.jsf/src/org/jboss/tools/jsf/model/pv/handler/JSFProjectsRootRedirectHandler.java
+++ jsf/plugins/org.jboss.tools.jsf/src/org/jboss/tools/jsf/model/pv/handler/JSFProjectsRootRedirectHandler.java
@@ -10,8 +10,14 @@
  ******************************************************************************/ 
 package org.jboss.tools.jsf.model.pv.handler;
 
+import java.util.Properties;
+
+import org.jboss.tools.common.meta.action.SpecialWizard;
 import org.jboss.tools.common.meta.action.impl.handlers.*;
+import org.jboss.tools.common.model.XModelException;
 import org.jboss.tools.common.model.XModelObject;
+import org.jboss.tools.common.model.handlers.RemoveModelNatureHandler;
+import org.jboss.tools.common.model.util.ModelFeatureFactory;
 
 public class JSFProjectsRootRedirectHandler extends DefaultRedirectHandler {
 
@@ -19,4 +25,13 @@ public class JSFProjectsRootRedirectHandler extends DefaultRedirectHandler {
 		return source.getModel().getByPath("FileSystems");
 	}
 
+    public void executeHandler(XModelObject object, Properties p) throws XModelException {
+    	String c = action.getProperty(RemoveModelNatureHandler.PARAM_CONTRIBUTION);
+    	if(c != null) {
+    		SpecialWizard w = (SpecialWizard)ModelFeatureFactory.getInstance().createFeatureInstance(c);
+    		if(p == null) p = new Properties();
+    		if(w != null) p.put(RemoveModelNatureHandler.PARAM_CONTRIBUTION, w);
+    	}
+    	super.executeHandler(object, p);
+    }
 }
