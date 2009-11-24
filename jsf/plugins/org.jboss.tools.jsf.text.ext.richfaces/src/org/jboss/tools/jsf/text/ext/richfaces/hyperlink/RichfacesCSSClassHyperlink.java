a04bdacd38be62356f8ecf0ba9091bb616054c25
Victor Rubezhny <ruby@exadel.com>
2009-11-24 14:01:57 +0000

    JBIDE-5237: Update existing CSS Class Hyperlink for "class" attribute of HTML-tags to search thru CSS Stylesheets loaded using JSF tags like <a:loadStyle />
    JBIDE-5149: Make OpenOn work for styleClass="|"
    
    Issues are fixed
    
    git-svn-id: http://svn.jboss.org/repos/jbosstools/trunk@18798 a97e2381-89e5-4abb-bab3-167db6db766c

--- jsf/plugins/org.jboss.tools.jsf.text.ext.richfaces/src/org/jboss/tools/jsf/text/ext/richfaces/hyperlink/RichfacesCSSClassHyperlink.java
+++ jsf/plugins/org.jboss.tools.jsf.text.ext.richfaces/src/org/jboss/tools/jsf/text/ext/richfaces/hyperlink/RichfacesCSSClassHyperlink.java
@@ -1,53 +1,38 @@
+/*******************************************************************************
+ * Copyright (c) 2009 Exadel, Inc. and Red Hat, Inc.
+ * Distributed under license by Red Hat, Inc. All rights reserved.
+ * This program is made available under the terms of the
+ * Eclipse Public License v1.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors:
+ *     Exadel, Inc. and Red Hat, Inc. - initial API and implementation
+ ******************************************************************************/
+
 package org.jboss.tools.jsf.text.ext.richfaces.hyperlink;
 
 import java.text.MessageFormat;
 import java.util.List;
 
-import org.eclipse.jface.text.IDocument;
 import org.eclipse.jface.text.IRegion;
 import org.eclipse.jface.text.Region;
-import org.eclipse.wst.sse.core.StructuredModelManager;
-import org.eclipse.wst.sse.core.internal.provisional.IModelManager;
-import org.eclipse.wst.sse.core.internal.provisional.IStructuredModel;
 import org.eclipse.wst.sse.core.internal.provisional.IndexedRegion;
+import org.jboss.tools.common.el.core.resolver.ELContext;
 import org.jboss.tools.common.text.ext.hyperlink.xpl.Messages;
 import org.jboss.tools.jst.text.ext.hyperlink.CSSClassHyperlink;
 import org.jboss.tools.jst.web.kb.ICSSContainerSupport;
-import org.jboss.tools.jst.web.kb.IPageContext;
 import org.jboss.tools.jst.web.kb.PageContextFactory;
 import org.jboss.tools.jst.web.kb.PageContextFactory.CSSStyleSheetDescriptor;
 import org.w3c.dom.css.CSSRule;
 import org.w3c.dom.css.CSSRuleList;
 
+/**
+ * 
+ * @author Victor Rubezhny
+ * 
+ */
+@SuppressWarnings("restriction")
 public class RichfacesCSSClassHyperlink  extends CSSClassHyperlink {
-
-	@Override
-	protected void doHyperlink(IRegion region) {
-		ICSSContainerSupport cssContainerSupport = null;
-		IPageContext context = PageContextFactory.createPageContext(getFile(), region.getOffset(), getContentType(getDocument()));
-		if (!(context instanceof ICSSContainerSupport)) {
-			openFileFailed();
-			return;
-		}
-		cssContainerSupport = (ICSSContainerSupport)context;
-		List<CSSStyleSheetDescriptor> descrs = cssContainerSupport.getCSSStyleSheetDescriptors();
-
-		for (int i = (descrs == null) ? -1 : descrs.size() - 1; descrs != null && i >= 0; i--) {
-			CSSStyleSheetDescriptor descr = descrs.get(i);
-			CSSRuleList rules = descr.sheet.getCssRules();
-			for (int r = 0; rules != null && r < rules.getLength(); r++) {
-				if (isRuleMatch(rules.item(r), getStyleName(region))) {
-					CSSRule rule = rules.item(r);
-					System.out.println();
-					showRegion(
-							PageContextFactory.getFileFromProject(descr.source, getFile()), 
-							new Region(((IndexedRegion)rule).getStartOffset(), ((IndexedRegion)rule).getLength()));
-					return;
-				}
-			}
-		}
-	}
-
 	/*
 	 * (non-Javadoc)
 	 * 
@@ -59,32 +44,5 @@ public class RichfacesCSSClassHyperlink  extends CSSClassHyperlink {
 			return MessageFormat.format(Messages.OpenA, Messages.CSSStyle);
 
 		return MessageFormat.format(Messages.OpenCSSStyle, styleName);
-	}
-
-	
-	/**
-	 * Returns the content type of document
-	 * 
-	 * @param document -
-	 *            assumes document is not null
-	 * @return String content type of given document
-	 */
-	@SuppressWarnings("restriction")
-	private String getContentType(IDocument document) {
-		String type = null;
-		
-		IModelManager mgr = StructuredModelManager.getModelManager();
-		IStructuredModel model = null;
-		try {
-			model = mgr.getExistingModelForRead(document);
-			if (model != null) {
-				type = model.getContentTypeIdentifier();
-			}
-		} finally {
-			if (model != null) {
-				model.releaseFromRead();
-			}
-		}
-		return type;
 	}
 }
--- jsf/plugins/org.jboss.tools.jsf.text.ext.richfaces/src/org/jboss/tools/jsf/text/ext/richfaces/hyperlink/RichfacesCSSClassHyperlinkPartitioner.java
+++ jsf/plugins/org.jboss.tools.jsf.text.ext.richfaces/src/org/jboss/tools/jsf/text/ext/richfaces/hyperlink/RichfacesCSSClassHyperlinkPartitioner.java
@@ -1,9 +1,25 @@
+/*******************************************************************************
+ * Copyright (c) 2009 Exadel, Inc. and Red Hat, Inc.
+ * Distributed under license by Red Hat, Inc. All rights reserved.
+ * This program is made available under the terms of the
+ * Eclipse Public License v1.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors:
+ *     Exadel, Inc. and Red Hat, Inc. - initial API and implementation
+ ******************************************************************************/
+
 package org.jboss.tools.jsf.text.ext.richfaces.hyperlink;
 
 import org.eclipse.jface.text.IDocument;
 import org.jboss.tools.common.text.ext.hyperlink.IHyperlinkRegion;
 import org.jboss.tools.jst.text.ext.hyperlink.CSSClassHyperlinkPartitioner;
 
+/**
+ * 
+ * @author Victor Rubezhny
+ *
+ */
 public class RichfacesCSSClassHyperlinkPartitioner extends CSSClassHyperlinkPartitioner {
 	public static final String RICHFACES_CSS_CLASS_PARTITION = "org.jboss.tools.common.text.ext.RICHFACES_CSS_CLASS"; //$NON-NLS-1$
 	private static final String RICHFACES_CSS_CLASS_TOKEN = "/styleClass/"; //$NON-NLS-1$
