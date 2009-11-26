becf341cfb9f76c83f52a9de5a0487825b193f72
Victor Rubezhny <ruby@exadel.com>
2009-11-26 17:59:36 +0000

    JBIDE-5149: Make OpenOn work for styleClass="|"
    The CSS-file search and an start position calculation are fixed
    
    git-svn-id: http://svn.jboss.org/repos/jbosstools/trunk@18867 a97e2381-89e5-4abb-bab3-167db6db766c

--- jsf/plugins/org.jboss.tools.jsf.text.ext.richfaces/src/org/jboss/tools/jsf/text/ext/richfaces/hyperlink/RichfacesCSSClassHyperlink.java
+++ jsf/plugins/org.jboss.tools.jsf.text.ext.richfaces/src/org/jboss/tools/jsf/text/ext/richfaces/hyperlink/RichfacesCSSClassHyperlink.java
@@ -12,26 +12,15 @@
 package org.jboss.tools.jsf.text.ext.richfaces.hyperlink;
 
 import java.text.MessageFormat;
-import java.util.List;
 
-import org.eclipse.jface.text.IRegion;
-import org.eclipse.jface.text.Region;
-import org.eclipse.wst.sse.core.internal.provisional.IndexedRegion;
-import org.jboss.tools.common.el.core.resolver.ELContext;
 import org.jboss.tools.common.text.ext.hyperlink.xpl.Messages;
 import org.jboss.tools.jst.text.ext.hyperlink.CSSClassHyperlink;
-import org.jboss.tools.jst.web.kb.ICSSContainerSupport;
-import org.jboss.tools.jst.web.kb.PageContextFactory;
-import org.jboss.tools.jst.web.kb.PageContextFactory.CSSStyleSheetDescriptor;
-import org.w3c.dom.css.CSSRule;
-import org.w3c.dom.css.CSSRuleList;
 
 /**
  * 
  * @author Victor Rubezhny
  * 
  */
-@SuppressWarnings("restriction")
 public class RichfacesCSSClassHyperlink  extends CSSClassHyperlink {
 	/*
 	 * (non-Javadoc)
