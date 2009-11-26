802196471b1b24ad0d7a01cf0e6d01ad3a3e0b5b
Denis Golovin <dgolovin@exadel.com>
2009-11-26 00:11:21 +0000

    https://jira.jboss.org/jira/browse/JBIDE-5224 - Some Plugins and Features has wrong or missing License or Copyright text 1. License fixed for all plug-ins. 2. Icon added for source and sdk plugins
    git-svn-id: http://svn.jboss.org/repos/jbosstools/trunk@18843 a97e2381-89e5-4abb-bab3-167db6db766c

---
+++ jsf/plugins/org.jboss.tools.jsf.ui/src/org/jboss/tools/jsf/ui/wizard/project/Messages.java
@@ -0,0 +1,17 @@
+package org.jboss.tools.jsf.ui.wizard.project;
+
+import org.eclipse.osgi.util.NLS;
+
+public class Messages extends NLS {
+	private static final String BUNDLE_NAME = "org.jboss.tools.jsf.ui.wizard.project.messages"; //$NON-NLS-1$
+	public static String ImportProjectFoldersPage_0;
+	public static String NewProjectWizard_0;
+	public static String NewProjectWizard_1;
+	static {
+		// initialize resource bundle
+		NLS.initializeMessages(BUNDLE_NAME, Messages.class);
+	}
+
+	private Messages() {
+	}
+}
