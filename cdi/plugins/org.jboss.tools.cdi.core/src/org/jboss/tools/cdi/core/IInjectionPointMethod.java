cd546b463926aaafa361a07ac2eaf4c4af652646
Alexey Kazakov <akazakov@exadel.com>
2009-12-15 16:39:28 +0000

    https://jira.jboss.org/jira/browse/JBIDE-4943
    git-svn-id: http://svn.jboss.org/repos/jbosstools/trunk@19281 a97e2381-89e5-4abb-bab3-167db6db766c

---
+++ cdi/plugins/org.jboss.tools.cdi.core/src/org/jboss/tools/cdi/core/IInjectionPointMethod.java
@@ -0,0 +1,20 @@
+/******************************************************************************* 
+ * Copyright (c) 2009 Red Hat, Inc. 
+ * Distributed under license by Red Hat, Inc. All rights reserved. 
+ * This program is made available under the terms of the 
+ * Eclipse Public License v1.0 which accompanies this distribution, 
+ * and is available at http://www.eclipse.org/legal/epl-v10.html 
+ * 
+ * Contributors: 
+ * Red Hat, Inc. - initial API and implementation 
+ ******************************************************************************/
+package org.jboss.tools.cdi.core;
+
+/**
+ * Represents an injection point which is a method of a bean class.
+ * 
+ * @author Alexey Kazakov
+ */
+public interface IInjectionPointMethod extends IInjectionPoint, IBeanMethod {
+
+}
