0317e2ec64a6fed005f442ddac7df98e0fda3174
Daniel Azarov <daniel@exadel.com>
2009-12-20 11:06:37 +0000

    https://jira.jboss.org/jira/browse/JBIDE-4758
    git-svn-id: http://svn.jboss.org/repos/jbosstools/trunk@19486 a97e2381-89e5-4abb-bab3-167db6db766c

--- jsf/plugins/org.jboss.tools.jsf.text.ext/src/org/jboss/tools/jsf/text/ext/hyperlink/JSPExprHyperlinkPartitioner.java
+++ jsf/plugins/org.jboss.tools.jsf.text.ext/src/org/jboss/tools/jsf/text/ext/hyperlink/JSPExprHyperlinkPartitioner.java
@@ -10,22 +10,22 @@
  ******************************************************************************/ 
 package org.jboss.tools.jsf.text.ext.hyperlink;
 
+import org.eclipse.core.resources.IFile;
 import org.eclipse.jface.text.IDocument;
-import org.eclipse.wst.xml.core.internal.provisional.document.IDOMAttr;
-import org.eclipse.wst.xml.core.internal.provisional.document.IDOMText;
+import org.jboss.tools.common.el.core.ELReference;
+import org.jboss.tools.common.el.core.model.ELExpression;
+import org.jboss.tools.common.el.core.resolver.ELContext;
 import org.jboss.tools.common.text.ext.hyperlink.AbstractHyperlinkPartitioner;
 import org.jboss.tools.common.text.ext.hyperlink.HyperlinkRegion;
 import org.jboss.tools.common.text.ext.hyperlink.IExclusiblePartitionerRecognition;
 import org.jboss.tools.common.text.ext.hyperlink.IHyperlinkPartitionRecognizer;
 import org.jboss.tools.common.text.ext.hyperlink.IHyperlinkRegion;
 import org.jboss.tools.common.text.ext.util.StructuredModelWrapper;
-import org.jboss.tools.common.text.ext.util.Utils;
 import org.jboss.tools.jst.text.ext.hyperlink.jsp.JSPRootHyperlinkPartitioner;
-import org.w3c.dom.Document;
-import org.w3c.dom.Node;
+import org.jboss.tools.jst.web.kb.PageContextFactory;
 
 /**
- * @author Jeremy
+ * @author Jeremy and Daniel
  */
 @SuppressWarnings("restriction")
 public class JSPExprHyperlinkPartitioner extends AbstractHyperlinkPartitioner implements IHyperlinkPartitionRecognizer, IExclusiblePartitionerRecognition {
@@ -39,28 +39,17 @@ public class JSPExprHyperlinkPartitioner extends AbstractHyperlinkPartitioner im
 	 * @see com.ibm.sse.editor.hyperlink.AbstractHyperlinkPartitioner#parse(org.eclipse.jface.text.IDocument, com.ibm.sse.editor.extensions.hyperlink.IHyperlinkRegion)
 	 */
 	protected IHyperlinkRegion parse(IDocument document, IHyperlinkRegion superRegion) {
-		StructuredModelWrapper smw = new StructuredModelWrapper();
-		smw.init(document);
-		try {
-			Document xmlDocument = smw.getDocument();
-			if (xmlDocument == null) return null;
-			
-			Utils.findNodeForOffset(xmlDocument, superRegion.getOffset());
-			
-			IHyperlinkRegion r = getRegion(document, superRegion.getOffset());
-			if (r == null) return null;
+		IHyperlinkRegion r = getRegion(document, superRegion.getOffset());
+		if (r == null) return null;
 
-			String axis = getAxis(document, superRegion);
-			String contentType = superRegion.getContentType();
-			String type = getPartitionType();
-			int length = r.getLength() - (superRegion.getOffset() - r.getOffset());
-			int offset = superRegion.getOffset();
-			
-			IHyperlinkRegion region = new HyperlinkRegion(offset, length, axis, contentType, type);
-			return region;
-		} finally {
-			smw.dispose();
-		}
+		String axis = getAxis(document, superRegion);
+		String contentType = superRegion.getContentType();
+		String type = getPartitionType();
+		int length = r.getLength() - (superRegion.getOffset() - r.getOffset());
+		int offset = superRegion.getOffset();
+		
+		IHyperlinkRegion region = new HyperlinkRegion(offset, length, axis, contentType, type);
+		return region;
 	}
 	
 	protected String getAxis(IDocument document, IHyperlinkRegion superRegion) {
@@ -70,81 +59,45 @@ public class JSPExprHyperlinkPartitioner extends AbstractHyperlinkPartitioner im
 		return superRegion.getAxis();
 	}
 	
-
 	private IHyperlinkRegion getRegion(IDocument document, final int offset) {
+		ELExpression expression = getExpression(document, offset);
+		if(expression != null){
+			IHyperlinkRegion region = new HyperlinkRegion(expression.getStartPosition(), expression.getLength(), null, null, null);
+			return region;
+		}
+		return null;
+	}
+	
+	public static ELExpression getExpression(IDocument document, final int offset){
 		StructuredModelWrapper smw = new StructuredModelWrapper();
 		smw.init(document);
 		try {
-			Document xmlDocument = smw.getDocument();
-			if (xmlDocument == null) return null;
-			
-			Node n = Utils.findNodeForOffset(xmlDocument, offset);
-
-			if (n == null || !(n instanceof IDOMAttr || n instanceof IDOMText)) return null;
-
-			int valStart = Utils.getValueStart(n);
-			int valEnd = Utils.getValueEnd(n);
-			if(valStart < 0 || valStart > offset) return null;
-
-			String valText = (n instanceof IDOMAttr)? 
-							((IDOMAttr)n).getValueRegionText():
-								((IDOMText)n).getData();
-			if (valText == null) 
-					return null;
-			
-			int startBracket = 0;
-			int exprStart = 0;
-			int exprLength = 0;
-			while (startBracket != -1) {
-				int v = valText.indexOf("#{", startBracket + exprLength); //$NON-NLS-1$
-				if (v == -1) v = valText.indexOf("${", startBracket + exprLength); //$NON-NLS-1$
-				if (v == -1) return null;
-				startBracket = v;
-
-				int endBracket = valText.indexOf("}", startBracket + 2); //$NON-NLS-1$
-				exprStart = valStart + startBracket + 2;
-				int exprEnd = (endBracket == -1 ? valEnd - 1: valStart + endBracket);
-				int lineBreaker = valText.indexOf('\n', startBracket + 2);
-				int lineBreaker1 = valText.indexOf('\r', startBracket + 2);
-				if (lineBreaker != -1 && lineBreaker + valStart < exprEnd) exprEnd = valStart + lineBreaker;
-				if (lineBreaker1 != -1 && lineBreaker1 + valStart < exprEnd) exprEnd =  valStart + lineBreaker1;
-				exprLength = exprEnd - exprStart;
+			IFile file = smw.getFile();
+			ELContext context = PageContextFactory.getInstance().createPageContext(file);
+			if(context != null){
 				
-				if(exprLength<=0) { 
-					return null;
-				} else if (exprStart <= offset && exprEnd >= offset) {
-					int start = exprStart;
-					int length = exprLength;
-	
-					IHyperlinkRegion region = new HyperlinkRegion(start, length, null, null, null);
-					return region;
+				ELReference[] references = context.getELReferences();
+				
+				for(ELReference reference : references){
+					for(ELExpression expression : reference.getEl()){
+						if (reference.getStartPosition()+expression.getStartPosition() <= offset && reference.getStartPosition()+expression.getEndPosition() >= offset)
+							return expression;
+					}
 				}
 			}
 			return null;
 		} finally {
 			smw.dispose();
 		}
-		
 	}
-
 
 	/**
 	 * @see com.ibm.sse.editor.extensions.hyperlink.IHyperlinkPartitionRecognizer#recognize(org.eclipse.jface.text.IDocument, com.ibm.sse.editor.extensions.hyperlink.IHyperlinkRegion)
 	 */
 	public boolean recognize(IDocument document, IHyperlinkRegion region) {
 		if(document == null || region == null) return false;
-		StructuredModelWrapper smw = new StructuredModelWrapper();
-		smw.init(document);
-		try {
-			Document xmlDocument = smw.getDocument();
-			if (xmlDocument == null) return false;
-			
-			Utils.findNodeForOffset(xmlDocument, region.getOffset());
-
-			return (getRegion(document, region.getOffset()) != null);
-		} finally {
-			smw.dispose();
-		}
+		
+		return (getRegion(document, region.getOffset()) != null);
 	}
 
 	public boolean excludes(String partitionType, IDocument document, IHyperlinkRegion superRegion) {
