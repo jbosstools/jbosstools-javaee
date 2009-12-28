054ef16c9c3515c646a59fed8508ebfaed53fe3c
Viacheslav Kabanovich <scabanovich@exadel.com>
2009-12-28 17:16:02 +0000

    https://jira.jboss.org/jira/browse/JBIDE-4943
    git-svn-id: http://svn.jboss.org/repos/jbosstools/trunk@19600 a97e2381-89e5-4abb-bab3-167db6db766c

--- cdi/plugins/org.jboss.tools.cdi.ui/src/org/jboss/tools/cdi/ui/preferences/CDIConfigurationBlock.java
+++ cdi/plugins/org.jboss.tools.cdi.ui/src/org/jboss/tools/cdi/ui/preferences/CDIConfigurationBlock.java
@@ -35,8 +35,74 @@ public class CDIConfigurationBlock extends SeverityConfigurationBlock {
 		CDICorePlugin.PLUGIN_ID
 	);
 
+	private static SectionDescription SECTION_NAME = new SectionDescription(
+		CDIPreferencesMessages.CDIValidatorConfigurationBlock_section_name,
+		new String[][]{
+			{CDIPreferences.STEREOTYPE_DECLARES_NON_EMPTY_NAME, CDIPreferencesMessages.CDIValidatorConfigurationBlock_pb_stereotypeDeclaresNonEmptyName_label},
+			{CDIPreferences.RESOURCE_PRODUCER_FIELD_SETS_EL_NAME, CDIPreferencesMessages.CDIValidatorConfigurationBlock_pb_resourceProducerFieldSetsElName_label},
+			{CDIPreferences.PARAM_INJECTION_DECLARES_EMPTY_NAME, CDIPreferencesMessages.CDIValidatorConfigurationBlock_pb_paramInjectionDeclaresEmptyName_label},
+			{CDIPreferences.INTERCEPTOR_HAS_NAME, CDIPreferencesMessages.CDIValidatorConfigurationBlock_pb_interceptorHasName_label},
+		},
+		CDICorePlugin.PLUGIN_ID
+	);
+
+	private static SectionDescription SECTION_TYPE = new SectionDescription(
+		CDIPreferencesMessages.CDIValidatorConfigurationBlock_section_type,
+		new String[][]{
+			{CDIPreferences.ILLEGAL_TYPE_IN_TYPED_DECLARATION, CDIPreferencesMessages.CDIValidatorConfigurationBlock_pb_illegalTypeInTypedDeclaration_label},
+			{CDIPreferences.PRODUCER_METHOD_RETURN_TYPE_HAS_WILDCARD, CDIPreferencesMessages.CDIValidatorConfigurationBlock_pb_producerMethodReturnTypeHasWildcard_label},
+			{CDIPreferences.PRODUCER_METHOD_RETURN_TYPE_IS_VARIABLE, CDIPreferencesMessages.CDIValidatorConfigurationBlock_pb_producerMethodReturnTypeIsVariable_label},
+			{CDIPreferences.PRODUCER_FIELD_TYPE_HAS_WILDCARD, CDIPreferencesMessages.CDIValidatorConfigurationBlock_pb_producerFieldTypeHasWildcard_label},
+			{CDIPreferences.PRODUCER_FIELD_TYPE_IS_VARIABLE, CDIPreferencesMessages.CDIValidatorConfigurationBlock_pb_producerFieldTypeIsVariable_label},
+			{CDIPreferences.PRODUCER_FIELD_TYPE_DOES_NOT_MATCH_JAVA_EE_OBJECT, CDIPreferencesMessages.CDIValidatorConfigurationBlock_pb_producerFieldTypeDoesNotMatchJavaEeObject_label},
+			{CDIPreferences.INJECTION_TYPE_IS_VARIABLE, CDIPreferencesMessages.CDIValidatorConfigurationBlock_pb_injectionTypeIsVariable_label},
+			{CDIPreferences.STEREOTYPE_IS_ANNOTATED_TYPED, CDIPreferencesMessages.CDIValidatorConfigurationBlock_pb_stereotypeIsAnnotatedTyped_label},
+			{CDIPreferences.MISSING_NONBINDING_IN_QUALIFIER_TYPE_MEMBER, CDIPreferencesMessages.CDIValidatorConfigurationBlock_pb_missingNonbindingInQualifierTypeMember_label},
+			{CDIPreferences.MISSING_NONBINDING_IN_INTERCEPTOR_BINDING_TYPE_MEMBER, CDIPreferencesMessages.CDIValidatorConfigurationBlock_pb_missingNonbindingInInterceptorBindingTypeMember_label},
+		},
+		CDICorePlugin.PLUGIN_ID
+	);
+
+	private static SectionDescription SECTION_SCOPE = new SectionDescription(
+		CDIPreferencesMessages.CDIValidatorConfigurationBlock_section_scope,
+		new String[][]{
+//			{CDIPreferences., CDIPreferencesMessages.CDIValidatorConfigurationBlock_pb_}
+		},
+		CDICorePlugin.PLUGIN_ID
+	);
+
+	private static SectionDescription SECTION_MEMBER = new SectionDescription(
+		CDIPreferencesMessages.CDIValidatorConfigurationBlock_section_member,
+		new String[][]{
+			{CDIPreferences.TEST, CDIPreferencesMessages.CDIValidatorConfigurationBlock_pb_test_label}
+		},
+		CDICorePlugin.PLUGIN_ID
+	);
+
+	private static SectionDescription SECTION_INTERCEPTOR = new SectionDescription(
+		CDIPreferencesMessages.CDIValidatorConfigurationBlock_section_interceptor_and_decorator,
+		new String[][]{
+			{CDIPreferences.TEST, CDIPreferencesMessages.CDIValidatorConfigurationBlock_pb_test_label}
+		},
+		CDICorePlugin.PLUGIN_ID
+	);
+
+	private static SectionDescription SECTION_MISCELLANEOUS = new SectionDescription(
+		CDIPreferencesMessages.CDIValidatorConfigurationBlock_section_miscellaneous,
+		new String[][]{
+			{CDIPreferences.TEST, CDIPreferencesMessages.CDIValidatorConfigurationBlock_pb_test_label}
+		},
+		CDICorePlugin.PLUGIN_ID
+	);
+
 	private static SectionDescription[] ALL_SECTIONS = new SectionDescription[]{
-		SECTION_TEST
+		SECTION_TEST,
+		SECTION_NAME,
+		SECTION_TYPE,
+		SECTION_SCOPE,
+		SECTION_MEMBER,
+		SECTION_INTERCEPTOR,
+		SECTION_MISCELLANEOUS
 	};
 
 	private static Key[] getKeys() {
