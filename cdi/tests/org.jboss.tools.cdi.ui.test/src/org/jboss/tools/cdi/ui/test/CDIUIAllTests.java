c6d88710a495e8dbc032720311fd83bddcadef35
Alexey Kazakov <akazakov@exadel.com>
2009-12-30 19:31:52 +0000

    https://jira.jboss.org/jira/browse/JBIDE-5383
    git-svn-id: http://svn.jboss.org/repos/jbosstools/trunk@19626 a97e2381-89e5-4abb-bab3-167db6db766c

---
+++ cdi/tests/org.jboss.tools.cdi.ui.test/src/org/jboss/tools/cdi/ui/test/CDIUIAllTests.java
@@ -0,0 +1,26 @@
+/******************************************************************************* 
+ * Copyright (c) 2009 Red Hat, Inc. 
+ * Distributed under license by Red Hat, Inc. All rights reserved. 
+ * This program is made available under the terms of the 
+ * Eclipse Public License v1.0 which accompanies this distribution, 
+ * and is available at http://www.eclipse.org/legal/epl-v10.html 
+ * 
+ * Contributors: 
+ * Red Hat, Inc. - initial API and implementation 
+ ******************************************************************************/ 
+package org.jboss.tools.cdi.ui.test;
+
+import junit.framework.Test;
+import junit.framework.TestSuite;
+
+/**
+ * @author Alexey Kazakov
+ */
+public class CDIUIAllTests {
+
+	public static Test suite() {
+		TestSuite suite = new TestSuite("CDI UI Tests");
+		suite.addTestSuite(CATest.class);
+		return suite;
+	}
+}
