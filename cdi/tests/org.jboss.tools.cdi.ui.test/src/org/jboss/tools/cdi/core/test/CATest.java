3e122ef44bb02a1532592ea89bffe4e729a7955b
Alexey Kazakov <akazakov@exadel.com>
2009-12-30 16:52:23 +0000

    https://jira.jboss.org/jira/browse/JBIDE-5383
    git-svn-id: http://svn.jboss.org/repos/jbosstools/trunk@19621 a97e2381-89e5-4abb-bab3-167db6db766c

--- cdi/tests/org.jboss.tools.cdi.ui.test/src/org/jboss/tools/cdi/core/test/CATest.java
+++ cdi/tests/org.jboss.tools.cdi.ui.test/src/org/jboss/tools/cdi/core/test/CATest.java
@@ -10,23 +10,48 @@
  ******************************************************************************/ 
 package org.jboss.tools.cdi.core.test;
 
+import java.io.File;
+
 import org.eclipse.core.resources.IProject;
+import org.eclipse.core.runtime.FileLocator;
+import org.eclipse.core.runtime.Platform;
 import org.jboss.tools.cdi.core.test.tck.TCKTest;
+import org.jboss.tools.common.util.FileUtil;
+import org.jboss.tools.jst.jsp.test.ca.ContentAssistantTestCase;
+import org.osgi.framework.Bundle;
 
 /**
  * @author Alexey Kazakov
  */
 public class CATest extends TCKTest {
 
-	protected void setUp() throws Exception {
-		IProject p = importPreparedProject("/definition/qualifier");
+	private IProject project;
+	private ContentAssistantTestCase caTest = new ContentAssistantTestCase();
+	private static final String PAGE_NAME = "/resources/CATest/test.jsp";
+	private String[] beanProposals = new String[] {"sheep"};
+	private String[] propertyProposals = new String[] {"sheep"};
+	private final static String UI_TEST_PLUGIN_ID = "org.jboss.tools.cdi.ui.test";
+
+	public CATest() {
+		super();
+		try {
+			project = importPreparedProject("/lookup");
+			Bundle core = Platform.getBundle(PLUGIN_ID);
+			Bundle ui = Platform.getBundle(UI_TEST_PLUGIN_ID);
+			String projectPath = FileLocator.resolve(core.getEntry(PROJECT_PATH)).getFile();
+			String resourcePath = FileLocator.resolve(ui.getEntry(PAGE_NAME)).getFile();
+
+			File from = new File(resourcePath);
+			File to = new File(projectPath, "/WebContent");
+
+			FileUtil.copyFile(from, to);
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
 	}
 
-	public void test1() {
-		
-	}
-
-	protected void tearDown() throws Exception {
-		cleanProject("/definition/qualifier");
+	public void testEL() {
+		caTest.checkProposals(PAGE_NAME, "value=\"#{", 0, beanProposals, false);
+		caTest.checkProposals(PAGE_NAME, "rendered=\"#{(game.", 0, propertyProposals, false);
 	}
 }
