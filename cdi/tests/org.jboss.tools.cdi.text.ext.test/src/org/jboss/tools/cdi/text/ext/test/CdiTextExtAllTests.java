16bd47c9d408dae39ecb55472e932e32354b2235
Daniel Azarov <daniel@exadel.com>
2010-01-29 16:42:00 +0000

    https://jira.jboss.org/jira/browse/JBIDE-3125
    git-svn-id: http://svn.jboss.org/repos/jbosstools/trunk@20022 a97e2381-89e5-4abb-bab3-167db6db766c

---
+++ cdi/tests/org.jboss.tools.cdi.text.ext.test/src/org/jboss/tools/cdi/text/ext/test/CdiTextExtAllTests.java
@@ -0,0 +1,9 @@
+package org.jboss.tools.cdi.text.ext.test;
+
+import junit.framework.Test;
+
+public class CdiTextExtAllTests {
+	public static Test suite() {
+		return null;
+	}
+}
---
+++ cdi/tests/org.jboss.tools.cdi.text.ext.test/src/org/jboss/tools/cdi/text/ext/test/InjectedPointHyperlinkDetectorTest.java
@@ -0,0 +1,15 @@
+package org.jboss.tools.cdi.text.ext.test;
+
+import junit.framework.TestCase;
+
+public class InjectedPointHyperlinkDetectorTest  extends TestCase {
+	public void setUp() throws Exception {
+	}
+
+	protected void tearDown() throws Exception {
+	}
+
+	public void testInjectedPointHyperlinkDetector() {
+		
+	}
+}
