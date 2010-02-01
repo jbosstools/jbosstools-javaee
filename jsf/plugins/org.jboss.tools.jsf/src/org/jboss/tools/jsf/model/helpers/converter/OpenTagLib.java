67f25d5a7b80480c361a92f0454215c3c2d6aae9
Viacheslav Kabanovich <scabanovich@exadel.com>
2010-02-01 14:32:51 +0000

    https://jira.jboss.org/jira/browse/JBIDE-5764
    git-svn-id: http://svn.jboss.org/repos/jbosstools/trunk@20050 a97e2381-89e5-4abb-bab3-167db6db766c

--- jsf/plugins/org.jboss.tools.jsf/src/org/jboss/tools/jsf/model/helpers/converter/OpenTagLib.java
+++ jsf/plugins/org.jboss.tools.jsf/src/org/jboss/tools/jsf/model/helpers/converter/OpenTagLib.java
@@ -10,12 +10,18 @@
  ******************************************************************************/ 
 package org.jboss.tools.jsf.model.helpers.converter;
 
+import org.eclipse.core.resources.IProject;
 import org.eclipse.osgi.util.NLS;
 
 import org.jboss.tools.common.model.*;
+import org.jboss.tools.common.model.util.EclipseResourceUtil;
 import org.jboss.tools.common.model.util.FindObjectHelper;
 import org.jboss.tools.jsf.messages.JSFUIMessages;
 import org.jboss.tools.jsf.model.pv.*;
+import org.jboss.tools.jst.web.kb.IKbProject;
+import org.jboss.tools.jst.web.kb.KbProjectFactory;
+import org.jboss.tools.jst.web.kb.internal.KbObject;
+import org.jboss.tools.jst.web.kb.taglib.ITagLibrary;
 import org.jboss.tools.jst.web.model.pv.*;
 
 public class OpenTagLib {
@@ -23,7 +29,7 @@ public class OpenTagLib {
 	public String run(XModel model, String uri, String tag, String attribute) {
 		if(model == null || uri == null) return null;
 		if(uri.length() == 0) return JSFUIMessages.URI_ISNOT_SPECIFIED;
-		XModelObject t = findTagLib(model, uri);
+		XModelObject t = findTagLib(model, uri, tag, attribute);
 		if(t == null) return NLS.bind(JSFUIMessages.CANNOT_FIND_TAG_LIBRARY, uri);
 		FindObjectHelper.findModelObject(t, FindObjectHelper.IN_EDITOR_ONLY);
 		if(tag != null) {
@@ -41,16 +47,52 @@ public class OpenTagLib {
 		return null;
 	}
 	
-	private XModelObject findTagLib(XModel model, String uri) {
+	private XModelObject findTagLib(XModel model, String uri, String tag, String attribute) {
+		XModelObject some = null;
 		JSFProjectsRoot root = JSFProjectsTree.getProjectsRoot(model);
 		if(root == null) return null;
 		WebProjectNode n = (WebProjectNode)root.getChildByPath("Tag Libraries");
 		if(n == null) return null;
 		XModelObject[] os = n.getTreeChildren();
 		for (int i = 0; i < os.length; i++) {
-			if(uri.equals(os[i].getAttributeValue("uri"))) return os[i];
+			if(uri.equals(os[i].getAttributeValue("uri"))) {
+				if(isOk(os[i], tag, attribute)) {
+					return os[i];
+				} else {
+					some = os[i];
+				}
+			}
 		}
-		return null;
+
+		IProject project = EclipseResourceUtil.getProject(root);
+		if(project != null) {
+			IKbProject kb = KbProjectFactory.getKbProject(project, true);
+			if(kb != null) {
+				ITagLibrary[] ls = kb.getTagLibraries(uri);
+				for (int i = 0; i < ls.length; i++) {
+					Object id = ((KbObject)ls[i]).getId();
+					if(id instanceof XModelObject) {
+						XModelObject lib = (XModelObject)id;
+						if(isOk(lib, tag, attribute)) {
+							return lib;
+						}
+					}					
+				}
+			}
+		}
+		return some;
+	}
+
+	private boolean isOk(XModelObject lib, String tag, String attribute) {
+		if(tag != null) {
+			XModelObject t = findTag(lib, tag);
+			if(t == null) return false;
+			if(t != null && attribute != null) {
+				XModelObject a = findAttribute(t, attribute);
+				if(a == null) return false;
+			}
+		}
+		return true;
 	}
 	
 	private XModelObject findTag(XModelObject taglib, String name) {
