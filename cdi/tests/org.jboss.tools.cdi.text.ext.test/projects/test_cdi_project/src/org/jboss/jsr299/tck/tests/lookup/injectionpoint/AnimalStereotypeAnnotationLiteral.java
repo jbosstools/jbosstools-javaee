c11e19878e4c96852acad2e98ed0e97d9fe64d98
Daniel Azarov <daniel@exadel.com>
2010-02-10 18:59:21 +0000

    https://jira.jboss.org/jira/browse/JBIDE-3125
    git-svn-id: http://svn.jboss.org/repos/jbosstools/trunk@20228 a97e2381-89e5-4abb-bab3-167db6db766c

--- cdi/tests/org.jboss.tools.cdi.text.ext.test/projects/test_cdi_project/src/org/jboss/jsr299/tck/tests/lookup/injectionpoint/AnimalStereotypeAnnotationLiteral.java
+++
@@ -1,8 +0,0 @@
-package org.jboss.jsr299.tck.tests.lookup.injectionpoint;
-
-import javax.enterprise.util.AnnotationLiteral;
-
-class AnimalStereotypeAnnotationLiteral extends AnnotationLiteral<AnimalStereotype> implements AnimalStereotype
-{
-
-}
--- cdi/tests/org.jboss.tools.cdi.text.ext.test/projects/test_cdi_project/src/org/jboss/jsr299/tck/tests/lookup/injectionpoint/MyLogger.java
+++
@@ -1,9 +0,0 @@
-package org.jboss.jsr299.tck.tests.lookup.injectionpoint;
-
-@Abc
-public class MyLogger implements Logger {
-
-	public void log(String message) {
-	}
-
-}
