772f3888619b7eb401c2aab1ca0115c18f2fec64
Alexey Kazakov <akazakov@exadel.com>
2010-02-27 00:55:31 +0000

    https://jira.jboss.org/jira/browse/JBIDE-5808
    git-svn-id: http://svn.jboss.org/repos/jbosstools/trunk@20526 a97e2381-89e5-4abb-bab3-167db6db766c

---
+++ cdi/tests/org.jboss.tools.cdi.core.test/resources/tck/tests/context/dependent/SpiderProducer.java
@@ -0,0 +1,75 @@
+/*
+ * JBoss, Home of Professional Open Source
+ * Copyright 2010, Red Hat, Inc., and individual contributors
+ * by the @authors tag. See the copyright.txt in the distribution for a
+ * full listing of individual contributors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * http://www.apache.org/licenses/LICENSE-2.0
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.jboss.jsr299.tck.tests.context.dependent;
+
+import javax.annotation.PreDestroy;
+import javax.enterprise.context.Dependent;
+import javax.enterprise.inject.Disposes;
+import javax.enterprise.inject.Produces;
+import javax.enterprise.inject.spi.BeanManager;
+import javax.inject.Inject;
+
+class SpiderProducer
+{
+   @Inject
+   private BeanManager beanManager;
+   
+   private static boolean dependentContextActive = false;
+   private static boolean destroyed = false;
+   private static SpiderProducer instanceUsedForDisposal = null;
+
+
+   @Produces @Pet public Tarantula produceTarantula(Tarantula spider)
+   {
+      dependentContextActive = beanManager.getContext(Dependent.class).isActive();
+      return spider;
+   }
+
+   public void disposeTarantula(@Disposes @Pet Tarantula tarantula, Fox fox)
+   {
+      dependentContextActive = beanManager.getContext(Dependent.class).isActive();
+      instanceUsedForDisposal = this;
+   }
+
+   public static boolean isDependentContextActive()
+   {
+      return dependentContextActive;
+   }
+
+   public static SpiderProducer getInstanceUsedForDisposal()
+   {
+      return instanceUsedForDisposal;
+   }
+   
+   public static boolean isDestroyed()
+   {
+      return destroyed;
+   }
+
+   public static void reset()
+   {
+     destroyed = false;
+     dependentContextActive = false;
+     instanceUsedForDisposal = null;
+   }
+   
+   @PreDestroy
+   public void destroy()
+   {
+      destroyed = true;
+   }
+}
