6b1cadd6519712f7303f65006f5144798fa74cd3
Daniel Azarov <daniel@exadel.com>
2010-03-09 18:00:30 +0000

    https://jira.jboss.org/jira/browse/JBIDE-5986
    git-svn-id: http://svn.jboss.org/repos/jbosstools/trunk@20712 a97e2381-89e5-4abb-bab3-167db6db766c

--- jsf/plugins/org.jboss.tools.jsf.text.ext.richfaces/src/org/jboss/tools/jsf/text/ext/richfaces/hyperlink/RichfacesCSSClassHyperlinkPartitioner.java
+++ jsf/plugins/org.jboss.tools.jsf.text.ext.richfaces/src/org/jboss/tools/jsf/text/ext/richfaces/hyperlink/RichfacesCSSClassHyperlinkPartitioner.java
@@ -22,13 +22,29 @@ import org.jboss.tools.jst.text.ext.hyperlink.CSSClassHyperlinkPartitioner;
  */
 public class RichfacesCSSClassHyperlinkPartitioner extends CSSClassHyperlinkPartitioner {
 	public static final String RICHFACES_CSS_CLASS_PARTITION = "org.jboss.tools.common.text.ext.RICHFACES_CSS_CLASS"; //$NON-NLS-1$
-	private static final String RICHFACES_CSS_CLASS_TOKEN = "/styleClass/"; //$NON-NLS-1$
+	
+	private static final String[] tokens = new String[]{
+		"/columnClasses/",
+		"/footerClass/",
+		"/headerClass/",
+		"/rowClasses/",
+		"/captionClass/",
+		"/captionStyle/",
+		"/styleClass/",
+		"/errorClass/",
+		"/fatalClass/",
+		"/infoClass/",
+		"/disabledClass/",
+		"/enabledClass/"
+	};
 
 	@Override
 	public boolean recognize(IDocument document, IHyperlinkRegion region) {
-		if (region.getAxis() != null
-				&& region.getAxis().endsWith(RICHFACES_CSS_CLASS_TOKEN))
-			return true;
+		if (region.getAxis() != null){
+			for(String token : tokens)
+				if(region.getAxis().endsWith(token))
+					return true;
+		}
 		return false;
 	}
 
