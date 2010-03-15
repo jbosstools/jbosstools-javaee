71cec55208371ed30996ca4adcdd24eeeae62337
Alexey Kazakov <akazakov@exadel.com>
2010-03-15 16:28:57 +0000

    https://jira.jboss.org/jira/browse/JBIDE-5808
    git-svn-id: http://svn.jboss.org/repos/jbosstools/trunk@20829 a97e2381-89e5-4abb-bab3-167db6db766c

--- cdi/tests/org.jboss.tools.cdi.core.test/src/org/jboss/tools/cdi/core/test/tck/QualifierDefinitionTest.java
+++ cdi/tests/org.jboss.tools.cdi.core.test/src/org/jboss/tools/cdi/core/test/tck/QualifierDefinitionTest.java
@@ -14,6 +14,7 @@ import java.util.Set;
 
 import org.eclipse.jdt.core.JavaModelException;
 import org.jboss.tools.cdi.core.IBean;
+import org.jboss.tools.cdi.core.IInjectionPoint;
 import org.jboss.tools.cdi.core.IQualifier;
 
 /**
@@ -35,5 +36,21 @@ public class QualifierDefinitionTest extends TCKTest {
 		assertEquals("Wrong number of qualifiers for org.jboss.jsr299.tck.tests.definition.qualifier.Order type.", 2, qualifiers.size());
 		assertContainsQualifierType(bean, "javax.enterprise.inject.Default");
 		assertContainsQualifierType(bean, "javax.enterprise.inject.Any");
+	}
+
+	/**
+	 * section 2.3.1, b)
+	 * section 11.1 c)
+	 * @throws JavaModelException 
+	 */
+	public void testDefaultQualifierForInjectionPoint() throws JavaModelException {
+		Set<IBean> beans = getBeans("org.jboss.jsr299.tck.tests.definition.qualifier.Order");
+		assertEquals("Wrong number of beans with org.jboss.jsr299.tck.tests.definition.qualifier.Order type.", 1, beans.size());
+		IBean bean = beans.iterator().next();
+		Set<IInjectionPoint> points = bean.getInjectionPoints();
+		IInjectionPoint point = points.iterator().next();
+		Set<IQualifier> qualifiers = bean.getQualifiers();
+		// TODO do we need to care about default qualifiers for InjectionPoint in CDI Tools?
+//		assertContainsQualifierType(point, "javax.enterprise.inject.Default");
 	}
 }
