8acbfa6078acf699bfa3d1e23f9d0e3a51d5f5ad
Vlado Pakan <vpakan@redhat.com>
2010-03-29 11:26:33 +0000

    Remove project from server after project is created
    git-svn-id: http://svn.jboss.org/repos/jbosstools/trunk@21106 a97e2381-89e5-4abb-bab3-167db6db766c

--- seam/tests/org.jboss.tools.seam.ui.bot.test/src/org/jboss/tools/seam/ui/bot/test/create/CreateSeamProjects.java
+++ seam/tests/org.jboss.tools.seam.ui.bot.test/src/org/jboss/tools/seam/ui/bot/test/create/CreateSeamProjects.java
@@ -1,14 +1,19 @@
 package org.jboss.tools.seam.ui.bot.test.create;
 
 import org.jboss.tools.seam.ui.bot.test.TestControl;
+import org.jboss.tools.ui.bot.ext.SWTJBTExt;
+import org.jboss.tools.ui.bot.ext.SWTUtilExt;
 
 public class CreateSeamProjects extends TestControl{
+  
+  private SWTJBTExt swtJbtExt = new SWTJBTExt(bot);
 	
 	public void testCreateSeamProject12war(){
 		createSeamProject(seam12Settings, jbossEAPRuntime, TYPE_WAR);
 		waitForBlockingJobsAcomplished(240000, BUILDING_WS, VALIDATION + 
 				" " + seam12Settings.getProperty("testProjectName")
 				+TYPE_WAR, DEPLOY_SOURCE, REG_IN_SERVER);
+		swtJbtExt.removeSeamProjectFromServers(seam12Settings.getProperty("testProjectName"));
 	}
 	
 	public void testCreateSeamProject12ear(){
@@ -16,6 +21,7 @@ public class CreateSeamProjects extends TestControl{
 		waitForBlockingJobsAcomplished(240000, BUILDING_WS, VALIDATION + 
 				" " + seam12Settings.getProperty("testProjectName")
 				+TYPE_EAR, DEPLOY_SOURCE, REG_IN_SERVER);
+		swtJbtExt.removeSeamProjectFromServers(seam12Settings.getProperty("testProjectName"));
 	}
 	
 	public void testCreateSeamProject2fpwar(){
@@ -23,6 +29,7 @@ public class CreateSeamProjects extends TestControl{
 		waitForBlockingJobsAcomplished(240000, BUILDING_WS, VALIDATION + 
 				" " + seam2fpSettings.getProperty("testProjectName")
 				+TYPE_WAR, DEPLOY_SOURCE, REG_IN_SERVER);
+		swtJbtExt.removeSeamProjectFromServers(seam2fpSettings.getProperty("testProjectName"));
 	}
 	
 	public void testCreateSeamProject2fpear(){
@@ -30,6 +37,7 @@ public class CreateSeamProjects extends TestControl{
 		waitForBlockingJobsAcomplished(240000, BUILDING_WS, VALIDATION + 
 				" " + seam2fpSettings.getProperty("testProjectName")
 				+TYPE_EAR, DEPLOY_SOURCE, REG_IN_SERVER);
+		swtJbtExt.removeSeamProjectFromServers(seam2fpSettings.getProperty("testProjectName"));
 	}
 	
 	public void testCreateSeamProject22war(){
@@ -37,6 +45,7 @@ public class CreateSeamProjects extends TestControl{
 		waitForBlockingJobsAcomplished(240000, BUILDING_WS, VALIDATION + 
 				" " + seam22Settings.getProperty("testProjectName")
 				+TYPE_WAR, DEPLOY_SOURCE, REG_IN_SERVER);
+		swtJbtExt.removeSeamProjectFromServers(seam22Settings.getProperty("testProjectName"));
 	}
 	
 	public void testCreateSeamProject22ear(){
@@ -44,6 +53,8 @@ public class CreateSeamProjects extends TestControl{
 		waitForBlockingJobsAcomplished(240000, BUILDING_WS, VALIDATION + 
 				" " + seam22Settings.getProperty("testProjectName")
 				+TYPE_EAR, DEPLOY_SOURCE, REG_IN_SERVER);
+		swtJbtExt.removeSeamProjectFromServers(seam22Settings.getProperty("testProjectName"));
+		new SWTUtilExt(bot).waitForAll(60 * 1000L);
 	}
 	
 }
