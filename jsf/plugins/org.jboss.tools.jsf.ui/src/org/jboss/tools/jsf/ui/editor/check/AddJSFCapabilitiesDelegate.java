9c51812fc8c554ff5d4cff41cb17b9c818ad200d
Yura Zhishko <yzhishko@exadel.com>
2010-04-02 15:09:39 +0000

    git-svn-id: http://svn.jboss.org/repos/jbosstools/trunk@21241 a97e2381-89e5-4abb-bab3-167db6db766c

--- jsf/plugins/org.jboss.tools.jsf.ui/src/org/jboss/tools/jsf/ui/editor/check/AddJSFCapabilitiesDelegate.java
+++ jsf/plugins/org.jboss.tools.jsf.ui/src/org/jboss/tools/jsf/ui/editor/check/AddJSFCapabilitiesDelegate.java
@@ -1,7 +1,24 @@
+/*******************************************************************************
+ * Copyright (c) 2007-2010 Exadel, Inc. and Red Hat, Inc.
+ * Distributed under license by Red Hat, Inc. All rights reserved.
+ * This program is made available under the terms of the
+ * Eclipse Public License v1.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors:
+ *     Exadel, Inc. and Red Hat, Inc. - initial API and implementation
+ ******************************************************************************/
+
 package org.jboss.tools.jsf.ui.editor.check;
 
 import org.eclipse.core.resources.IProject;
 import org.jboss.tools.jsf.ui.action.AddJSFNatureActionDelegate;
+
+/**
+ * 
+ * @author yzhishko
+ *
+ */
 
 public class AddJSFCapabilitiesDelegate extends AddJSFNatureActionDelegate {
 
--- jsf/plugins/org.jboss.tools.jsf.ui/src/org/jboss/tools/jsf/ui/editor/check/ProjectNaturesPartListener.java
+++ jsf/plugins/org.jboss.tools.jsf.ui/src/org/jboss/tools/jsf/ui/editor/check/ProjectNaturesPartListener.java
@@ -1,3 +1,14 @@
+/*******************************************************************************
+ * Copyright (c) 2007-2010 Exadel, Inc. and Red Hat, Inc.
+ * Distributed under license by Red Hat, Inc. All rights reserved.
+ * This program is made available under the terms of the
+ * Eclipse Public License v1.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors:
+ *     Exadel, Inc. and Red Hat, Inc. - initial API and implementation
+ ******************************************************************************/
+
 package org.jboss.tools.jsf.ui.editor.check;
 
 import org.eclipse.core.runtime.CoreException;
@@ -9,6 +20,12 @@ import org.jboss.tools.common.reporting.ProblemReportingHelper;
 import org.jboss.tools.jst.jsp.JspEditorPlugin;
 import org.jboss.tools.jst.jsp.jspeditor.JSPMultiPageEditor;
 import org.jboss.tools.jst.jsp.preferences.IVpePreferencesPage;
+
+/**
+ * 
+ * @author yzhishko
+ *
+ */
 
 public class ProjectNaturesPartListener implements IPartListener {
 
