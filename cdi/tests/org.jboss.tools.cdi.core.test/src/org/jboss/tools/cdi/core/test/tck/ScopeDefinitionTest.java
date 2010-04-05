6df2f428bd4690ff76552af3502900f4c3b58a8a
Alexey Kazakov <akazakov@exadel.com>
2010-04-05 19:05:08 +0000

    https://jira.jboss.org/jira/browse/JBIDE-5808 Added new CDI Core tests for Scope definitions.
    git-svn-id: http://svn.jboss.org/repos/jbosstools/trunk@21271 a97e2381-89e5-4abb-bab3-167db6db766c

--- cdi/tests/org.jboss.tools.cdi.core.test/src/org/jboss/tools/cdi/core/test/tck/ScopeDefinitionTest.java
+++ cdi/tests/org.jboss.tools.cdi.core.test/src/org/jboss/tools/cdi/core/test/tck/ScopeDefinitionTest.java
@@ -69,4 +69,88 @@ public class ScopeDefinitionTest extends TCKTest {
 		assertEquals("Wrong scope type", "javax.enterprise.context.Dependent",
 				bean.getScope().getSourceType().getFullyQualifiedName());
 	}
+
+	/**
+	 * section 2.4.4 e), section 2.7.2 a)
+	 * 
+	 * @throws JavaModelException
+	 */
+	public void testScopeSpecifiedAndStereotyped() throws JavaModelException {
+		Set<IBean> beans = getBeans("org.jboss.jsr299.tck.tests.definition.scope.Minnow");
+		assertEquals("Wrong number of beans.", 1, beans.size());
+		IBean bean = beans.iterator().next();
+		assertEquals("Wrong scope type",
+				"javax.enterprise.context.RequestScoped", bean.getScope()
+						.getSourceType().getFullyQualifiedName());
+		Set<IScopeDeclaration> declarations = bean.getScopeDeclarations();
+		assertEquals("Wrong number of scope declarations", 1, declarations
+				.size());
+		assertLocationEquals(declarations, 920, 14);
+	}
+
+	/**
+	 * section 2.4.4 da)
+	 * 
+	 * @throws JavaModelException
+	 */
+	public void testMultipleIncompatibleScopeStereotypesWithScopeSpecified()
+			throws JavaModelException {
+		Set<IBean> beans = getBeans("org.jboss.jsr299.tck.tests.definition.scope.Pollock");
+		assertEquals("Wrong number of beans.", 1, beans.size());
+		IBean bean = beans.iterator().next();
+		assertEquals("Wrong scope type", "javax.enterprise.context.Dependent",
+				bean.getScope().getSourceType().getFullyQualifiedName());
+		Set<IScopeDeclaration> declarations = bean.getScopeDeclarations();
+		assertEquals("Wrong number of scope declarations", 1, declarations
+				.size());
+		assertLocationEquals(declarations, 930, 10);
+	}
+
+	/**
+	 * section 2.4.4 c)
+	 * 
+	 * @throws JavaModelException
+	 */
+	public void testMultipleCompatibleScopeStereotypes()
+			throws JavaModelException {
+		Set<IBean> beans = getBeans("org.jboss.jsr299.tck.tests.definition.scope.Grayling");
+		assertEquals("Wrong number of beans.", 1, beans.size());
+		IBean bean = beans.iterator().next();
+		assertEquals("Wrong scope type",
+				"javax.enterprise.context.ApplicationScoped", bean.getScope()
+						.getSourceType().getFullyQualifiedName());
+	}
+
+	/**
+	 * section 2.7.2 db), section 4.1 ab)
+	 * 
+	 * @throws JavaModelException
+	 */
+	public void testWebBeanScopeTypeOverridesStereotype()
+			throws JavaModelException {
+		Set<IBean> beans = getBeans("org.jboss.jsr299.tck.tests.definition.scope.RedSnapper");
+		assertEquals("Wrong number of beans.", 1, beans.size());
+		IBean bean = beans.iterator().next();
+		assertEquals("Wrong scope type",
+				"javax.enterprise.context.RequestScoped", bean.getScope()
+						.getSourceType().getFullyQualifiedName());
+		Set<IScopeDeclaration> declarations = bean.getScopeDeclarations();
+		assertEquals("Wrong number of scope declarations", 1, declarations
+				.size());
+		assertLocationEquals(declarations, 915, 14);
+	}
+
+	/**
+	 * section 4.1 ba)
+	 * 
+	 * @throws JavaModelException
+	 */
+	public void testScopeTypeDeclaredInheritedIsInherited()
+			throws JavaModelException {
+		Set<IBean> beans = getBeans("org.jboss.jsr299.tck.tests.definition.scope.BorderCollie");
+		IBean bean = beans.iterator().next();
+		assertEquals("Wrong scope type",
+				"javax.enterprise.context.RequestScoped", bean.getScope()
+						.getSourceType().getFullyQualifiedName());
+	}
 }
