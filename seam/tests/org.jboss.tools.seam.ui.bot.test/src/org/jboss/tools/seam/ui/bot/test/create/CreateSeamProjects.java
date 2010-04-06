2538c1afc61edf602b6e01e2f198e44cb05bdd9e
Vlado Pakan <vpakan@redhat.com>
2010-04-06 08:13:02 +0000

    Ignore NullPointerException when creating first Seam project. It's eclispe bug already fixed in newer Eclipse version.
    git-svn-id: http://svn.jboss.org/repos/jbosstools/trunk@21278 a97e2381-89e5-4abb-bab3-167db6db766c

--- seam/tests/org.jboss.tools.seam.ui.bot.test/src/org/jboss/tools/seam/ui/bot/test/create/CreateSeamProjects.java
+++ seam/tests/org.jboss.tools.seam.ui.bot.test/src/org/jboss/tools/seam/ui/bot/test/create/CreateSeamProjects.java
@@ -1,7 +1,6 @@
 package org.jboss.tools.seam.ui.bot.test.create;
 
 import org.jboss.tools.seam.ui.bot.test.TestControl;
-import org.jboss.tools.ui.bot.ext.SWTBotExt;
 import org.jboss.tools.ui.bot.ext.SWTJBTExt;
 import org.jboss.tools.ui.bot.ext.SWTUtilExt;
 
@@ -14,6 +13,17 @@ public class CreateSeamProjects extends TestControl{
 		waitForBlockingJobsAcomplished(240000, BUILDING_WS, VALIDATION + 
 				" " + seam12Settings.getProperty("testProjectName")
 				+TYPE_WAR, DEPLOY_SOURCE, REG_IN_SERVER);
+		//TODO: This is eclipse bug already fixed in newer versions of Elipse so this should be removed
+		//       when JBDS will use never version of Eclipse
+		Throwable exception = getException(); 
+		if (exception != null 
+		    && exception instanceof NullPointerException
+		    && exception.getStackTrace().length > 0
+		    && exception.getStackTrace()[0].getClassName().equals("org.eclipse.wst.server.core.internal.Server")
+		    && exception.getStackTrace()[0].getLineNumber() == 783){
+		  // Reset returned exception
+		  setException(null);
+		}
 		swtJbtExt.removeSeamProjectFromServers(seam12Settings.getProperty("testProjectName"));
 	}
 	
