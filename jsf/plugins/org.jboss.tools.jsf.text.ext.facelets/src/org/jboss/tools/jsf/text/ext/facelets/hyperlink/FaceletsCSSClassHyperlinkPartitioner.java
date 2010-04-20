3d6d7bc2708ffafbdc5bf20fdd90d34260a1de02
Daniel Azarov <daniel@exadel.com>
2010-04-20 15:52:21 +0000

    https://jira.jboss.org/jira/browse/JBIDE-5986
    git-svn-id: http://svn.jboss.org/repos/jbosstools/branches/jbosstools-3.1.x@21559 a97e2381-89e5-4abb-bab3-167db6db766c

--- jsf/plugins/org.jboss.tools.jsf.text.ext.facelets/src/org/jboss/tools/jsf/text/ext/facelets/hyperlink/FaceletsCSSClassHyperlinkPartitioner.java
+++ jsf/plugins/org.jboss.tools.jsf.text.ext.facelets/src/org/jboss/tools/jsf/text/ext/facelets/hyperlink/FaceletsCSSClassHyperlinkPartitioner.java
@@ -9,7 +9,7 @@
  *     Exadel, Inc. and Red Hat, Inc. - initial API and implementation
  ******************************************************************************/
 
-package org.jboss.tools.jsf.text.ext.richfaces.hyperlink;
+package org.jboss.tools.jsf.text.ext.facelets.hyperlink;
 
 import org.eclipse.jface.text.IDocument;
 import org.jboss.tools.common.text.ext.hyperlink.IHyperlinkRegion;
@@ -20,21 +20,21 @@ import org.jboss.tools.jst.text.ext.hyperlink.CSSClassHyperlinkPartitioner;
  * @author Victor Rubezhny
  *
  */
-public class RichfacesCSSClassHyperlinkPartitioner extends CSSClassHyperlinkPartitioner {
-	public static final String RICHFACES_CSS_CLASS_PARTITION = "org.jboss.tools.common.text.ext.RICHFACES_CSS_CLASS"; //$NON-NLS-1$
-	private static final String RICHFACES_CSS_CLASS_TOKEN = "/styleClass/"; //$NON-NLS-1$
+public class FaceletsCSSClassHyperlinkPartitioner extends CSSClassHyperlinkPartitioner {
+	public static final String FACELETS_CSS_CLASS_PARTITION = "org.jboss.tools.common.text.ext.FACELETS_CSS_CLASS"; //$NON-NLS-1$
+	
 
 	@Override
 	public boolean recognize(IDocument document, IHyperlinkRegion region) {
-		if (region.getAxis() != null
-				&& region.getAxis().endsWith(RICHFACES_CSS_CLASS_TOKEN))
+		if (region.getAxis() != null){
 			return true;
+		}
 		return false;
 	}
 
 	@Override
 	protected String getPartitionType(String axis) {
-		return RICHFACES_CSS_CLASS_PARTITION;
+		return FACELETS_CSS_CLASS_PARTITION;
 	}
 	
 }
