75eea16ab03d0ea871191e3e11d1ab9f6934d121
Daniel Azarov <daniel@exadel.com>
2010-05-24 15:03:51 +0000

    https://jira.jboss.org/browse/JBIDE-6311
    git-svn-id: http://svn.jboss.org/repos/jbosstools/trunk@22286 a97e2381-89e5-4abb-bab3-167db6db766c

--- cdi/tests/org.jboss.tools.cdi.text.ext.test/src/org/jboss/tools/cdi/text/ext/test/CdiTextExtAllTests.java
+++ cdi/tests/org.jboss.tools.cdi.text.ext.test/src/org/jboss/tools/cdi/text/ext/test/CdiTextExtAllTests.java
@@ -1,15 +1,15 @@
 package org.jboss.tools.cdi.text.ext.test;
 
 
+import org.jboss.tools.cdi.core.test.CDICoreTestSetup;
+
 import junit.framework.Test;
 import junit.framework.TestSuite;
 
 public class CdiTextExtAllTests {
 	public static Test suite() {
 		TestSuite suite = new TestSuite(CdiTextExtAllTests.class.getName());
-		suite.addTest(InjectedPointHyperlinkDetectorTest.suite());
-		suite.addTest(ProducerDisposerHyperlinkDetectorTest.suite());
-		suite.addTest(InjectedPointInProducerMethodHyperlinkDetectorTest.suite());
+		suite.addTest(new CDICoreTestSetup(CDITextExtTestSuite.suite()));
 		return suite;
 	}
 }
