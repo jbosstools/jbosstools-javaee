b47b18a311becc519270f7cc6bb4e0b145b8540b
Daniel Azarov <daniel@exadel.com>
2010-10-01 15:47:43 +0000

    https://jira.jboss.org/browse/JBIDE-7157
    git-svn-id: http://svn.jboss.org/repos/jbosstools/trunk@25401 a97e2381-89e5-4abb-bab3-167db6db766c

--- jsf/tests/org.jboss.tools.jsf.test/src/org/jboss/tools/jsf/test/refactoring/JSF2RefactoringTest.java
+++ jsf/tests/org.jboss.tools.jsf.test/src/org/jboss/tools/jsf/test/refactoring/JSF2RefactoringTest.java
@@ -13,6 +13,7 @@ import org.eclipse.ltk.internal.core.refactoring.resource.MoveResourcesProcessor
 import org.eclipse.ltk.internal.core.refactoring.resource.RenameResourceProcessor;
 import org.jboss.tools.jsf.jsf2.refactoring.JSF2RenameParticipant;
 import org.jboss.tools.jsf.jsf2.refactoring.JSf2MoveParticipant;
+import org.jboss.tools.test.util.JobUtils;
 import org.jboss.tools.test.util.ProjectImportTestSetup;
 import org.jboss.tools.tests.AbstractRefactorTest;
 
@@ -27,6 +28,7 @@ public class JSF2RefactoringTest extends AbstractRefactorTest  {
 	protected void setUp() throws Exception {
 		project = ProjectImportTestSetup.loadProject(projectName);
 		project.build(IncrementalProjectBuilder.FULL_BUILD, new NullProgressMonitor());
+		JobUtils.waitForIdle(2000);
 	}
 	
 	public void testRenameCompositeComponentFile() throws CoreException {
@@ -40,6 +42,7 @@ public class JSF2RefactoringTest extends AbstractRefactorTest  {
 		IFile sourceFile = project.getProject().getFile("/WebContent/resources/demo/input.xhtml");
 		
 		RenameResourceProcessor processor = new RenameResourceProcessor(sourceFile);
+		processor.setNewResourceName("input2.xhtml");
 		
 		JSF2RenameParticipant participant = new JSF2RenameParticipant();
 
@@ -57,6 +60,7 @@ public class JSF2RefactoringTest extends AbstractRefactorTest  {
 		IFolder sourceFolder = project.getProject().getFolder("/WebContent/resources/demo");
 		
 		RenameResourceProcessor processor = new RenameResourceProcessor(sourceFolder);
+		processor.setNewResourceName("demo2");
 		
 		JSF2RenameParticipant participant = new JSF2RenameParticipant();
 
@@ -67,14 +71,15 @@ public class JSF2RefactoringTest extends AbstractRefactorTest  {
 		ArrayList<TestChangeStructure> list = new ArrayList<TestChangeStructure>();
 
 		TestChangeStructure structure = new TestChangeStructure(project.getProject(), "/WebContent/pages/inputname.xhtml");
-		TestTextChange change = new TestTextChange(776, 6, "input2");
+		TestTextChange change = new TestTextChange(382, 3, "new");
 		structure.addTextChange(change);
 		list.add(structure);
 
-		IFile sourceFile = project.getProject().getFile("/WebContent/resources/demo/input.xhtml");
+		IFile sourceFile = project.getProject().getFile("/WebContent/resources/demo2/input2.xhtml");
 		IFolder destinationFolder = project.getProject().getFolder("/WebContent/resources/new");
 		
 		MoveResourcesProcessor processor = new MoveResourcesProcessor(new IResource[]{sourceFile});
+		processor.setDestination(destinationFolder);
 		
 		JSf2MoveParticipant participant = new JSf2MoveParticipant();
 
@@ -85,14 +90,15 @@ public class JSF2RefactoringTest extends AbstractRefactorTest  {
 		ArrayList<TestChangeStructure> list = new ArrayList<TestChangeStructure>();
 
 		TestChangeStructure structure = new TestChangeStructure(project.getProject(), "/WebContent/pages/inputname.xhtml");
-		TestTextChange change = new TestTextChange(776, 6, "input2");
+		TestTextChange change = new TestTextChange(382, 9, "demo2/new");
 		structure.addTextChange(change);
 		list.add(structure);
 
-		IFolder sourceFolder = project.getProject().getFolder("/WebContent/resources/demo");
-		IFolder destinationFolder = project.getProject().getFolder("/WebContent/resources/new");
+		IFolder sourceFolder = project.getProject().getFolder("/WebContent/resources/new");
+		IFolder destinationFolder = project.getProject().getFolder("/WebContent/resources/demo2");
 		
 		MoveResourcesProcessor processor = new MoveResourcesProcessor(new IResource[]{sourceFolder});
+		processor.setDestination(destinationFolder);
 		
 		JSf2MoveParticipant participant = new JSf2MoveParticipant();
 
