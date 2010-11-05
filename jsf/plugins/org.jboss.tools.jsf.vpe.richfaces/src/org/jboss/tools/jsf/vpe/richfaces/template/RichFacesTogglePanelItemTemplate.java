a7f0c1813947cacfe27e9eedd2b3b02de5c3bd0b
Denis Maliarevich <dmaliarevich@exadel.com>
2010-11-05 16:51:16 +0000

    https://jira.jboss.org/browse/JBIDE-7451 , RF4 OUTPUT templates were added.
    git-svn-id: http://svn.jboss.org/repos/jbosstools/trunk@26306 a97e2381-89e5-4abb-bab3-167db6db766c

---
+++ jsf/plugins/org.jboss.tools.jsf.vpe.richfaces/src/org/jboss/tools/jsf/vpe/richfaces/template/RichFacesTogglePanelItemTemplate.java
@@ -0,0 +1,31 @@
+/*******************************************************************************
+ * Copyright (c) 2007-2010 Exadel, Inc. and Red Hat, Inc.
+ * Distributed under license by Red Hat, Inc. All rights reserved.
+ * This program is made available under the terms of the
+ * Eclipse Public License v1.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors:
+ *     Exadel, Inc. and Red Hat, Inc. - initial API and implementation
+ ******************************************************************************/ 
+package org.jboss.tools.jsf.vpe.richfaces.template;
+
+import org.jboss.tools.vpe.editor.context.VpePageContext;
+import org.jboss.tools.vpe.editor.template.VpeAbstractTemplate;
+import org.jboss.tools.vpe.editor.template.VpeCreationData;
+import org.jboss.tools.vpe.editor.util.HTML;
+import org.mozilla.interfaces.nsIDOMDocument;
+import org.mozilla.interfaces.nsIDOMElement;
+import org.w3c.dom.Element;
+import org.w3c.dom.Node;
+
+public class RichFacesTogglePanelItemTemplate extends VpeAbstractTemplate {
+
+	public VpeCreationData create(VpePageContext pageContext, Node sourceNode,
+			nsIDOMDocument visualDocument) {
+		nsIDOMElement div = visualDocument.createElement(HTML.TAG_DIV);
+		VpeCreationData creationData = new VpeCreationData(div);
+		return creationData;
+	}
+
+}
