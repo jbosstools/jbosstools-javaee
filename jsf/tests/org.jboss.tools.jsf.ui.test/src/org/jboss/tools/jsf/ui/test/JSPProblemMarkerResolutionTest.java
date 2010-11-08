64b6cd11b05193bf388ae768bb97da94db963a1c
Daniel Azarov <daniel@exadel.com>
2010-11-08 15:29:25 +0000

    https://jira.jboss.org/browse/JBIDE-7491
    git-svn-id: http://svn.jboss.org/repos/jbosstools/trunk@26330 a97e2381-89e5-4abb-bab3-167db6db766c

--- jsf/tests/org.jboss.tools.jsf.ui.test/src/org/jboss/tools/jsf/ui/test/JSPProblemMarkerResolutionTest.java
+++ jsf/tests/org.jboss.tools.jsf.ui.test/src/org/jboss/tools/jsf/ui/test/JSPProblemMarkerResolutionTest.java
@@ -4,6 +4,7 @@ import org.eclipse.core.resources.IFile;
 import org.eclipse.core.resources.IMarker;
 import org.eclipse.core.resources.IProject;
 import org.eclipse.core.resources.IResource;
+import org.eclipse.core.resources.IncrementalProjectBuilder;
 import org.eclipse.core.resources.ResourcesPlugin;
 import org.eclipse.core.runtime.CoreException;
 import org.eclipse.ui.IMarkerResolution;
@@ -35,15 +36,16 @@ public class JSPProblemMarkerResolutionTest extends AbstractResourceMarkerTest{
 			project = setup.importProject();
 		}
 		this.project = project.getProject();
+		this.project.build(IncrementalProjectBuilder.FULL_BUILD, null);
 		JobUtils.waitForIdle();
 	}
 	
 	public void testProblemMarkerResolutionInJSP() throws CoreException {
 		IFile jspFile = project.getFile("WebContent/pages/test_page1.jsp");
 		
-		assertMarkerIsCreated(jspFile, "org.eclipse.jst.jsp.core.validationMarker", "Unknown tag.*");
+		assertMarkerIsCreated(jspFile, "org.eclipse.jst.jsp.core.validationMarker", "Unknown tag (h:commandButton).");
 		
-		IMarker[] markers = findMarkers(jspFile, "org.eclipse.jst.jsp.core.validationMarker", "Unknown tag.*");
+		IMarker[] markers = findMarkers(jspFile, "org.eclipse.jst.jsp.core.validationMarker", "Unknown tag (h:commandButton).");
 		
 		assertEquals(1, markers.length);
 		
@@ -59,15 +61,15 @@ public class JSPProblemMarkerResolutionTest extends AbstractResourceMarkerTest{
 		
 		refreshProject(project);
 		
-		assertMarkerIsNotCreated(jspFile, "org.eclipse.jst.jsp.core.validationMarker", "Unknown tag.*");
+		assertMarkerIsNotCreated(jspFile, "org.eclipse.jst.jsp.core.validationMarker", "Unknown tag (h:commandButton).");
 	}
 	
 	public void testProblemMarkerResolutionInXHTML() throws CoreException {
 		IFile jspFile = project.getFile("WebContent/pages/test_page2.xhtml");
 		
-		assertMarkerIsCreated(jspFile, "org.eclipse.wst.html.core.validationMarker", "Unknown tag.*");
+		assertMarkerIsCreated(jspFile, "org.eclipse.wst.html.core.validationMarker", "Unknown tag (ui:insert).");
 		
-		IMarker[] markers = findMarkers(jspFile, "org.eclipse.wst.html.core.validationMarker", "Unknown tag.*");
+		IMarker[] markers = findMarkers(jspFile, "org.eclipse.wst.html.core.validationMarker", "Unknown tag (ui:insert).");
 		
 		assertEquals(3, markers.length);
 		
@@ -83,12 +85,13 @@ public class JSPProblemMarkerResolutionTest extends AbstractResourceMarkerTest{
 		
 		refreshProject(project);
 		
-		assertMarkerIsNotCreated(jspFile, "org.eclipse.wst.html.core.validationMarker", "Unknown tag.*");
+		assertMarkerIsNotCreated(jspFile, "org.eclipse.wst.html.core.validationMarker", "Unknown tag (ui:insert).");
 	}
 	
 	private void refreshProject(IProject project){
 		try {
 			project.refreshLocal(IResource.DEPTH_INFINITE, null);
+			project.build(IncrementalProjectBuilder.FULL_BUILD, null);
 			JobUtils.waitForIdle();
 			JobUtils.delay(2000);
 		} catch (CoreException e) {
