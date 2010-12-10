5ae2cd10a093531a53aa4555abdb9682ea85f275
Viacheslav Kabanovich <scabanovich@exadel.com>
2010-12-10 15:17:44 +0000

    JBIDE-7842 https://issues.jboss.org/browse/JBIDE-7842
    git-svn-id: http://svn.jboss.org/repos/jbosstools/trunk@27362 a97e2381-89e5-4abb-bab3-167db6db766c

--- cdi/plugins/org.jboss.tools.cdi.ui/src/org/jboss/tools/cdi/ui/wizard/NewBeansXMLCreationWizard.java
+++ cdi/plugins/org.jboss.tools.cdi.ui/src/org/jboss/tools/cdi/ui/wizard/NewBeansXMLCreationWizard.java
@@ -1,3 +1,14 @@
+/******************************************************************************* 
+ * Copyright (c) 2010 Red Hat, Inc. 
+ * Distributed under license by Red Hat, Inc. All rights reserved. 
+ * This program is made available under the terms of the 
+ * Eclipse Public License v1.0 which accompanies this distribution, 
+ * and is available at http://www.eclipse.org/legal/epl-v10.html 
+ * 
+ * Contributors: 
+ * Red Hat, Inc. - initial API and implementation 
+ ******************************************************************************/
+
 package org.jboss.tools.cdi.ui.wizard;
 
 import java.io.ByteArrayInputStream;
@@ -34,6 +45,11 @@ import org.jboss.tools.common.model.options.PreferenceModelUtilities;
 import org.jboss.tools.common.model.project.ProjectHome;
 import org.jboss.tools.common.model.util.EclipseResourceUtil;
 
+/**
+ * 
+ * @author Viacheslav Kabanovich
+ *
+ */
 public class NewBeansXMLCreationWizard extends BasicNewResourceWizard {
 	public static final String WIZARD_ID = "org.jboss.tools.cdi.ui.wizard.NewBeansXMLCreationWizard"; //$NON-NLS-1$
 	
