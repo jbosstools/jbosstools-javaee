f8f1c35b41ca920a1663d55a0ce196a945fbfc10
Alexey Kazakov <akazakov@exadel.com>
2010-12-28 18:25:47 +0000

    https://issues.jboss.org/browse/JBIDE-7955
    git-svn-id: http://svn.jboss.org/repos/jbosstools/trunk@27779 a97e2381-89e5-4abb-bab3-167db6db766c

---
+++ jsf/plugins/org.jboss.tools.jsf/src/org/jboss/tools/jsf/web/validation/JSFELValidationDelegate.java
@@ -0,0 +1,61 @@
+/******************************************************************************* 
+ * Copyright (c) 2010 Red Hat, Inc. 
+ * Distributed under license by Red Hat, Inc. All rights reserved. 
+ * This program is made available under the terms of the 
+ * Eclipse Public License v1.0 which accompanies this distribution, 
+ * and is available at http://www.eclipse.org/legal/epl-v10.html 
+ * 
+ * Contributors: 
+ * Red Hat, Inc. - initial API and implementation 
+ ******************************************************************************/
+package org.jboss.tools.jsf.web.validation;
+
+import java.util.HashSet;
+import java.util.Set;
+
+import org.eclipse.core.resources.IProject;
+import org.eclipse.core.runtime.CoreException;
+import org.jboss.tools.jsf.JSFModelPlugin;
+import org.jboss.tools.jsf.project.JSFNature;
+import org.jboss.tools.jst.web.kb.IKbProject;
+import org.jboss.tools.jst.web.kb.KbProjectFactory;
+import org.jboss.tools.jst.web.kb.internal.KbProject;
+import org.jboss.tools.jst.web.kb.internal.validation.SimpleValidatingProjectTree;
+import org.jboss.tools.jst.web.kb.internal.validation.ValidatingProjectSet;
+import org.jboss.tools.jst.web.kb.validation.IELValidationDelegate;
+import org.jboss.tools.jst.web.kb.validation.IProjectValidationContext;
+import org.jboss.tools.jst.web.kb.validation.IValidatingProjectSet;
+import org.jboss.tools.jst.web.kb.validation.IValidatingProjectTree;
+
+/**
+ * @author Alexey Kazakov
+ */
+public class JSFELValidationDelegate implements IELValidationDelegate {
+
+	/* (non-Javadoc)
+	 * @see org.jboss.tools.jst.web.kb.validation.IELValidationDelegate#getValidatingProjects(org.eclipse.core.resources.IProject)
+	 */
+	public IValidatingProjectTree getValidatingProjects(IProject project) {
+		Set<IProject> projects = new HashSet<IProject>();
+		projects.add(project);
+		IKbProject kbProject = KbProjectFactory.getKbProject(project, false);
+		if(kbProject!=null) {
+			IProjectValidationContext rootContext = kbProject.getValidationContext();
+			IValidatingProjectSet projectSet = new ValidatingProjectSet(project, projects, rootContext);
+			return new SimpleValidatingProjectTree(projectSet);
+		}
+		return new SimpleValidatingProjectTree(project);
+	}
+
+	/* (non-Javadoc)
+	 * @see org.jboss.tools.jst.web.kb.validation.IELValidationDelegate#shouldValidate(org.eclipse.core.resources.IProject)
+	 */
+	public boolean shouldValidate(IProject project) {
+		try {
+			return project!=null && project.isAccessible() && project.hasNature(JSFNature.NATURE_ID) && KbProject.checkKBBuilderInstalled(project);
+		} catch (CoreException e) {
+			JSFModelPlugin.getDefault().logError(e);
+		}
+		return false;
+	}
+}
