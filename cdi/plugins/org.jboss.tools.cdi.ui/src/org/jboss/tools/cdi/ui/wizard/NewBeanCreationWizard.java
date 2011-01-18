b3bd089cc551ba42f1d460eca0cfb8ae2b51e853
Viacheslav Kabanovich <scabanovich@exadel.com>
2011-01-18 17:20:56 +0000

    JBIDE-8124 https://issues.jboss.org/browse/JBIDE-8124
    git-svn-id: http://svn.jboss.org/repos/jbosstools/branches/jbosstools-3.2.0.CR1@28365 a97e2381-89e5-4abb-bab3-167db6db766c

---
+++ cdi/plugins/org.jboss.tools.cdi.ui/src/org/jboss/tools/cdi/ui/wizard/NewBeanCreationWizard.java
@@ -0,0 +1,89 @@
+/******************************************************************************* 
+ * Copyright (c) 2010 Red Hat, Inc. 
+ * Distributed under license by Red Hat, Inc. All rights reserved. 
+ * This program is made available under the terms of the 
+ * Eclipse Public License v1.0 which accompanies this distribution, 
+ * and is available at http://www.eclipse.org/legal/epl-v10.html 
+ * 
+ * Contributors: 
+ * Red Hat, Inc. - initial API and implementation 
+ ******************************************************************************/
+
+package org.jboss.tools.cdi.ui.wizard;
+
+import org.eclipse.core.resources.IFile;
+import org.eclipse.core.resources.IResource;
+import org.eclipse.core.runtime.CoreException;
+import org.eclipse.core.runtime.IProgressMonitor;
+import org.eclipse.jdt.core.IJavaElement;
+import org.eclipse.jdt.internal.ui.wizards.NewElementWizard;
+import org.eclipse.jdt.ui.wizards.NewClassWizardPage;
+import org.jboss.tools.cdi.ui.CDIUIMessages;
+
+/**
+ * 
+ * @author Viacheslav Kabanovich
+ *
+ */
+public class NewBeanCreationWizard extends NewElementWizard {
+	private NewClassWizardPage fPage;
+    private boolean fOpenEditorOnFinish = true;
+
+
+	public NewBeanCreationWizard() {
+		setWindowTitle(CDIUIMessages.NEW_BEAN_WIZARD_TITLE);
+	}
+
+	/*
+	 * @see Wizard#createPages
+	 */
+	public void addPages() {
+		super.addPages();
+		if (fPage == null) {
+			fPage = new  NewBeanWizardPage();
+			fPage.init(getSelection());
+		}
+		addPage(fPage);
+	}
+
+	/*(non-Javadoc)
+	 * @see org.eclipse.jdt.internal.ui.wizards.NewElementWizard#canRunForked()
+	 */
+	protected boolean canRunForked() {
+		return !fPage.isEnclosingTypeSelected();
+	}
+
+	/* (non-Javadoc)
+	 * @see org.eclipse.jdt.internal.ui.wizards.NewElementWizard#finishPage(org.eclipse.core.runtime.IProgressMonitor)
+	 */
+	protected void finishPage(IProgressMonitor monitor) throws InterruptedException, CoreException {
+		fPage.createType(monitor); // use the full progress monitor
+	}
+
+	/* (non-Javadoc)
+	 * @see org.eclipse.jface.wizard.IWizard#performFinish()
+	 */
+	public boolean performFinish() {
+		warnAboutTypeCommentDeprecation();
+		boolean res= super.performFinish();
+		if (res) {
+			IResource resource= fPage.getModifiedResource();
+			if (resource != null) {
+				selectAndReveal(resource);
+				if (fOpenEditorOnFinish) {
+					openResource((IFile) resource);
+				}
+			}
+		}
+		return res;
+	}
+
+	/* (non-Javadoc)
+	 * @see org.eclipse.jdt.internal.ui.wizards.NewElementWizard#getCreatedElement()
+	 */
+	public IJavaElement getCreatedElement() {
+		return fPage.getCreatedType();
+	}
+
+
+}
