779c89930084f696b4f6586e2fcf3891b4fc3f53
Viacheslav Kabanovich <scabanovich@exadel.com>
2011-03-29 20:21:01 +0000

    JBIDE-3120 https://issues.jboss.org/browse/JBIDE-3120
    git-svn-id: http://svn.jboss.org/repos/jbosstools/trunk@30120 a97e2381-89e5-4abb-bab3-167db6db766c

--- cdi/tests/org.jboss.tools.cdi.core.test/src/org/jboss/tools/cdi/core/test/extension/CDIExtensionImpl.java
+++ cdi/tests/org.jboss.tools.cdi.core.test/src/org/jboss/tools/cdi/core/test/extension/CDIExtensionImpl.java
@@ -1,10 +1,19 @@
 package org.jboss.tools.cdi.core.test.extension;
 
+import org.jboss.tools.cdi.core.IBean;
 import org.jboss.tools.cdi.core.extension.ICDIExtension;
+import org.jboss.tools.cdi.core.extension.feature.IBeanNameFeature;
 
-public class CDIExtensionImpl implements ICDIExtension {
+public class CDIExtensionImpl implements ICDIExtension, IBeanNameFeature {
 
 	public Object getAdapter(Class adapter) {
+		if(adapter == IBeanNameFeature.class) {
+			return this;
+		}
+		return null;
+	}
+
+	public String computeBeanName(IBean bean) {
 		return null;
 	}
 
--- cdi/tests/org.jboss.tools.cdi.core.test/src/org/jboss/tools/cdi/core/test/extension/ExtensionFactoryTest.java
+++ cdi/tests/org.jboss.tools.cdi.core.test/src/org/jboss/tools/cdi/core/test/extension/ExtensionFactoryTest.java
@@ -3,13 +3,14 @@ package org.jboss.tools.cdi.core.test.extension;
 import java.util.Set;
 
 import org.jboss.tools.cdi.core.extension.CDIExtensionFactory;
+import org.jboss.tools.cdi.core.extension.feature.IBeanNameFeature;
 
 import junit.framework.TestCase;
 
 public class ExtensionFactoryTest extends TestCase {
 
 	public void testExtensionFactory() throws Exception {
-		Set<String> clss = CDIExtensionFactory.getInstance().getExtensionClassesByfeature("feature_1");
+		Set<String> clss = CDIExtensionFactory.getInstance().getExtensionClassesByFeature(IBeanNameFeature.class);
 		System.out.println(clss.size());
 	}
 
