fa63fcbe505e68df0a3e3e096c26aabc97e5c527
Viacheslav Kabanovich <scabanovich@exadel.com>
2011-03-29 01:11:24 +0000

    Share project "org.jboss.tools.cdi.solder.core" into "https://svn.jboss.org/repos/jbosstools" JBIDE-3120 https://issues.jboss.org/browse/JBIDE-3120
    git-svn-id: http://svn.jboss.org/repos/jbosstools/trunk@30085 a97e2381-89e5-4abb-bab3-167db6db766c

---
+++ cdi/plugins/org.jboss.tools.cdi.solder.core/src/org/jboss/tools/cdi/solder/core/CDISolderCoreExtension.java
@@ -0,0 +1,12 @@
+package org.jboss.tools.cdi.solder.core;
+
+import org.jboss.tools.cdi.core.extension.ICDIExtension;
+
+public class CDISolderCoreExtension implements ICDIExtension {
+
+	public Object getAdapter(Class adapter) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
