092727e6aeba19422dbdc448ada2d0e5b8fd1280
Viacheslav Kabanovich <scabanovich@exadel.com>
2011-04-04 21:53:50 +0000

    JBIDE-8198 https://issues.jboss.org/browse/JBIDE-8198
    git-svn-id: http://svn.jboss.org/repos/jbosstools/trunk@30293 a97e2381-89e5-4abb-bab3-167db6db766c

---
+++ cdi/tests/org.jboss.tools.cdi.solder.core.test/src/org/jboss/tools/seam/solder/core/test/CDISolderCoreAllTests.java
@@ -0,0 +1,33 @@
+/******************************************************************************* 
+ * Copyright (c) 2011 Red Hat, Inc. 
+ * Distributed under license by Red Hat, Inc. All rights reserved. 
+ * This program is made available under the terms of the 
+ * Eclipse Public License v1.0 which accompanies this distribution, 
+ * and is available at http://www.eclipse.org/legal/epl-v10.html 
+ * 
+ * Contributors: 
+ * Red Hat, Inc. - initial API and implementation 
+ ******************************************************************************/ 
+package org.jboss.tools.seam.solder.core.test;
+
+import junit.framework.Test;
+import junit.framework.TestSuite;
+
+import org.eclipse.jdt.internal.core.JavaModelManager;
+/**
+ * @author Viacheslav Kabanovich
+ */
+public class CDISolderCoreAllTests {
+
+	public static Test suite() {
+		// it could be done here because it is not needed to be enabled back
+		JavaModelManager.getIndexManager().disable();
+
+		TestSuite suiteAll = new TestSuite("CDI Solder Core Tests");
+
+		suiteAll.addTestSuite(BeanNamingTest.class);
+		suiteAll.addTestSuite(VetoTest.class);
+
+		return suiteAll;
+	}
+}
