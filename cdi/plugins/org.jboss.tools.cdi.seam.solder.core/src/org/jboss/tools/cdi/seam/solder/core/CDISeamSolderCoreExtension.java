57c24a1b6bf204d46e49406b240239cbe09a5e79
Viacheslav Kabanovich <scabanovich@exadel.com>
2011-04-05 18:22:17 +0000

    JBIDE-8200 https://issues.jboss.org/browse/JBIDE-8200
    git-svn-id: http://svn.jboss.org/repos/jbosstools/trunk@30331 a97e2381-89e5-4abb-bab3-167db6db766c

--- cdi/plugins/org.jboss.tools.cdi.seam.solder.core/src/org/jboss/tools/cdi/seam/solder/core/CDISeamSolderCoreExtension.java
+++ cdi/plugins/org.jboss.tools.cdi.seam.solder.core/src/org/jboss/tools/cdi/seam/solder/core/CDISeamSolderCoreExtension.java
@@ -1,41 +1,62 @@
 package org.jboss.tools.cdi.seam.solder.core;
 
 
+import java.beans.Introspector;
 import java.util.ArrayList;
 import java.util.HashSet;
 import java.util.List;
 import java.util.Set;
 
 import org.eclipse.jdt.core.IJavaProject;
+import org.eclipse.jdt.core.IMemberValuePair;
 import org.eclipse.jdt.core.JavaModelException;
+import org.eclipse.jdt.core.SourceRange;
+import org.eclipse.jdt.internal.core.MemberValuePair;
+import org.jboss.tools.cdi.core.CDIConstants;
 import org.jboss.tools.cdi.core.IAnnotated;
 import org.jboss.tools.cdi.core.IAnnotationDeclaration;
-import org.jboss.tools.cdi.core.IParametedType;
 import org.jboss.tools.cdi.core.extension.ICDIExtension;
-import org.jboss.tools.cdi.core.extension.feature.IBeanNameFeature;
 import org.jboss.tools.cdi.core.extension.feature.IProcessAnnotatedTypeFeature;
+import org.jboss.tools.cdi.internal.core.impl.AnnotationLiteral;
 import org.jboss.tools.cdi.internal.core.impl.ParametedType;
 import org.jboss.tools.cdi.internal.core.impl.TypeDeclaration;
+import org.jboss.tools.cdi.internal.core.impl.definition.AbstractMemberDefinition;
+import org.jboss.tools.cdi.internal.core.impl.definition.AbstractTypeDefinition;
+import org.jboss.tools.cdi.internal.core.impl.definition.AnnotationDefinition;
 import org.jboss.tools.cdi.internal.core.impl.definition.DefinitionContext;
 import org.jboss.tools.cdi.internal.core.impl.definition.FieldDefinition;
 import org.jboss.tools.cdi.internal.core.impl.definition.MethodDefinition;
+import org.jboss.tools.cdi.internal.core.impl.definition.PackageDefinition;
 import org.jboss.tools.cdi.internal.core.impl.definition.ParameterDefinition;
 import org.jboss.tools.cdi.internal.core.impl.definition.TypeDefinition;
 import org.jboss.tools.common.model.util.EclipseResourceUtil;
+import org.jboss.tools.common.util.BeanUtil;
 import org.jboss.tools.common.util.EclipseJavaUtil;
 
-public class CDISeamSolderCoreExtension implements ICDIExtension,
-		IProcessAnnotatedTypeFeature {
+public class CDISeamSolderCoreExtension implements ICDIExtension, IProcessAnnotatedTypeFeature {
 
 	public Object getAdapter(Class adapter) {
-		if (adapter == IBeanNameFeature.class) {
-			return BeanNameFeature.instance;
-		}
 		return null;
 	}
 
-	public void processAnnotatedType(TypeDefinition typeDefinition,
-			DefinitionContext context) {
+	public void processAnnotatedType(TypeDefinition typeDefinition, DefinitionContext context) {
+
+		if(processVeto(typeDefinition, context)) {
+			return;
+		}
+
+		if(processRequires(typeDefinition, context)) {
+			return;
+		}
+	
+		processNames(typeDefinition, context);
+
+		processExact(typeDefinition, context);
+
+	}
+
+	// @Veto
+	private boolean processVeto(TypeDefinition typeDefinition, DefinitionContext context) {
 		if (typeDefinition
 				.isAnnotationPresent(CDISeamSolderConstants.VETO_ANNOTATION_TYPE_NAME)
 				|| (typeDefinition.getPackageDefinition() != null && typeDefinition
@@ -43,9 +64,13 @@ public class CDISeamSolderCoreExtension implements ICDIExtension,
 						.isAnnotationPresent(
 								CDISeamSolderConstants.VETO_ANNOTATION_TYPE_NAME))) {
 			typeDefinition.veto();
-			return;
+			return true;
 		}
+		return false;
+	}
 
+	// @Requires
+	private boolean processRequires(TypeDefinition typeDefinition, DefinitionContext context) {
 		Set<String> requiredClasses = new HashSet<String>();
 		List<String> typeRequiredClasses = getRequiredClasses(typeDefinition);
 		if (typeRequiredClasses != null)
@@ -62,35 +87,76 @@ public class CDISeamSolderCoreExtension implements ICDIExtension,
 				try {
 					if (EclipseJavaUtil.findType(jp, c) == null) {
 						typeDefinition.veto();
-						return;
+						return true;
 					}
 				} catch (JavaModelException e) {
 					CDISeamSolderCorePlugin.getDefault().logError(e);
 					typeDefinition.veto();
-					return;
+					return true;
 				}
 			}
 		}
-
-		List<FieldDefinition> fs = typeDefinition.getFields();
-		for (FieldDefinition f : fs) {
-			TypeDeclaration exact = getExactType(f, typeDefinition, context);
-			if (exact != null) {
-				f.setOverridenType(exact);
-			}
-		}
-
-		List<MethodDefinition> ms = typeDefinition.getMethods();
-		for (MethodDefinition m : ms) {
-			List<ParameterDefinition> ps = m.getParameters();
-			for (ParameterDefinition p : ps) {
-				TypeDeclaration exact = getExactType(p, typeDefinition, context);
-				if (exact != null) {
-					p.setOverridenType(exact);
-				}
-			}
-		}
+		return false;
 	}
+
+	// @FullyQualified @Named
+	private void processNames(TypeDefinition typeDefinition, DefinitionContext context) {
+		PackageDefinition p = typeDefinition.getPackageDefinition();
+		IAnnotationDeclaration namedOnPackage = null;
+		IAnnotationDeclaration fullyQualifiedOnPackage = null;
+		if(p != null) {
+			namedOnPackage = p.getAnnotation(CDIConstants.NAMED_QUALIFIER_TYPE_NAME);
+			fullyQualifiedOnPackage = p.getAnnotation(CDISeamSolderConstants.FULLY_QUALIFIED_ANNOTATION_TYPE_NAME);
+		}
+
+		processNames(typeDefinition, context, namedOnPackage, fullyQualifiedOnPackage, p);
+	
+		List<FieldDefinition> fs = typeDefinition.getFields();
+		for (FieldDefinition f: fs) {
+			if(f.isAnnotationPresent(CDIConstants.PRODUCES_ANNOTATION_TYPE_NAME)) {
+				processNames(f, context, null, fullyQualifiedOnPackage, p);
+			}
+		}
+		
+		List<MethodDefinition> ms = typeDefinition.getMethods();
+		for (MethodDefinition m: ms) {
+			if(m.isAnnotationPresent(CDIConstants.PRODUCES_ANNOTATION_TYPE_NAME)) {
+				processNames(m, context, null, fullyQualifiedOnPackage, p);
+			}
+		}
+		
+	}
+
+	private void processNames(AbstractMemberDefinition d, DefinitionContext context,
+			IAnnotationDeclaration namedOnPackage, IAnnotationDeclaration fullyQualifiedOnPackage, PackageDefinition p) {
+		IAnnotationDeclaration named = d.getAnnotation(CDIConstants.NAMED_QUALIFIER_TYPE_NAME);
+		IAnnotationDeclaration fullyQualified = d.getAnnotation(CDISeamSolderConstants.FULLY_QUALIFIED_ANNOTATION_TYPE_NAME);
+
+		String beanName = null;
+
+		if((fullyQualified != null || fullyQualifiedOnPackage != null) && (named != null || namedOnPackage != null)) {
+			//@FullyQualified
+			if(named == null) named = namedOnPackage;
+			String pkg = resolvePackageName(fullyQualified, fullyQualifiedOnPackage, d.getTypeDefinition(), p);
+			String simpleName = getSimpleBeanName(d, named);
+			beanName = (simpleName == null) ? null : pkg.length() > 0 ? pkg + "." + simpleName : simpleName;			
+		} else if(named == null && namedOnPackage != null) {
+			// @Named on package only
+			beanName = getSimpleBeanName(d, namedOnPackage);
+		}
+		
+		if(beanName != null) {
+			IMemberValuePair[] pairs = new IMemberValuePair[]{new MemberValuePair("value", beanName, IMemberValuePair.K_STRING)};
+			AnnotationDefinition n = context.getAnnotation(CDIConstants.NAMED_QUALIFIER_TYPE_NAME);
+			if(n != null) {
+				AnnotationLiteral l = new AnnotationLiteral(d.getResource(), "", new SourceRange(0, 0), pairs, n.getType());
+				if(named != null) d.removeAnnotation(named);
+				d.addAnnotation(l, context);
+			}
+		}
+
+	}
+
 
 	private List<String> getRequiredClasses(IAnnotated d) {
 		if (d == null)
@@ -113,6 +179,28 @@ public class CDISeamSolderCoreExtension implements ICDIExtension,
 			result.add(value.toString());
 		}
 		return result;
+	}
+
+	// @Exact
+	private void processExact(TypeDefinition typeDefinition, DefinitionContext context) {
+		List<FieldDefinition> fs = typeDefinition.getFields();
+		for (FieldDefinition f : fs) {
+			TypeDeclaration exact = getExactType(f, typeDefinition, context);
+			if (exact != null) {
+				f.setOverridenType(exact);
+			}
+		}
+
+		List<MethodDefinition> ms = typeDefinition.getMethods();
+		for (MethodDefinition m : ms) {
+			List<ParameterDefinition> ps = m.getParameters();
+			for (ParameterDefinition p : ps) {
+				TypeDeclaration exact = getExactType(p, typeDefinition, context);
+				if (exact != null) {
+					p.setOverridenType(exact);
+				}
+			}
+		}
 	}
 
 	private TypeDeclaration getExactType(IAnnotated annotated, TypeDefinition declaringType, DefinitionContext context) {
@@ -143,4 +231,54 @@ public class CDISeamSolderCoreExtension implements ICDIExtension,
 		}
 		return null;
 	}
+
+	private String resolvePackageName(IAnnotationDeclaration fullyQualified, IAnnotationDeclaration fullyQualifiedOnPackage, AbstractTypeDefinition t, PackageDefinition p) {
+		String contextClass = null;
+		IAnnotationDeclaration a = fullyQualified != null ? fullyQualified : fullyQualifiedOnPackage;
+		contextClass = getStringValue(a);
+		if(contextClass == null) {
+			contextClass = t == null ? "" : t.getQualifiedName();
+		} else if(fullyQualified != null && t != null) {
+			String resolved = EclipseJavaUtil.resolveType(t.getType(), contextClass);
+			if(resolved != null) contextClass = resolved;				
+		} else if(fullyQualifiedOnPackage != null) {
+			contextClass = p.resolveType(contextClass);
+		}
+		if("java.lang.Class".equals(contextClass)) {
+			contextClass = t.getType().getFullyQualifiedName();
+		}
+		int dot = contextClass.lastIndexOf('.');
+		return dot < 0 ? "" : contextClass.substring(0, dot);
+	}
+
+	private String getSimpleBeanName(AbstractMemberDefinition d, IAnnotationDeclaration named) {
+		String simpleName = null;
+		if(named != null) {
+			simpleName = getStringValue(named);
+		}
+		if(simpleName != null && simpleName.length() > 0) {
+			//do nothing
+		} else if(d instanceof TypeDefinition) {
+			simpleName = Introspector.decapitalize(((TypeDefinition)d).getType().getElementName());
+		} else if(d instanceof FieldDefinition) {
+			simpleName = ((FieldDefinition)d).getField().getElementName();
+		} else if(d instanceof MethodDefinition) {
+			MethodDefinition m = (MethodDefinition)d;
+			String mn = m.getMethod().getElementName();
+			if(BeanUtil.isGetter(m.getMethod())) {
+				simpleName = BeanUtil.getPropertyName(mn);
+			} else {
+				simpleName = mn;
+			}
+		}
+		
+		return simpleName;
+	}
+
+	private String getStringValue(IAnnotationDeclaration a) {
+		if(a == null) return null;
+		Object o = a.getMemberValue(null);
+		return o == null ? null : o.toString();
+	}
+
 }
