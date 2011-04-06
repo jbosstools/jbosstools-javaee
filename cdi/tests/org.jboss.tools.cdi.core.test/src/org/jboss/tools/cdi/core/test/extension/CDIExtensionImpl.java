a51102fad5f60f90b614004f2524e4cfc42d0f0b
Viacheslav Kabanovich <scabanovich@exadel.com>
2011-04-06 00:00:19 +0000

    JBIDE-3120 https://issues.jboss.org/browse/JBIDE-3120
    git-svn-id: http://svn.jboss.org/repos/jbosstools/trunk@30348 a97e2381-89e5-4abb-bab3-167db6db766c

--- cdi/tests/org.jboss.tools.cdi.core.test/src/org/jboss/tools/cdi/core/test/extension/CDIExtensionImpl.java
+++ cdi/tests/org.jboss.tools.cdi.core.test/src/org/jboss/tools/cdi/core/test/extension/CDIExtensionImpl.java
@@ -1,9 +1,9 @@
 package org.jboss.tools.cdi.core.test.extension;
 
-import org.jboss.tools.cdi.core.IBean;
+
+import org.jboss.tools.cdi.core.IRootDefinitionContext;
 import org.jboss.tools.cdi.core.extension.ICDIExtension;
 import org.jboss.tools.cdi.core.extension.feature.IProcessAnnotatedTypeFeature;
-import org.jboss.tools.cdi.internal.core.impl.definition.DefinitionContext;
 import org.jboss.tools.cdi.internal.core.impl.definition.TypeDefinition;
 
 public class CDIExtensionImpl implements ICDIExtension, IProcessAnnotatedTypeFeature {
@@ -16,7 +16,7 @@ public class CDIExtensionImpl implements ICDIExtension, IProcessAnnotatedTypeFea
 	}
 
 	public void processAnnotatedType(TypeDefinition typeDefinition,
-			DefinitionContext context) {
+			IRootDefinitionContext context) {
 		
 	}
 
