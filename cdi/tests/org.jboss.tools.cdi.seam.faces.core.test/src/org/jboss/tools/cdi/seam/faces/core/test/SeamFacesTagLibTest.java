e6f609ea73369f68946b2562557afab37de064dc
Alexey Kazakov <akazakov@exadel.com>
2011-04-25 20:11:07 +0000

    https://issues.jboss.org/browse/JBIDE-8572
    git-svn-id: http://svn.jboss.org/repos/jbosstools/trunk@30828 a97e2381-89e5-4abb-bab3-167db6db766c

--- cdi/tests/org.jboss.tools.cdi.seam.faces.core.test/src/org/jboss/tools/cdi/seam/faces/core/test/SeamFacesTagLibTest.java
+++ cdi/tests/org.jboss.tools.cdi.seam.faces.core.test/src/org/jboss/tools/cdi/seam/faces/core/test/SeamFacesTagLibTest.java
@@ -10,11 +10,47 @@
  ******************************************************************************/ 
 package org.jboss.tools.cdi.seam.faces.core.test;
 
+import java.util.HashSet;
+import java.util.Set;
+
 import junit.framework.TestCase;
+
+import org.jboss.tools.jst.web.kb.internal.proposal.CustomProposalType;
+import org.jboss.tools.jst.web.kb.internal.proposal.EnumerationProposalType;
+import org.jboss.tools.jst.web.kb.internal.taglib.CustomTagLibAttribute;
+import org.jboss.tools.jst.web.kb.taglib.CustomTagLibManager;
+import org.jboss.tools.jst.web.kb.taglib.IAttribute;
+import org.jboss.tools.jst.web.kb.taglib.IComponent;
+import org.jboss.tools.jst.web.kb.taglib.ICustomTagLibrary;
 
 /**
  * @author Alexey Kazakov
  */
 public class SeamFacesTagLibTest extends TestCase {
 
+	public void testCustomTagLibs() {
+		ICustomTagLibrary[] libs = CustomTagLibManager.getInstance().getLibraries();
+		boolean found = false;
+		for (ICustomTagLibrary lib : libs) {
+			if("http://jboss.org/seam/faces".equals(lib.getURI())) {
+				found = true;
+				IComponent component = lib.getComponent("viewAction");
+				assertNotNull(component);
+				IAttribute attribute = component.getAttribute("phase");
+				assertNotNull(attribute);
+				assertTrue(attribute instanceof CustomTagLibAttribute);
+				CustomTagLibAttribute customAttribute = (CustomTagLibAttribute)attribute;
+				CustomProposalType[] proposals = customAttribute.getProposals();
+				Set<EnumerationProposalType> enums = new HashSet<EnumerationProposalType>();
+				for (CustomProposalType proposal : proposals) {
+					if(proposal instanceof EnumerationProposalType) {
+						enums.add((EnumerationProposalType)proposal);
+					}
+				}
+				assertEquals(4, enums.size());
+				break;
+			}
+		}
+		assertTrue("Can't find http://jboss.org/seam/faces tag lib.", found);
+	}
 }
