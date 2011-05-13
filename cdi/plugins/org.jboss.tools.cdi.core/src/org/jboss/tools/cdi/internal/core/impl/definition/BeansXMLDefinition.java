16b2bdfda3d453fc08139c89245fdb9646f439ab
Viacheslav Kabanovich <scabanovich@exadel.com>
2011-05-13 23:01:00 +0000

    Removed unnecessary check for null.
    git-svn-id: http://svn.jboss.org/repos/jbosstools/trunk@31310 a97e2381-89e5-4abb-bab3-167db6db766c

--- cdi/plugins/org.jboss.tools.cdi.core/src/org/jboss/tools/cdi/internal/core/impl/definition/BeansXMLDefinition.java
+++ cdi/plugins/org.jboss.tools.cdi.core/src/org/jboss/tools/cdi/internal/core/impl/definition/BeansXMLDefinition.java
@@ -1,3 +1,13 @@
+/******************************************************************************* 
+ * Copyright (c) 2009-2011 Red Hat, Inc. 
+ * Distributed under license by Red Hat, Inc. All rights reserved. 
+ * This program is made available under the terms of the 
+ * Eclipse Public License v1.0 which accompanies this distribution, 
+ * and is available at http://www.eclipse.org/legal/epl-v10.html 
+ * 
+ * Contributors: 
+ * Red Hat, Inc. - initial API and implementation 
+ ******************************************************************************/ 
 package org.jboss.tools.cdi.internal.core.impl.definition;
 
 import java.util.HashSet;
@@ -7,6 +17,11 @@ import org.eclipse.core.runtime.IPath;
 import org.jboss.tools.common.model.XModelObject;
 import org.jboss.tools.common.text.INodeReference;
 
+/**
+ * 
+ * @author Viacheslav Kabanovich
+ *
+ */
 public class BeansXMLDefinition {
 	static String NODE_INTERCEPTORS = "Interceptors";
 	static String NODE_DECORATORS = "Decorators";
@@ -23,31 +38,34 @@ public class BeansXMLDefinition {
 	public BeansXMLDefinition() {}
 
 	public void setBeansXML(XModelObject beansXML) {
-		if(beansXML == null || !beansXML.getModelEntity().getName().startsWith("FileCDIBeans")) return;
-		XModelObject interceptorsObject = beansXML.getChildByPath(NODE_INTERCEPTORS);
-		if(interceptorsObject != null) {
-			XModelObject[] cs = interceptorsObject.getChildren();
-			for (XModelObject o: cs) {
-				interceptors.add(new XMLNodeReference(o, ATTR_CLASS));
+		if(beansXML.getModelEntity().getName().startsWith("FileCDIBeans")) {
+
+			XModelObject interceptorsObject = beansXML.getChildByPath(NODE_INTERCEPTORS);
+			if(interceptorsObject != null) {
+				XModelObject[] cs = interceptorsObject.getChildren();
+				for (XModelObject o: cs) {
+					interceptors.add(new XMLNodeReference(o, ATTR_CLASS));
+				}
 			}
-		}
-		XModelObject decoratorsObject = beansXML.getChildByPath(NODE_DECORATORS);
-		if(decoratorsObject != null) {
-			XModelObject[] cs = decoratorsObject.getChildren();
-			for (XModelObject o: cs) {
-				decorators.add(new XMLNodeReference(o, ATTR_CLASS));
+			XModelObject decoratorsObject = beansXML.getChildByPath(NODE_DECORATORS);
+			if(decoratorsObject != null) {
+				XModelObject[] cs = decoratorsObject.getChildren();
+				for (XModelObject o: cs) {
+					decorators.add(new XMLNodeReference(o, ATTR_CLASS));
+				}
 			}
-		}
-		XModelObject alternativesObject = beansXML.getChildByPath(NODE_ALTERNATIVES);
-		if(alternativesObject != null) {
-			XModelObject[] cs = alternativesObject.getChildren("CDIClass");
-			for (XModelObject o: cs) {
-				typeAlternatives.add(new XMLNodeReference(o, ATTR_CLASS));
+			XModelObject alternativesObject = beansXML.getChildByPath(NODE_ALTERNATIVES);
+			if(alternativesObject != null) {
+				XModelObject[] cs = alternativesObject.getChildren("CDIClass");
+				for (XModelObject o: cs) {
+					typeAlternatives.add(new XMLNodeReference(o, ATTR_CLASS));
+				}
+				cs = alternativesObject.getChildren("CDIStereotype");
+				for (XModelObject o: cs) {
+					stereotypeAlternatives.add(new XMLNodeReference(o, ATTR_STEREOTYPE));
+				}
 			}
-			cs = alternativesObject.getChildren("CDIStereotype");
-			for (XModelObject o: cs) {
-				stereotypeAlternatives.add(new XMLNodeReference(o, ATTR_STEREOTYPE));
-			}
+
 		}
 		
 	}
