d8aa6baad6e7894436f2c0c724dcd49d0bc07aa6
Viacheslav Kabanovich <scabanovich@exadel.com>
2011-06-01 20:28:09 +0000

    JBIDE-9028 https://issues.jboss.org/browse/JBIDE-9028
    git-svn-id: http://svn.jboss.org/repos/jbosstools/trunk@31748 a97e2381-89e5-4abb-bab3-167db6db766c

--- seam/plugins/org.jboss.tools.seam.core/src/org/jboss/tools/seam/internal/core/validation/SeamELValidationDelegate.java
+++ seam/plugins/org.jboss.tools.seam.core/src/org/jboss/tools/seam/internal/core/validation/SeamELValidationDelegate.java
@@ -1,5 +1,5 @@
 /******************************************************************************* 
- * Copyright (c) 2010 Red Hat, Inc. 
+ * Copyright (c) 2010-2011 Red Hat, Inc. 
  * Distributed under license by Red Hat, Inc. All rights reserved. 
  * This program is made available under the terms of the 
  * Eclipse Public License v1.0 which accompanies this distribution, 
@@ -12,10 +12,14 @@ package org.jboss.tools.seam.internal.core.validation;
 
 import org.eclipse.core.resources.IProject;
 import org.eclipse.core.runtime.CoreException;
+import org.jboss.tools.jst.web.kb.internal.validation.ELValidator;
+import org.jboss.tools.jst.web.kb.internal.validation.ValidatorManager;
 import org.jboss.tools.jst.web.kb.validation.IELValidationDelegate;
 import org.jboss.tools.jst.web.kb.validation.IValidatingProjectTree;
 import org.jboss.tools.seam.core.ISeamProject;
+import org.jboss.tools.seam.core.SeamCoreBuilder;
 import org.jboss.tools.seam.core.SeamCorePlugin;
+import org.jboss.tools.seam.core.SeamPreferences;
 
 /**
  * @author Alexey Kazakov
@@ -34,10 +38,22 @@ public class SeamELValidationDelegate implements IELValidationDelegate {
 	 */
 	public boolean shouldValidate(IProject project) {
 		try {
-			return project!=null && project.isAccessible() && project.hasNature(ISeamProject.NATURE_ID);
+			return project != null 
+					&& project.isAccessible() 
+					&& project.hasNature(ISeamProject.NATURE_ID)
+					&& validateBuilderOrder(project);
 		} catch (CoreException e) {
 			SeamCorePlugin.getDefault().logError(e);
 		}
 		return false;
 	}
+
+	private boolean validateBuilderOrder(IProject project) throws CoreException {
+		return ValidatorManager.validateBuilderOrder(project, getBuilderId(), ELValidator.ID + "-Seam", SeamPreferences.getInstance());
+	}
+
+	public String getBuilderId() {
+		return SeamCoreBuilder.BUILDER_ID;
+	}
+
 }
