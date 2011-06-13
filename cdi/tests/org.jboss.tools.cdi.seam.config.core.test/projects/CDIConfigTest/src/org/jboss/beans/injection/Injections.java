96922de64671b6300e59c4e792bcd1a2a03ee268
Viacheslav Kabanovich <scabanovich@exadel.com>
2011-06-13 23:20:00 +0000

    JBIDE-9110 https://issues.jboss.org/browse/JBIDE-9110
    git-svn-id: http://svn.jboss.org/repos/jbosstools/trunk@32072 a97e2381-89e5-4abb-bab3-167db6db766c

--- cdi/tests/org.jboss.tools.cdi.seam.config.core.test/projects/CDIConfigTest/src/org/jboss/beans/injection/Injections.java
+++ cdi/tests/org.jboss.tools.cdi.seam.config.core.test/projects/CDIConfigTest/src/org/jboss/beans/injection/Injections.java
@@ -9,12 +9,24 @@ import org.jboss.beans.test05.MyType1;
 
 public class Injections {
 
+	/**
+	 * Class bean defined in seam-config.xml
+	 */
 	@Inject MyBean5 b5;
 
+	/**
+	 * Virtual field producer bean defined in seam-config.xml
+	 */
 	@Inject @org.jboss.beans.test06.MyQualifier("one") String s;
 	
+	/**
+	 * Method producer bean defined in seam-config.xml
+	 */
 	@Inject @org.jboss.beans.test05.MyQualifier MyType1 t1;
 	
+	/**
+	 * Field producer bean defined in seam-config.xml
+	 */
 	@Inject @MyQualifier(kind="kind-04-3") MyType3 t3;
 
 }
