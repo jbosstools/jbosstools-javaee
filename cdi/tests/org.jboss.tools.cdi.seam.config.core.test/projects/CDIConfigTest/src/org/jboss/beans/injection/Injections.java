f10a6fd8068e22f5d36dc75669b235553a57f90b
Viacheslav Kabanovich <scabanovich@exadel.com>
2011-06-13 23:08:25 +0000

    JBIDE-9110 https://issues.jboss.org/browse/JBIDE-9110
    git-svn-id: http://svn.jboss.org/repos/jbosstools/trunk@32071 a97e2381-89e5-4abb-bab3-167db6db766c

--- cdi/tests/org.jboss.tools.cdi.seam.config.core.test/projects/CDIConfigTest/src/org/jboss/beans/injection/Injections.java
+++ cdi/tests/org.jboss.tools.cdi.seam.config.core.test/projects/CDIConfigTest/src/org/jboss/beans/injection/Injections.java
@@ -2,13 +2,19 @@ package org.jboss.beans.injection;
 
 import javax.inject.Inject;
 
+import org.jboss.beans.test04.MyBean5;
 import org.jboss.beans.test04.MyQualifier;
 import org.jboss.beans.test04.MyType3;
+import org.jboss.beans.test05.MyType1;
 
 public class Injections {
 
-	@Inject
-	@MyQualifier(kind="kind-04-3")
-	MyType3 b4;
+	@Inject MyBean5 b5;
+
+	@Inject @org.jboss.beans.test06.MyQualifier("one") String s;
+	
+	@Inject @org.jboss.beans.test05.MyQualifier MyType1 t1;
+	
+	@Inject @MyQualifier(kind="kind-04-3") MyType3 t3;
 
 }
