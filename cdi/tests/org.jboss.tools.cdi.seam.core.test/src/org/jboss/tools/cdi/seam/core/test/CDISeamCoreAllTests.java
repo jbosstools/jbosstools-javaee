08a5c51b1a6e120ff06b0e55d2983658330f37a0
Alexey Kazakov <akazakov@exadel.com>
2011-06-30 18:49:01 +0000

    Share project "org.jboss.tools.cdi.seam.core.test" into "https://svn.jboss.org/repos/jbosstools" https://issues.jboss.org/browse/JBIDE-9087
    git-svn-id: http://svn.jboss.org/repos/jbosstools/trunk@32506 a97e2381-89e5-4abb-bab3-167db6db766c

---
+++ cdi/tests/org.jboss.tools.cdi.seam.core.test/src/org/jboss/tools/cdi/seam/core/test/CDISeamCoreAllTests.java
@@ -0,0 +1,31 @@
+/******************************************************************************* 
+ * Copyright (c) 2011 Red Hat, Inc. 
+ * Distributed under license by Red Hat, Inc. All rights reserved. 
+ * This program is made available under the terms of the 
+ * Eclipse Public License v1.0 which accompanies this distribution, 
+ * and is available at http://www.eclipse.org/legal/epl-v10.html 
+ * 
+ * Contributors: 
+ * Red Hat, Inc. - initial API and implementation 
+ ******************************************************************************/ 
+package org.jboss.tools.cdi.seam.core.test;
+
+import junit.framework.Test;
+import junit.framework.TestSuite;
+
+import org.eclipse.jdt.internal.core.JavaModelManager;
+
+/**
+ * @author Alexey Kazakov
+ */
+public class CDISeamCoreAllTests {
+
+	public static Test suite() {
+		JavaModelManager.getIndexManager().disable();
+
+		TestSuite suite = new TestSuite("Seam Core Tests");
+
+		suite.addTestSuite(SeamResourceBundlesTest.class);
+		return suite;
+	}
+}
