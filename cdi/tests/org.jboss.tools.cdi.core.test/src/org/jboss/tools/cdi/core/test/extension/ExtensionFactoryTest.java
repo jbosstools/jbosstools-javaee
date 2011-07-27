6a00c2863ed3011d651183559482a0b5e5240781
Viacheslav Kabanovich <scabanovich@exadel.com>
2011-07-27 18:29:32 +0000

    JBIDE-9402 https://issues.jboss.org/browse/JBIDE-9402
    git-svn-id: http://svn.jboss.org/repos/jbosstools/trunk@33265 a97e2381-89e5-4abb-bab3-167db6db766c

--- cdi/tests/org.jboss.tools.cdi.core.test/src/org/jboss/tools/cdi/core/test/extension/ExtensionFactoryTest.java
+++ cdi/tests/org.jboss.tools.cdi.core.test/src/org/jboss/tools/cdi/core/test/extension/ExtensionFactoryTest.java
@@ -3,15 +3,21 @@ package org.jboss.tools.cdi.core.test.extension;
 import java.util.Set;
 
 import org.jboss.tools.cdi.core.extension.CDIExtensionFactory;
+import org.jboss.tools.cdi.core.extension.ICDIExtension;
 import org.jboss.tools.cdi.core.extension.feature.IProcessAnnotatedTypeFeature;
 
 import junit.framework.TestCase;
 
 public class ExtensionFactoryTest extends TestCase {
+	static String RUNTIME = "org.jboss.tools.cdi.core.fake.FakeExtension";
+	static String IMPL = "org.jboss.tools.cdi.core.test.extension.CDIExtensionImpl";
 
 	public void testExtensionFactory() throws Exception {
-		Set<String> clss = CDIExtensionFactory.getInstance().getExtensionClassesByFeature(IProcessAnnotatedTypeFeature.class);
-		System.out.println(clss.size());
+		Set<String> set = CDIExtensionFactory.getInstance().getExtensionClassesByRuntime(RUNTIME);
+		assertTrue(set.contains(IMPL));
+		ICDIExtension ext = CDIExtensionFactory.getInstance().createExtensionInstance(IMPL);
+		Set<Class<?>> fs = CDIExtensionFactory.getInstance().getFeatures(ext);
+		assertTrue(fs.contains(IProcessAnnotatedTypeFeature.class));
 	}
 
 }
