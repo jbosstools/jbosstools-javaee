790b6ef6442cdb450b95e29e0c6b1ea813b853e1
Viacheslav Kabanovich <scabanovich@exadel.com>
2011-08-16 17:03:36 +0000

    JBIDE-9483 https://issues.jboss.org/browse/JBIDE-9483
    git-svn-id: http://svn.jboss.org/repos/jbosstools/trunk@33979 a97e2381-89e5-4abb-bab3-167db6db766c

--- cdi/tests/org.jboss.tools.cdi.core.test/src/org/jboss/tools/cdi/core/test/extension/ExtensionsInSrsAndUsedProjectTest.java
+++ cdi/tests/org.jboss.tools.cdi.core.test/src/org/jboss/tools/cdi/core/test/extension/ExtensionsInSrsAndUsedProjectTest.java
@@ -12,10 +12,12 @@ public class ExtensionsInSrsAndUsedProjectTest  extends TestCase {
 	protected static String PLUGIN_ID = "org.jboss.tools.cdi.core.test";
 	IProject project1 = null;
 	IProject project2 = null;
+	IProject project3 = null;
 
 	public ExtensionsInSrsAndUsedProjectTest() {
 		project1 = DependentProjectTest.getTestProject(project1, "/projects/CDITest1", "CDITest1");
 		project2 = DependentProjectTest.getTestProject(project2, "/projects/CDITest2", "CDITest2");
+		project3 = DependentProjectTest.getTestProject(project3, "/projects/CDITest3", "CDITest3");
 	}
 
 	public void testRuntimes() {
@@ -24,6 +26,12 @@ public class ExtensionsInSrsAndUsedProjectTest  extends TestCase {
 		assertTrue(cdi2.getExtensionManager().isCDIExtensionAvailable("c.d.e"));
 		//Extension declared in src of project1
 		assertTrue(cdi2.getExtensionManager().isCDIExtensionAvailable("a.b.c"));
+
+		CDICoreNature cdi3 = CDICorePlugin.getCDI(project3, true);
+		//Extension declared in src of project2
+		assertTrue(cdi3.getExtensionManager().isCDIExtensionAvailable("c.d.e"));
+		//Extension declared in src of project1
+		assertTrue(cdi3.getExtensionManager().isCDIExtensionAvailable("a.b.c"));
 	}
 
 }
