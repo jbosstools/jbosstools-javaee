70f5403a4637639673fdf8727e096b149c582bed
Viacheslav Kabanovich <scabanovich@exadel.com>
2011-09-09 22:57:44 +0000

    JBIDE-9347 https://issues.jboss.org/browse/JBIDE-9347 Initial implementation of Assignable Beans dialog.
    git-svn-id: http://svn.jboss.org/repos/jbosstools/trunk@34621 a97e2381-89e5-4abb-bab3-167db6db766c

--- cdi/plugins/org.jboss.tools.cdi.seam.text.ext/src/org/jboss/tools/cdi/seam/text/ext/hyperlink/SeamConfigInjectedPointHyperlink.java
+++ cdi/plugins/org.jboss.tools.cdi.seam.text.ext/src/org/jboss/tools/cdi/seam/text/ext/hyperlink/SeamConfigInjectedPointHyperlink.java
@@ -10,29 +10,12 @@
  ******************************************************************************/
 package org.jboss.tools.cdi.seam.text.ext.hyperlink;
 
-import org.eclipse.core.resources.IFile;
-import org.eclipse.core.resources.IResource;
 import org.eclipse.jface.text.IDocument;
 import org.eclipse.jface.text.IRegion;
-import org.eclipse.jface.text.TextSelection;
-import org.eclipse.jface.viewers.ISelectionProvider;
-import org.eclipse.osgi.util.NLS;
-import org.eclipse.ui.IEditorPart;
-import org.eclipse.ui.IWorkbenchPage;
-import org.eclipse.ui.IWorkbenchWindow;
-import org.eclipse.ui.PartInitException;
-import org.eclipse.ui.ide.IDE;
-import org.eclipse.ui.texteditor.ITextEditor;
 import org.jboss.tools.cdi.core.IBean;
-import org.jboss.tools.cdi.core.IProducerField;
-import org.jboss.tools.cdi.core.IProducerMethod;
 import org.jboss.tools.cdi.internal.core.impl.AbstractBeanElement;
 import org.jboss.tools.cdi.seam.config.core.definition.IConfigDefinition;
-import org.jboss.tools.cdi.seam.text.ext.CDISeamExtMessages;
-import org.jboss.tools.cdi.seam.text.ext.CDISeamExtPlugin;
 import org.jboss.tools.cdi.text.ext.hyperlink.InjectedPointHyperlink;
-import org.jboss.tools.common.editor.ObjectMultiPageEditor;
-import org.jboss.tools.common.model.ui.editor.EditorPartWrapper;
 import org.jboss.tools.common.text.ITextSourceReference;
 
 public class SeamConfigInjectedPointHyperlink extends InjectedPointHyperlink {
@@ -42,31 +25,7 @@ public class SeamConfigInjectedPointHyperlink extends InjectedPointHyperlink {
 	}
 
 	protected void doHyperlink(IRegion region) {
-		IEditorPart part = null;
-		ITextSourceReference source = ((AbstractBeanElement)bean).getDefinition().getOriginalDefinition();
-		IFile resource = (IFile)source.getResource();
-		IWorkbenchWindow window = CDISeamExtPlugin.getDefault().getWorkbench().getActiveWorkbenchWindow();
-		if (window == null)	return;
-		IWorkbenchPage page = window.getActivePage();
-		try {
-			part = IDE.openEditor(page, resource);
-		} catch (PartInitException e) {
-			CDISeamExtPlugin.log(e);
-		}
-		if(part instanceof EditorPartWrapper) {
-			part = ((EditorPartWrapper)part).getEditor();
-		}
-		if(part instanceof ObjectMultiPageEditor) {
-			ObjectMultiPageEditor mpe = (ObjectMultiPageEditor)part;
-			ITextEditor textEditor = (ITextEditor)mpe.getAdapter(ITextEditor.class);
-			if(textEditor != null) {
-				mpe.setActiveEditor(textEditor);
-				part = textEditor;
-			}
-		}
-		if(part != null) {
-			part.getEditorSite().getSelectionProvider().setSelection(new TextSelection(source.getStartPosition(), source.getLength()));
-		}
+		bean.open();
 	}
 
 	@Override
--- cdi/tests/org.jboss.tools.cdi.seam.text.ext.test/src/org/jboss/tools/cdi/seam/text/ext/test/InjectedPointHyperlinkTest.java
+++ cdi/tests/org.jboss.tools.cdi.seam.text.ext.test/src/org/jboss/tools/cdi/seam/text/ext/test/InjectedPointHyperlinkTest.java
@@ -6,6 +6,7 @@ import java.util.Set;
 import org.jboss.tools.cdi.core.IBean;
 import org.jboss.tools.cdi.seam.solder.core.test.SeamSolderTest;
 import org.jboss.tools.cdi.text.ext.CDIExtensionsMessages;
+import org.jboss.tools.cdi.text.ext.hyperlink.AssignableBeansHyperlink;
 import org.jboss.tools.cdi.text.ext.hyperlink.InjectedPointHyperlink;
 import org.jboss.tools.cdi.text.ext.hyperlink.InjectedPointHyperlinkDetector;
 import org.jboss.tools.cdi.text.ext.test.CDIHyperlinkTestUtil;
@@ -22,27 +23,35 @@ public class InjectedPointHyperlinkTest extends SeamSolderTest {
 		ArrayList<TestRegion> regionList = new ArrayList<TestRegion>();
 		regionList.add(new TestRegion(/*97, 6*/"Inject",   new TestHyperlink[]{
 			new TestHyperlink(InjectedPointHyperlink.class, CDIExtensionsMessages.CDI_INJECTED_POINT_HYPERLINK_OPEN_INJECT_BEAN+ " MessageManager.messageDispatcherProducer()", bean),
+			new TestHyperlink(AssignableBeansHyperlink.class, CDIExtensionsMessages.CDI_INJECTED_POINT_HYPERLINK_SHOW_ASSIGNABLE)
 		})); // Inject
 		regionList.add(new TestRegion(/*106, 7*/"Durable",   new TestHyperlink[]{
 			new TestHyperlink(InjectedPointHyperlink.class, CDIExtensionsMessages.CDI_INJECTED_POINT_HYPERLINK_OPEN_INJECT_BEAN+ " MessageManager.messageDispatcherProducer()", bean),
+			new TestHyperlink(AssignableBeansHyperlink.class, CDIExtensionsMessages.CDI_INJECTED_POINT_HYPERLINK_SHOW_ASSIGNABLE)
 		})); // Logger
 		regionList.add(new TestRegion(/*115, 28*/"MessageDispatcher dispatcher",   new TestHyperlink[]{
 			new TestHyperlink(InjectedPointHyperlink.class, CDIExtensionsMessages.CDI_INJECTED_POINT_HYPERLINK_OPEN_INJECT_BEAN+ " MessageManager.messageDispatcherProducer()", bean),
+			new TestHyperlink(AssignableBeansHyperlink.class, CDIExtensionsMessages.CDI_INJECTED_POINT_HYPERLINK_SHOW_ASSIGNABLE)
 		})); // logger
 		regionList.add(new TestRegion(/*148, 6*/"Inject",   new TestHyperlink[]{
 			new TestHyperlink(InjectedPointHyperlink.class, CDIExtensionsMessages.CDI_INJECTED_POINT_HYPERLINK_OPEN_INJECT_BEAN+ " MessageManager.getPolicy()", bean),
+			new TestHyperlink(AssignableBeansHyperlink.class, CDIExtensionsMessages.CDI_INJECTED_POINT_HYPERLINK_SHOW_ASSIGNABLE)
 		})); // logger
 		regionList.add(new TestRegion(/*157, 7*/"Durable",   new TestHyperlink[]{
 			new TestHyperlink(InjectedPointHyperlink.class, CDIExtensionsMessages.CDI_INJECTED_POINT_HYPERLINK_OPEN_INJECT_BEAN+ " MessageManager.getPolicy()", bean),
+			new TestHyperlink(AssignableBeansHyperlink.class, CDIExtensionsMessages.CDI_INJECTED_POINT_HYPERLINK_SHOW_ASSIGNABLE)
 		})); // logger
 		regionList.add(new TestRegion(/*166, 23*/"DispatcherPolicy policy",   new TestHyperlink[]{
 			new TestHyperlink(InjectedPointHyperlink.class, CDIExtensionsMessages.CDI_INJECTED_POINT_HYPERLINK_OPEN_INJECT_BEAN+ " MessageManager.getPolicy()", bean),
+			new TestHyperlink(AssignableBeansHyperlink.class, CDIExtensionsMessages.CDI_INJECTED_POINT_HYPERLINK_SHOW_ASSIGNABLE)
 		})); // logger
 		regionList.add(new TestRegion(/*281, 31*/"Durable DispatcherPolicy policy",   new TestHyperlink[]{
 			new TestHyperlink(InjectedPointHyperlink.class, CDIExtensionsMessages.CDI_INJECTED_POINT_HYPERLINK_OPEN_INJECT_BEAN+ " MessageManager.getPolicy()", bean),
+			new TestHyperlink(AssignableBeansHyperlink.class, CDIExtensionsMessages.CDI_INJECTED_POINT_HYPERLINK_SHOW_ASSIGNABLE)
 		})); // logger
 		regionList.add(new TestRegion(/*318, 6*/"policy",   new TestHyperlink[]{
 			new TestHyperlink(InjectedPointHyperlink.class, CDIExtensionsMessages.CDI_INJECTED_POINT_HYPERLINK_OPEN_INJECT_BEAN+ " MessageManager.getPolicy()", bean),
+			new TestHyperlink(AssignableBeansHyperlink.class, CDIExtensionsMessages.CDI_INJECTED_POINT_HYPERLINK_SHOW_ASSIGNABLE)
 		})); // logger
 
 		CDIHyperlinkTestUtil.checkRegions(getTestProject(), "src/org/jboss/generic2/DurableMessageLogger.java", regionList, new InjectedPointHyperlinkDetector());
