6f97148e13f7007410e8f5abf9bc699f26b13197
Alexey Kazakov <akazakov@exadel.com>
2011-09-12 20:39:14 +0000

    https://issues.jboss.org/browse/JBIDE-9685 Seam JMS: CDI validator should be aware of JMS resource injections
    git-svn-id: http://svn.jboss.org/repos/jbosstools/trunk@34647 a97e2381-89e5-4abb-bab3-167db6db766c

--- cdi/tests/org.jboss.tools.cdi.core.test/resources/tck/tests/jbt/core/TestInjection.java
+++ cdi/tests/org.jboss.tools.cdi.core.test/resources/tck/tests/jbt/core/TestInjection.java
@@ -4,18 +4,20 @@ import javax.inject.Inject;
 
 public class TestInjection {
 
-	@Inject @TestQualifier Object i1;   // has @TestQualifier
-	@Inject @TestQualifier1 Object i2;  // has @TestQualifier and @TestQualifier1
-	@Inject @TestQualifier2 Object i3;  // has @TestQualifier, @TestQualifier1 and @TestQualifier2
-	@Inject @TestQualifier3 Object i4;  // @TestQualifier3
+	@Inject @TestQualifier Test i1;   // has @TestQualifier
+	@Inject @TestQualifier1 Test i2;  // has @TestQualifier and @TestQualifier1
+	@Inject @TestQualifier2 Test i3;  // has @TestQualifier, @TestQualifier1 and @TestQualifier2
+	@Inject @TestQualifier3 Test i4;  // @TestQualifier3
 
-	@Inject @TestQualifier3 @TestQualifier2 Object i5;  // has @TestQualifier, @TestQualifier1, @TestQualifier2 and @TestQualifier3
-	@Inject @TestQualifier2 @TestQualifier3 Object i6;  // has @TestQualifier, @TestQualifier1, @TestQualifier2 and @TestQualifier3
-	@Inject @TestQualifier Object i7;
+	@Inject @TestQualifier3 @TestQualifier2 Test i5;  // has @TestQualifier, @TestQualifier1, @TestQualifier2 and @TestQualifier3
+	@Inject @TestQualifier2 @TestQualifier3 Test i6;  // has @TestQualifier, @TestQualifier1, @TestQualifier2 and @TestQualifier3
+	@Inject @TestQualifier Test i7;
 
-	@Inject @TestStereotype Object i8;   // has @TestQualifier, @TestQualifier1, @TestQualifier2 and @TestQualifier3
+	@Inject @TestStereotype Test i8;   // has @TestQualifier, @TestQualifier1, @TestQualifier2 and @TestQualifier3
 
 	@Inject
-	public void foo(@TestQualifier2 @TestQualifier3 Object i8) {  // has @TestQualifier, @TestQualifier1, @TestQualifier2 and @TestQualifier3
+	public void foo(@TestQualifier2 @TestQualifier3 Test i8) {  // has @TestQualifier, @TestQualifier1, @TestQualifier2 and @TestQualifier3
 	}
+
+	public static class Test {}
 }
