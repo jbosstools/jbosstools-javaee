1a1390bef18c84cddb130f0f9bd4d51c283aecb6
Alexey Kazakov <akazakov@exadel.com>
2011-09-20 20:07:44 +0000

    fixed compilation error
    git-svn-id: http://svn.jboss.org/repos/jbosstools/trunk@34891 a97e2381-89e5-4abb-bab3-167db6db766c

--- cdi/tests/org.jboss.tools.cdi.ui.test/src/org/jboss/tools/cdi/ui/test/preferences/CDIPreferencePageTest.java
+++ cdi/tests/org.jboss.tools.cdi.ui.test/src/org/jboss/tools/cdi/ui/test/preferences/CDIPreferencePageTest.java
@@ -1,28 +1,28 @@
+/******************************************************************************* 
+ * Copyright (c) 2009-2011 Red Hat, Inc. 
+ * Distributed under license by Red Hat, Inc. All rights reserved. 
+ * This program is made available under the terms of the 
+ * Eclipse Public License v1.0 which accompanies this distribution, 
+ * and is available at http://www.eclipse.org/legal/epl-v10.html 
+ * 
+ * Contributors: 
+ * Red Hat, Inc. - initial API and implementation 
+ ******************************************************************************/ 
 package org.jboss.tools.cdi.ui.test.preferences;
 
-import junit.framework.TestCase;
-
-import org.eclipse.jface.preference.PreferenceDialog;
-import org.eclipse.ui.PlatformUI;
-import org.eclipse.ui.dialogs.PreferencesUtil;
 import org.jboss.tools.cdi.ui.preferences.CDIPreferencePage;
 import org.jboss.tools.cdi.ui.preferences.CDISettingsPreferencePage;
 import org.jboss.tools.cdi.ui.preferences.CDIValidatorPreferencePage;
 import org.jboss.tools.test.util.PreferencePageAbstractTest;
 
-
 public class CDIPreferencePageTest extends PreferencePageAbstractTest{
-	
 
-	
 	public void testCDIPreferencePageIsCreated() {
-		PreferenceDialog prefDialog = createPreferenceDialog(CDIPreferencePage.ID);
-		assertTrue("Selected page is not an instance of CDIPreferencePage", isPreferencePageIsCreated(CDISettingsPreferencePage.ID, CDIPreferencePage.class));
-		
+		createPreferenceDialog(CDIPreferencePage.ID);
+		assertTrue("Selected page is not an instance of CDIPreferencePage", createPreferencePage(CDISettingsPreferencePage.ID, CDIPreferencePage.class));
 	}
-	
+
 	public void testCDIValidatorPreferencePageIsCreated() {
-		PreferenceDialog prefDialog = createPreferenceDialog(CDIValidatorPreferencePage.PREF_ID);
-		assertTrue("Selected page is not an instance of CDIPreferencePage", isPreferencePageIsCreated(CDIValidatorPreferencePage.PREF_ID, CDIValidatorPreferencePage.class));
+		assertTrue("Selected page is not an instance of CDIPreferencePage", createPreferencePage(CDIValidatorPreferencePage.PREF_ID, CDIValidatorPreferencePage.class));
 	}
 }
