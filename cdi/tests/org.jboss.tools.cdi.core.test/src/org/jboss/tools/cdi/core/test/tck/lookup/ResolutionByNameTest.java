3ec375eef453ec59c66ac4b939689bf465eabf72
Viacheslav Kabanovich <scabanovich@exadel.com>
2011-09-29 23:33:20 +0000

    JBIDE-9795 https://issues.jboss.org/browse/JBIDE-9795 Test added.
    git-svn-id: http://svn.jboss.org/repos/jbosstools/trunk@35183 a97e2381-89e5-4abb-bab3-167db6db766c

--- cdi/tests/org.jboss.tools.cdi.core.test/src/org/jboss/tools/cdi/core/test/tck/lookup/ResolutionByNameTest.java
+++ cdi/tests/org.jboss.tools.cdi.core.test/src/org/jboss/tools/cdi/core/test/tck/lookup/ResolutionByNameTest.java
@@ -16,9 +16,16 @@ import java.util.Set;
 import org.eclipse.core.resources.IFile;
 import org.eclipse.core.runtime.CoreException;
 import org.eclipse.core.runtime.IPath;
+import org.eclipse.jdt.core.IField;
+import org.jboss.tools.cdi.core.CDIConstants;
 import org.jboss.tools.cdi.core.IBean;
+import org.jboss.tools.cdi.core.IClassBean;
+import org.jboss.tools.cdi.core.IInjectionPoint;
+import org.jboss.tools.cdi.core.IInjectionPointField;
+import org.jboss.tools.cdi.core.IProducer;
 import org.jboss.tools.cdi.core.test.tck.TCKTest;
 import org.jboss.tools.cdi.internal.core.impl.CDIProject;
+import org.jboss.tools.common.java.IAnnotationDeclaration;
 
 /**
  * @author Alexey Kazakov
@@ -60,6 +67,45 @@ public class ResolutionByNameTest extends TCKTest {
 			assertContainsBeanClass(beans, "org.jboss.jsr299.tck.tests.lookup.byname.Sole");
 		} finally {
 			old = ((CDIProject)cdiProject).replaceBeanXML(old);
+		}
+	}
+
+	/**
+	 * Abstract class NotBean is annotated @Named("abstractClass")
+	 * There is no bean with bean name "abstractClass" for EL or injections, 
+	 * but instance of IClassBean created for NotBean can be obtained through 
+	 * its members available in model (e.g. producers)
+	 * 
+	 * @throws CoreException
+	 */
+	public void testAbstractClassAnnotatedNamed() throws CoreException {
+		String abstractClass = "abstractClass";
+		Set<IBean> beans = cdiProject.getBeans(abstractClass, false);
+		assertTrue(beans.isEmpty());
+		
+		beans = cdiProject.getBeans("producerInAbstractClass", false);
+		assertEquals(1, beans.size());
+		IBean b = beans.iterator().next();
+		assertTrue(b instanceof IProducer);
+
+		IClassBean cb = ((IProducer)b).getClassBean();
+		assertNotNull(cb);
+		IAnnotationDeclaration named = cb.getAnnotation(CDIConstants.NAMED_QUALIFIER_TYPE_NAME);
+		assertEquals(abstractClass, named.getMemberValue(null));
+		
+		Set<IInjectionPoint> injections = cdiProject.getInjections("org.jboss.jsr299.tck.tests.jbt.lookup.NotBean");
+		assertEquals(2, injections.size());
+		for (IInjectionPoint p: injections) {
+			assertTrue(p instanceof IInjectionPointField);
+			IField f = ((IInjectionPointField)p).getField();
+			Set<IBean> bs = cdiProject.getBeans(false, p);
+			if("f1".endsWith(f.getElementName())) {
+				assertEquals("Injection field f1 cannot be resolved to abstract class bean.", 0, bs.size());
+			} else if("f2".endsWith(f.getElementName())) {
+				assertEquals("Injection field f2 should be resolved to producer.", 1, bs.size());
+			} else {
+				fail("Unexpected field: " + f.getElementName());
+			}
 		}
 	}
 }
