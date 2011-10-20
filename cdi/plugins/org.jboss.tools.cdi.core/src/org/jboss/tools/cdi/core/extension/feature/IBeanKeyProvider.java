321cf449e73ca80c244dda68c2227172a111b77f
Viacheslav Kabanovich <scabanovich@exadel.com>
2011-10-20 19:49:40 +0000

    JBIDE-9244 https://issues.jboss.org/browse/JBIDE-9244 Comments to new interface are added.
    git-svn-id: http://svn.jboss.org/repos/jbosstools/trunk@35863 a97e2381-89e5-4abb-bab3-167db6db766c

--- cdi/plugins/org.jboss.tools.cdi.core/src/org/jboss/tools/cdi/core/extension/feature/IBeanKeyProvider.java
+++ cdi/plugins/org.jboss.tools.cdi.core/src/org/jboss/tools/cdi/core/extension/feature/IBeanKeyProvider.java
@@ -1,7 +1,33 @@
+/******************************************************************************* 
+ * Copyright (c) 2011 Red Hat, Inc. 
+ * Distributed under license by Red Hat, Inc. All rights reserved. 
+ * This program is made available under the terms of the 
+ * Eclipse Public License v1.0 which accompanies this distribution, 
+ * and is available at http://www.eclipse.org/legal/epl-v10.html 
+ * 
+ * Contributors: 
+ * Red Hat, Inc. - initial API and implementation 
+ ******************************************************************************/
 package org.jboss.tools.cdi.core.extension.feature;
 
 import org.jboss.tools.cdi.core.IBean;
 
+/**
+ * 
+ * @author Viacheslav Kabanovich
+ *
+ */
 public interface IBeanKeyProvider extends ICDIFeature {
+	/**
+	 * Returns a key for the bean that allows to link relevant resources 
+	 * to the resource that declares the bean. Relevance is determined 
+	 * by consistency of the incremental validation. The key needs not to be unique, 
+	 * it just has to provide low probability of irrelevant links. 
+	 * Implementation does not have to provide key for any bean.
+	 * Method may return null.
+	 * 
+	 * @param bean
+	 * @return a key for the bean to be used by the incremental validation for linking resources or null
+	 */
 	public String getKey(IBean bean);
 }
