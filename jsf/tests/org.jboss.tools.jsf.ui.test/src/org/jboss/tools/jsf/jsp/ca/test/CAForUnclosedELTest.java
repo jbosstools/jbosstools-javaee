d1594cf96ad91d81c75b9eeadcbd655d92e0c78e
Viacheslav Kabanovich <scabanovich@exadel.com>
2011-12-23 01:37:19 +0000

    JBIDE-10565 https://issues.jboss.org/browse/JBIDE-10565 Test added.
    git-svn-id: http://svn.jboss.org/repos/jbosstools/trunk@37545 a97e2381-89e5-4abb-bab3-167db6db766c

--- jsf/tests/org.jboss.tools.jsf.ui.test/src/org/jboss/tools/jsf/jsp/ca/test/CAForUnclosedELTest.java
+++ jsf/tests/org.jboss.tools.jsf.ui.test/src/org/jboss/tools/jsf/jsp/ca/test/CAForUnclosedELTest.java
@@ -1,5 +1,12 @@
 package org.jboss.tools.jsf.jsp.ca.test;
 
+import java.util.List;
+
+import org.eclipse.jface.text.BadLocationException;
+import org.eclipse.jface.text.FindReplaceDocumentAdapter;
+import org.eclipse.jface.text.IRegion;
+import org.eclipse.jface.text.contentassist.ICompletionProposal;
+import org.jboss.tools.common.base.test.contentassist.CATestUtil;
 import org.jboss.tools.jst.jsp.test.ca.ContentAssistantTestCase;
 import org.jboss.tools.test.util.TestProjectProvider;
 
@@ -27,5 +34,17 @@ public class CAForUnclosedELTest extends ContentAssistantTestCase{
 
 		checkProposals(PAGE_NAME, "<ui:define name=\"pageTitle#{  \"", 30, proposals, false);
 		checkProposals(PAGE_NAME, "<ui:define name=\"pageHeader\">Greeting Page#{   </ui:define>", 46, proposals, false);
+	}
+
+	public void testCAInELStartToken() throws BadLocationException {
+		openEditor(PAGE_NAME);
+		
+		IRegion reg = new FindReplaceDocumentAdapter(document).find(0, "<ui:define name=\"pageTitle#{  \"", true, true, false, false);
+		List<ICompletionProposal> res = CATestUtil.collectProposals(contentAssistant, viewer, reg.getOffset() + 27);
+		boolean found = compareProposal("person", null, res.toArray(new ICompletionProposal[0]));
+		assertFalse(found);
+		res = CATestUtil.collectProposals(contentAssistant, viewer, reg.getOffset() + 28);
+		found = compareProposal("person", null, res.toArray(new ICompletionProposal[0]));
+		assertTrue(found);
 	}
 }
