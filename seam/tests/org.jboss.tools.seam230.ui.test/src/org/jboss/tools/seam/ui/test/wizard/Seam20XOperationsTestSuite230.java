081b7bc7b57bb0e7bca2cc3cb976c2bbc1b410f2
Alexey Kazakov <akazakov@exadel.com>
2012-01-20 00:58:39 +0000

    https://issues.jboss.org/browse/JBIDE-10679 Seam facet version 2.3
    git-svn-id: http://svn.jboss.org/repos/jbosstools/trunk@37984 a97e2381-89e5-4abb-bab3-167db6db766c

---
+++ seam/tests/org.jboss.tools.seam230.ui.test/src/org/jboss/tools/seam/ui/test/wizard/Seam20XOperationsTestSuite230.java
@@ -0,0 +1,18 @@
+/*******************************************************************************
+ * Copyright (c) 2012 Red Hat, Inc.
+ * Distributed under license by Red Hat, Inc. All rights reserved.
+ * This program is made available under the terms of the
+ * Eclipse Public License v1.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors:
+ *     Red Hat, Inc. - initial API and implementation
+ ******************************************************************************/
+package org.jboss.tools.seam.ui.test.wizard;
+
+/**
+ * @author Alexey Kazakov
+ */
+public class Seam20XOperationsTestSuite230 extends Seam20XOperationsTestSuite201GABase {
+
+}
