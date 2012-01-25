5db1fea91167db7a74cff7eab3ed128c2665af8f
Viacheslav Kabanovich <scabanovich@exadel.com>
2012-01-25 00:46:46 +0000

    JBIDE-10703 https://issues.jboss.org/browse/JBIDE-10703 SeamExtPlugin is modified to follow the common pattern.
    git-svn-id: http://svn.jboss.org/repos/jbosstools/trunk@38150 a97e2381-89e5-4abb-bab3-167db6db766c

--- seam/plugins/org.jboss.tools.seam.text.ext/src/org/jboss/tools/seam/text/ext/SeamExtPlugin.java
+++ seam/plugins/org.jboss.tools.seam.text.ext/src/org/jboss/tools/seam/text/ext/SeamExtPlugin.java
@@ -2,7 +2,6 @@ package org.jboss.tools.seam.text.ext;
 
 import org.jboss.tools.common.log.BaseUIPlugin;
 import org.jboss.tools.common.log.IPluginLog;
-import org.osgi.framework.BundleContext;
 
 /**
  * The activator class controls the plug-in life cycle
@@ -19,24 +18,7 @@ public class SeamExtPlugin extends BaseUIPlugin {
 	 * The constructor
 	 */
 	public SeamExtPlugin() {
-	}
-
-	/*
-	 * (non-Javadoc)
-	 * @see org.eclipse.ui.plugin.AbstractUIPlugin#start(org.osgi.framework.BundleContext)
-	 */
-	public void start(BundleContext context) throws Exception {
-		super.start(context);
 		plugin = this;
-	}
-
-	/*
-	 * (non-Javadoc)
-	 * @see org.eclipse.ui.plugin.AbstractUIPlugin#stop(org.osgi.framework.BundleContext)
-	 */
-	public void stop(BundleContext context) throws Exception {
-		plugin = null;
-		super.stop(context);
 	}
 
 	/**
