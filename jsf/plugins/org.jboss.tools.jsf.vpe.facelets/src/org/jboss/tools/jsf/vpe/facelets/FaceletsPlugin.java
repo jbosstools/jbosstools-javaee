8cdef248826f8f90a3d0e45719e6a22a6da8cc4c
Viacheslav Kabanovich <scabanovich@exadel.com>
2012-01-25 00:11:56 +0000

    JBIDE-10703 https://issues.jboss.org/browse/JBIDE-10703 FaceletsPlugin is modified to follow the common pattern.
    git-svn-id: http://svn.jboss.org/repos/jbosstools/trunk@38142 a97e2381-89e5-4abb-bab3-167db6db766c

--- jsf/plugins/org.jboss.tools.jsf.vpe.facelets/src/org/jboss/tools/jsf/vpe/facelets/FaceletsPlugin.java
+++ jsf/plugins/org.jboss.tools.jsf.vpe.facelets/src/org/jboss/tools/jsf/vpe/facelets/FaceletsPlugin.java
@@ -11,8 +11,6 @@
 package org.jboss.tools.jsf.vpe.facelets;
 
 import org.eclipse.ui.plugin.*;
-import org.eclipse.jface.resource.ImageDescriptor;
-import org.osgi.framework.BundleContext;
 
 /**
  * The main plugin class to be used in the desktop.
@@ -30,35 +28,10 @@ public class FaceletsPlugin extends AbstractUIPlugin {
 	}
 
 	/**
-	 * This method is called upon plug-in activation
-	 */
-	public void start(BundleContext context) throws Exception {
-		super.start(context);
-	}
-
-	/**
-	 * This method is called when the plug-in is stopped
-	 */
-	public void stop(BundleContext context) throws Exception {
-		super.stop(context);
-		plugin = null;
-	}
-
-	/**
 	 * Returns the shared instance.
 	 */
 	public static FaceletsPlugin getDefault() {
 		return plugin;
 	}
 
-	/**
-	 * Returns an image descriptor for the image file at the given
-	 * plug-in relative path.
-	 *
-	 * @param path the path
-	 * @return the image descriptor
-	 */
-	public static ImageDescriptor getImageDescriptor(String path) {
-		return AbstractUIPlugin.imageDescriptorFromPlugin("org.jboss.tools.jsf.vpe.facelets", path); //$NON-NLS-1$
-	}
 }
