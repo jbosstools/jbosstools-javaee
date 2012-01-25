74146bea4e8b6477fec38bcf620fd8073ab41a44
Viacheslav Kabanovich <scabanovich@exadel.com>
2012-01-25 00:13:26 +0000

    JBIDE-10703 https://issues.jboss.org/browse/JBIDE-10703 JBPMTemplatePlugin is modified to follow the common pattern.
    git-svn-id: http://svn.jboss.org/repos/jbosstools/trunk@38143 a97e2381-89e5-4abb-bab3-167db6db766c

--- jsf/plugins/org.jboss.tools.jsf.vpe.jbpm/src/org/jboss/tools/jsf/vpe/jbpm/JBPMTemplatePlugin.java
+++ jsf/plugins/org.jboss.tools.jsf.vpe.jbpm/src/org/jboss/tools/jsf/vpe/jbpm/JBPMTemplatePlugin.java
@@ -28,24 +28,7 @@ public class JBPMTemplatePlugin extends BaseUIPlugin {
 	 * The constructor
 	 */
 	public JBPMTemplatePlugin() {
-	}
-
-	/*
-	 * (non-Javadoc)
-	 * @see org.eclipse.ui.plugin.AbstractUIPlugin#start(org.osgi.framework.BundleContext)
-	 */
-	public void start(BundleContext context) throws Exception {
-		super.start(context);
 		plugin = this;
-	}
-
-	/*
-	 * (non-Javadoc)
-	 * @see org.eclipse.ui.plugin.AbstractUIPlugin#stop(org.osgi.framework.BundleContext)
-	 */
-	public void stop(BundleContext context) throws Exception {
-		plugin = null;
-		super.stop(context);
 	}
 
 	/**
