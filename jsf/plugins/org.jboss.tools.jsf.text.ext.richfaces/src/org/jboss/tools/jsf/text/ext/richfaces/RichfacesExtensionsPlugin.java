4c4c209adf4c4f26993cdabf305155d7b9205c4a
Viacheslav Kabanovich <scabanovich@exadel.com>
2012-01-25 00:05:18 +0000

    JBIDE-10703 https://issues.jboss.org/browse/JBIDE-10703 RichfacesExtensionsPlugin: obsolete methods are removed.
    git-svn-id: http://svn.jboss.org/repos/jbosstools/trunk@38140 a97e2381-89e5-4abb-bab3-167db6db766c

--- jsf/plugins/org.jboss.tools.jsf.text.ext.richfaces/src/org/jboss/tools/jsf/text/ext/richfaces/RichfacesExtensionsPlugin.java
+++ jsf/plugins/org.jboss.tools.jsf.text.ext.richfaces/src/org/jboss/tools/jsf/text/ext/richfaces/RichfacesExtensionsPlugin.java
@@ -10,14 +10,7 @@
  ******************************************************************************/ 
 package org.jboss.tools.jsf.text.ext.richfaces;
 
-import java.util.MissingResourceException;
-import java.util.ResourceBundle;
-
 import org.eclipse.ui.plugin.*;
-import org.eclipse.core.resources.IWorkspace;
-import org.eclipse.core.resources.ResourcesPlugin;
-import org.eclipse.jface.resource.ImageDescriptor;
-import org.osgi.framework.BundleContext;
 
 /**
  * The main plugin class to be used in the desktop.
@@ -26,29 +19,14 @@ public class RichfacesExtensionsPlugin extends AbstractUIPlugin {
 
 	//The shared instance.
 	private static RichfacesExtensionsPlugin plugin;
-	//Resource bundle.
-	private ResourceBundle resourceBundle;
 	
 	public static final String PLUGIN_ID = "org.jboss.tools.jsf.text.ext.richfaces";  //$NON-NLS-1$
-
 	
 	/**
 	 * The constructor.
 	 */
 	public RichfacesExtensionsPlugin() {
 		plugin = this;
-		try {
-			resourceBundle= ResourceBundle.getBundle("org.jboss.tools.jsf.text.ext.richfaces.RichfacesExtensionsPlugin"); //$NON-NLS-1$
-		} catch (MissingResourceException x) {
-			resourceBundle = null;
-		}
-	}
-
-	/**
-	 * This method is called upon plug-in activation
-	 */
-	public void start(BundleContext context) throws Exception {
-		super.start(context);
 	}
 
 	/**
@@ -56,44 +34,6 @@ public class RichfacesExtensionsPlugin extends AbstractUIPlugin {
 	 */
 	public static RichfacesExtensionsPlugin getDefault() {
 		return plugin;
-	}
-
-	/**
-	 * Returns an image descriptor for the image file at the given
-	 * plug-in relative path.
-	 *
-	 * @param path the path
-	 * @return the image descriptor
-	 */
-	public static ImageDescriptor getImageDescriptor(String path) {
-		return AbstractUIPlugin.imageDescriptorFromPlugin("org.jboss.tools.jsf.text.ext.richfaces", path); //$NON-NLS-1$
-	}
-	
-	/**
-	 * Returns the workspace instance.
-	 */
-	public static IWorkspace getWorkspace() {
-		return ResourcesPlugin.getWorkspace();
-	}
-
-	/**
-	 * Returns the string from the plugin's resource bundle,
-	 * or 'key' if not found.
-	 */
-	public static String getResourceString(String key) {
-		ResourceBundle bundle= RichfacesExtensionsPlugin.getDefault().getResourceBundle();
-		try {
-			return bundle.getString(key);
-		} catch (MissingResourceException e) {
-			return key;
-		}
-	}
-
-	/**
-	 * Returns the plugin's resource bundle,
-	 */
-	public ResourceBundle getResourceBundle() {
-		return resourceBundle;
 	}
 
 }
