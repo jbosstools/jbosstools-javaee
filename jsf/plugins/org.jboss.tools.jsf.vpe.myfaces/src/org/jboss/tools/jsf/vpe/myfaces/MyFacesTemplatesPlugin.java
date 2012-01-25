9981f6573014ef3738360d263d8600e100fd5b25
Viacheslav Kabanovich <scabanovich@exadel.com>
2012-01-25 00:20:10 +0000

    JBIDE-10703 https://issues.jboss.org/browse/JBIDE-10703 MyFacesTemplatesPlugin is modified to follow the common pattern.
    git-svn-id: http://svn.jboss.org/repos/jbosstools/trunk@38146 a97e2381-89e5-4abb-bab3-167db6db766c

--- jsf/plugins/org.jboss.tools.jsf.vpe.myfaces/src/org/jboss/tools/jsf/vpe/myfaces/MyFacesTemplatesPlugin.java
+++ jsf/plugins/org.jboss.tools.jsf.vpe.myfaces/src/org/jboss/tools/jsf/vpe/myfaces/MyFacesTemplatesPlugin.java
@@ -1,7 +1,6 @@
 package org.jboss.tools.jsf.vpe.myfaces;
 
 import org.jboss.tools.common.log.BaseUIPlugin;
-import org.osgi.framework.BundleContext;
 
 /**
  * The activator class controls the plug-in life cycle
@@ -18,24 +17,7 @@ public class MyFacesTemplatesPlugin extends BaseUIPlugin {
 	 * The constructor
 	 */
 	public MyFacesTemplatesPlugin() {
-	}
-
-	/*
-	 * (non-Javadoc)
-	 * @see org.eclipse.ui.plugin.AbstractUIPlugin#start(org.osgi.framework.BundleContext)
-	 */
-	public void start(BundleContext context) throws Exception {
-		super.start(context);
 		plugin = this;
-	}
-
-	/*
-	 * (non-Javadoc)
-	 * @see org.eclipse.ui.plugin.AbstractUIPlugin#stop(org.osgi.framework.BundleContext)
-	 */
-	public void stop(BundleContext context) throws Exception {
-		plugin = null;
-		super.stop(context);
 	}
 
 	/**
