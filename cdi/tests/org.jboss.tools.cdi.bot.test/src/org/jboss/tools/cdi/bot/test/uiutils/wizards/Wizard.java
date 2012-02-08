8a92526286dbdf93ce350b72f272a4403e477077
Jaroslav Jankovič <jjankovi@redhat.com>
2012-02-08 14:34:35 +0000

    Modifying Wizard to extend SWTBotWizard
    git-svn-id: http://svn.jboss.org/repos/jbosstools/trunk@38504 a97e2381-89e5-4abb-bab3-167db6db766c

--- cdi/tests/org.jboss.tools.cdi.bot.test/src/org/jboss/tools/cdi/bot/test/uiutils/wizards/Wizard.java
+++ cdi/tests/org.jboss.tools.cdi.bot.test/src/org/jboss/tools/cdi/bot/test/uiutils/wizards/Wizard.java
@@ -12,93 +12,24 @@
 package org.jboss.tools.cdi.bot.test.uiutils.wizards;
 
 import org.eclipse.swt.widgets.Shell;
-import org.eclipse.swtbot.swt.finder.widgets.SWTBotShell;
-import org.eclipse.swtbot.swt.finder.widgets.SWTBotText;
-import org.eclipse.swtbot.swt.finder.widgets.SWTBotTree;
-import org.eclipse.swtbot.swt.finder.widgets.SWTBotTreeItem;
-import org.jboss.tools.ui.bot.ext.Timing;
+import org.jboss.tools.ui.bot.ext.wizards.SWTBotWizard;
 
-public class Wizard extends SWTBotShell {
+public class Wizard extends SWTBotWizard {
 
-	protected static final String NEXT = "Next >";
-	protected static final String BACK = "< Back";
-	protected static final String CANCEL = "Cancel";
-	protected static final String FINISH = "Finish";
-	
 	public Wizard(Shell shell) {
 		super(shell);
-		assert getText().contains("New ");
 	}
 
-	public Wizard selectTemplate(String... item) {
-		assert item.length > 0;
-		SWTBotTree tree = bot().tree();
-		SWTBotTreeItem ti = null;
-		for (int i = 0; i < item.length - 1; i++) {
-			ti = ti != null ? ti.expandNode(item[i]).select() : tree.expandNode(item[i]).select();
-		}
-		if (ti != null) {
-			ti.select(item[item.length - 1]);
-		} else {
-			tree.select(item[item.length - 1]);
-		}
-		return this;
-	}
-
-	public Wizard back() {
-		clickButton(BACK);
-		return this;
-	}
-
-	public Wizard next() {
-		clickButton(NEXT);
-		return this;
-	}
-
-	public void cancel() {
-		clickButton(CANCEL);
-	}
-
-	public void finish() {
-		clickButton(FINISH);
-	}
-	
-	public boolean canFinish() {
-		return canClick(FINISH);
-	}
-	
-	public boolean canNext() {
-		return canClick(NEXT);
-	}
-		
 	protected void checkCheckbox(String text) {
 		bot().checkBoxWithLabel(text).select();
-		bot().sleep(Timing.time500MS());
 	}
 	
 	protected void uncheckCheckbox(String text) {
 		bot().checkBoxWithLabel(text).deselect();
-		bot().sleep(Timing.time500MS());
-	}
-	
-	protected void clickButton(String text) {
-		bot().button(text).click();
-		bot().sleep(Timing.time500MS());
 	}
 
-	protected void setText(String label, String text) {
-		SWTBotText t = bot().textWithLabel(label);
-		t.setFocus();
-		t.setText(text);
-	}
-	
 	protected void setTextInCombobox(String combobox, String text) {
-		bot().comboBoxWithLabel(combobox).setSelection(text);
-		bot().sleep(Timing.time500MS());		
-	}
-		
-	protected boolean canClick(String button) {
-		return bot().button(button).isEnabled();
+		bot().comboBoxWithLabel(combobox).setSelection(text);		
 	}
 	
 	protected boolean canCheckInCombobox(String combobox, String text) {		
