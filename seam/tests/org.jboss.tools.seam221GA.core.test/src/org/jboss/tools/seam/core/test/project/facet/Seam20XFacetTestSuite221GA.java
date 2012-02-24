33c81c0078af87019f06939d9c59352def162e7c
Alexey Kazakov <akazakov@exadel.com>
2012-02-24 21:07:11 +0000

    https://issues.jboss.org/browse/JBIDE-10950 use seam 2.2.1.Final instead of 2.2.1.CR1 and 2.2.1.CR2 in requirements?
    git-svn-id: http://svn.jboss.org/repos/jbosstools/trunk@39126 a97e2381-89e5-4abb-bab3-167db6db766c

---
+++ seam/tests/org.jboss.tools.seam221GA.core.test/src/org/jboss/tools/seam/core/test/project/facet/Seam20XFacetTestSuite221GA.java
@@ -0,0 +1,27 @@
+/*******************************************************************************
+ * Copyright (c) 2007 Red Hat, Inc.
+ * Distributed under license by Red Hat, Inc. All rights reserved.
+ * This program is made available under the terms of the
+ * Eclipse Public License v1.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors:
+ *     Red Hat, Inc. - initial API and implementation
+ ******************************************************************************/
+package org.jboss.tools.seam.core.test.project.facet;
+
+import junit.framework.Test;
+import junit.framework.TestSuite;
+
+/**
+ * @author eskimo
+ *
+ */
+public class Seam20XFacetTestSuite221GA extends Seam20XFacetTestSuite201GA {
+
+	public static Test suite() {
+		TestSuite suite = new TestSuite("Seam 2.2.1.GA tests");
+		suite.addTest(new Seam2FacetInstallDelegateTestSetup(new TestSuite(Seam221GAFacetInstallDelegateTest.class)));
+		return suite;
+	}
+}
