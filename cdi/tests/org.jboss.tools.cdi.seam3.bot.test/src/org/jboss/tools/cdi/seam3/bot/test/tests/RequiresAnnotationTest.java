3fd890d80ef82fd96cb37f91b277868c51960f41
Jaroslav Jankovič <jjankovi@redhat.com>
2012-03-21 15:32:26 +0000

    RequiresAnnotationTest implementation finished
    git-svn-id: http://svn.jboss.org/repos/jbosstools/trunk@39717 a97e2381-89e5-4abb-bab3-167db6db766c

--- cdi/tests/org.jboss.tools.cdi.seam3.bot.test/src/org/jboss/tools/cdi/seam3/bot/test/tests/RequiresAnnotationTest.java
+++ cdi/tests/org.jboss.tools.cdi.seam3.bot.test/src/org/jboss/tools/cdi/seam3/bot/test/tests/RequiresAnnotationTest.java
@@ -11,53 +11,147 @@
 
 package org.jboss.tools.cdi.seam3.bot.test.tests;
 
-import org.jboss.tools.cdi.seam3.bot.test.base.SolderTestBase;
+import org.jboss.tools.cdi.bot.test.CDIConstants;
+import org.jboss.tools.cdi.seam3.bot.test.base.SolderAnnotationTestBase;
+import org.jboss.tools.cdi.seam3.bot.test.util.SeamLibraries;
 import org.junit.Test;
 
-public class RequiresAnnotationTest extends SolderTestBase {
+public class RequiresAnnotationTest extends SolderAnnotationTestBase {
 
 	@Override
 	public String getProjectName() {
-		return "requires";
+		return "requires1";
 	}
 	
 	@Override
 	public void waitForJobs() {
-		
+		projectExplorer.deleteAllProjects();		
 	} 
+	
+	@Override
+	public void prepareWorkspace() {
+		
+	}
 	
 	@Test
 	public void testManagedBeans() {
 		
-	}
-	
-	@Test
-	public void testDecorator() {
+		String managerProducer = "ManagerProducer";
+		String manager = "Manager";
+		String projectName = "requires1";
 		
-	}
-	
-	@Test
-	public void testInterceptor() {
+		importProjectWithLibrary(projectName, SeamLibraries.SOLDER);
+		
+		setEd(packageExplorer.openFile(projectName, CDIConstants.SRC, 
+				getPackageName(), APPLICATION_CLASS).toTextEditor());
+		
+		testAnnotationImproperValue(projectName, true);
+		
+		setEd(packageExplorer.openFile(projectName, CDIConstants.SRC, 
+				getPackageName(), managerProducer + ".java").toTextEditor());
+		editResourceUtil.replaceInEditor("@Requires(\"cdi.test." + manager + "\")",
+				"@Requires(\"" + getPackageName() + "." + manager + "\")");
+		
+		testAnnotationProperValue(projectName, "managerProducer", managerProducer, false, null);
 		
 	}
 	
 	@Test
 	public void testSessionBean() {
+	
+		String managerProducer = "ManagerProducer";
+		String manager = "Manager";
+		String projectName = "requires2";
+		
+		importProjectWithLibrary(projectName, SeamLibraries.SOLDER);
+		
+		setEd(packageExplorer.openFile(projectName, CDIConstants.SRC, 
+				getPackageName(), APPLICATION_CLASS).toTextEditor());
+		
+		testAnnotationImproperValue(projectName, true);
+		
+		setEd(packageExplorer.openFile(projectName, CDIConstants.SRC, 
+				getPackageName(), managerProducer + ".java").toTextEditor());
+		editResourceUtil.replaceInEditor("@Requires(\"cdi.test." + manager + "\")",
+				"@Requires(\"" + getPackageName() + "." + manager + "\")");
+		
+		testAnnotationProperValue(projectName, "managerProducer", managerProducer, false, null);
 		
 	}
 	
 	@Test
 	public void testProducerMethod() {
 		
+		String managerProducer = "ManagerProducer";
+		String manager = "Manager";
+		String projectName = "requires3";
+		
+		importProjectWithLibrary(projectName, SeamLibraries.SOLDER);
+		
+		setEd(packageExplorer.openFile(projectName, CDIConstants.SRC, 
+				getPackageName(), APPLICATION_CLASS).toTextEditor());
+		
+		testAnnotationImproperValue(projectName, true);
+		
+		setEd(packageExplorer.openFile(projectName, CDIConstants.SRC, 
+				getPackageName(), managerProducer + ".java").toTextEditor());
+		editResourceUtil.replaceInEditor("@Requires(\"cdi.test." + manager + "\")",
+				"@Requires(\"" + getPackageName() + "." + manager + "\")");
+		
+		testAnnotationProperValue(projectName, "managerProducer", 
+				managerProducer, true, "getManagerProducer");
+		
 	}
 	
 	@Test
 	public void testProducerField() {
 		
+		String managerProducer = "ManagerProducer";
+		String manager = "Manager";
+		String projectName = "requires4";
+		
+		importProjectWithLibrary(projectName, SeamLibraries.SOLDER);
+		
+		setEd(packageExplorer.openFile(projectName, CDIConstants.SRC, 
+				getPackageName(), APPLICATION_CLASS).toTextEditor());
+		
+		testAnnotationImproperValue(projectName, true);
+		
+		setEd(packageExplorer.openFile(projectName, CDIConstants.SRC, 
+				getPackageName(), managerProducer + ".java").toTextEditor());
+		editResourceUtil.replaceInEditor("@Requires(\"cdi.test." + manager + "\")",
+				"@Requires(\"" + getPackageName() + "." + manager + "\")");
+		
+		testAnnotationProperValue(projectName, "managerProducer", 
+				managerProducer, true, "managerProducer");
+		
 	}
 	
 	@Test
 	public void testObserverMethods() {
+		
+		String managerProducer = "ManagerProducer";
+		String manager = "Manager";
+		String projectName = "requires5";
+		String eventAttribute = "eventAttribute";
+		
+		importProjectWithLibrary(projectName, SeamLibraries.SOLDER);
+		
+		setEd(packageExplorer.openFile(projectName, CDIConstants.SRC, 
+				getPackageName(), APPLICATION_CLASS).toTextEditor());
+		
+		assertFalse(openOnUtil.openOnByOption(eventAttribute, APPLICATION_CLASS, 
+				CDIConstants.OPEN_CDI_OBSERVER_METHOD));
+		
+		setEd(packageExplorer.openFile(projectName, CDIConstants.SRC, 
+				getPackageName(), managerProducer + ".java").toTextEditor());
+		editResourceUtil.replaceInEditor("@Requires(\"cdi.test." + manager + "\")",
+				"@Requires(\"" + getPackageName() + "." + manager + "\")");
+		
+		assertTrue(openOnUtil.openOnByOption(eventAttribute, APPLICATION_CLASS, 
+				CDIConstants.OPEN_CDI_OBSERVER_METHOD));
+		assertTrue(getEd().getTitle().equals(managerProducer + ".java"));
+		assertTrue(getEd().getSelection().equals("method"));
 		
 	}
 	
