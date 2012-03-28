bcf8f57faa078a9235de95030a7c77514320038d
Jaroslav Jankovič <jjankovi@redhat.com>
2012-03-28 12:23:04 +0000

    removed one method which was moved to another class
    git-svn-id: http://svn.jboss.org/repos/jbosstools/trunk@39878 a97e2381-89e5-4abb-bab3-167db6db766c

--- cdi/tests/org.jboss.tools.cdi.seam3.bot.test/src/org/jboss/tools/cdi/seam3/bot/test/base/SolderTestBase.java
+++ cdi/tests/org.jboss.tools.cdi.seam3.bot.test/src/org/jboss/tools/cdi/seam3/bot/test/base/SolderTestBase.java
@@ -9,10 +9,4 @@ public class SolderTestBase extends Seam3TestBase {
 		importProjectWithLibrary(getProjectName(), SeamLibraries.SOLDER);
 	}
 	
-	public void importProjectWithLibrary(String projectName, SeamLibraries library) {
-		projectImportHelper.importTestProject("/resources/projects/" + projectName, projectName);
-		addAndCheckLibraryInProject(projectName, library);
-		eclipse.cleanAllProjects();
-	}
-	
 }
