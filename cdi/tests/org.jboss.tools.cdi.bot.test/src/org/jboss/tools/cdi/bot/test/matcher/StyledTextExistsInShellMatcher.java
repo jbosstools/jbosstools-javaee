ce563624b94a6ffebe6b29f67c586c69abfa6ef8
Jaroslav Jankovič <jjankovi@redhat.com>
2012-04-03 12:32:47 +0000

    matcher package added into cdi bot test
    git-svn-id: http://svn.jboss.org/repos/jbosstools/trunk@39997 a97e2381-89e5-4abb-bab3-167db6db766c

---
+++ cdi/tests/org.jboss.tools.cdi.bot.test/src/org/jboss/tools/cdi/bot/test/matcher/StyledTextExistsInShellMatcher.java
@@ -0,0 +1,37 @@
+/*******************************************************************************
+ * Copyright (c) 2010-2012 Red Hat, Inc.
+ * Distributed under license by Red Hat, Inc. All rights reserved.
+ * This program is made available under the terms of the
+ * Eclipse Public License v1.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors:
+ * Red Hat, Inc. - initial API and implementation
+ ******************************************************************************/
+
+package org.jboss.tools.cdi.bot.test.matcher;
+
+import org.eclipse.swtbot.swt.finder.exceptions.WidgetNotFoundException;
+import org.eclipse.swtbot.swt.finder.widgets.SWTBotShell;
+import org.hamcrest.Description;
+import org.junit.internal.matchers.TypeSafeMatcher;
+
+@SuppressWarnings("restriction")
+public class StyledTextExistsInShellMatcher extends TypeSafeMatcher<SWTBotShell> {
+
+	public void describeTo(Description description) {
+		description.appendText("shell does not contain any " +
+				"styled text");
+	}
+
+	@Override
+	public boolean matchesSafely(SWTBotShell shell) {
+		try {
+			shell.bot().styledText();
+			return true;
+		} catch (WidgetNotFoundException exc) {				
+			return false;
+		}
+	}
+
+}
