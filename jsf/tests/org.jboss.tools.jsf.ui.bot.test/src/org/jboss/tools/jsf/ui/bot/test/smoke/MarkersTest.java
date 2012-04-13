a619dab46e6ed38371e66253e010d2fb97caffb6
Vlado Pakan <vpakan@redhat.com>
2012-04-13 12:51:38 +0000

    Added Quick Fix Testing
    git-svn-id: http://svn.jboss.org/repos/jbosstools/trunk@40174 a97e2381-89e5-4abb-bab3-167db6db766c

--- jsf/tests/org.jboss.tools.jsf.ui.bot.test/src/org/jboss/tools/jsf/ui/bot/test/smoke/MarkersTest.java
+++ jsf/tests/org.jboss.tools.jsf.ui.bot.test/src/org/jboss/tools/jsf/ui/bot/test/smoke/MarkersTest.java
@@ -10,12 +10,18 @@
  ******************************************************************************/
 package org.jboss.tools.jsf.ui.bot.test.smoke;
 
+import java.util.LinkedList;
+
+import org.eclipse.swtbot.swt.finder.SWTBot;
+import org.eclipse.swtbot.swt.finder.exceptions.WidgetNotFoundException;
 import org.jboss.tools.jsf.ui.bot.test.JSFAutoTestCase;
 import org.jboss.tools.ui.bot.ext.SWTJBTExt;
 import org.jboss.tools.ui.bot.ext.SWTTestExt;
 import org.jboss.tools.ui.bot.ext.Timing;
 import org.jboss.tools.ui.bot.ext.helper.MarkerHelper;
+import org.jboss.tools.ui.bot.ext.helper.QuickFixHelper;
 import org.jboss.tools.ui.bot.ext.parts.SWTBotEditorExt;
+import org.jboss.tools.ui.bot.ext.types.IDELabel;
 import org.jboss.tools.vpe.ui.bot.test.VPEAutoTestCase;
 /**
  * Test Markers position and attributes regarding to JSF components within jsp page
@@ -71,17 +77,58 @@ public class MarkersTest extends JSFAutoTestCase{
 	    markerHelper.checkForMarker(String.valueOf(expectedMarkerLines[index] + 1),
 	        expectedMarkerDesc[index]);  
 	  }	  
+	  // check quick fix
+	  LinkedList<String> expectedQuickFixes = new LinkedList<String>();
+	  expectedQuickFixes.add("Configure Problem Severity for preference 'Property cannot be resolved'");
+    QuickFixHelper.checkQuickFixContent(SWTTestExt.bot, 
+        TEST_PAGE,
+        textToSelect,
+        textToSelect.length(),
+        insertText.length(),
+        expectedQuickFixes);
+    editor.quickFix().useQuickFix(0);
+    final SWTBot botProperties = bot.shell(IDELabel.Shell.PREFERENCES_FILTERED).activate().bot();
+    String selectedTreeItemLabel = botProperties.tree().selection().get(0,0);
+    boolean isFilterValueOK = false;
+    final String expectedFilterValue = "Property cannot be resolved:";
+    try{
+      botProperties.text(expectedFilterValue);
+      isFilterValueOK = true;
+    } catch (WidgetNotFoundException wnfe){
+      // do nothing
+    }
+    boolean isValidationOptionPresent = false;
+    final String expectedValidationOption = "Property cannot be resolved:";
+    try{
+      botProperties.label(expectedValidationOption);
+      isValidationOptionPresent = true;
+    } catch (WidgetNotFoundException wnfe){
+      // do nothing
+    }
+    bot.button(IDELabel.Button.CANCEL).click();
+    final String expectedSelectedTreeItem = "Validation";
+    assertTrue("Selected tree item within Preferences Dialog has to have label "
+      +  expectedSelectedTreeItem 
+      + " but is " + selectedTreeItemLabel,
+      selectedTreeItemLabel.equals(expectedSelectedTreeItem));
+    assertTrue("Validation Filter has to have value " + expectedFilterValue
+        + " but it does not",
+      isFilterValueOK);
+    assertTrue("Validation Option has to have label " + expectedValidationOption
+        + " but it does not",
+      isValidationOptionPresent);
 	}   
   @Override
-public void setUp() throws Exception {
+  public void setUp() throws Exception {
     super.setUp();
     openPage(TEST_PAGE);
     editor = SWTTestExt.bot.swtBotEditorExtByTitle(TEST_PAGE);
     originalEditorText = editor.getText();
   }
+
   @Override
-public void tearDown() throws Exception {
-    if (editor != null){
+  public void tearDown() throws Exception {
+    if (editor != null) {
       editor.setText(originalEditorText);
       editor.saveAndClose();
     }
