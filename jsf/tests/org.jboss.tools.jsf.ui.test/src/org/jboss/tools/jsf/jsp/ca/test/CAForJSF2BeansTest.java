d4b9f0329fc9744e022f9608ef7788bad9cfa1d1
Viacheslav Kabanovich <scabanovich@exadel.com>
2012-04-20 00:40:00 +0000

    JBIDE-11477 https://issues.jboss.org/browse/JBIDE-11477 Test is added.
    git-svn-id: http://svn.jboss.org/repos/jbosstools/trunk@40369 a97e2381-89e5-4abb-bab3-167db6db766c

--- jsf/tests/org.jboss.tools.jsf.ui.test/src/org/jboss/tools/jsf/jsp/ca/test/CAForJSF2BeansTest.java
+++ jsf/tests/org.jboss.tools.jsf.ui.test/src/org/jboss/tools/jsf/jsp/ca/test/CAForJSF2BeansTest.java
@@ -3,32 +3,53 @@ package org.jboss.tools.jsf.jsp.ca.test;
 import org.jboss.tools.jst.jsp.test.ca.ContentAssistantTestCase;
 import org.jboss.tools.test.util.TestProjectProvider;
 
-public class CAForJSF2BeansTest extends ContentAssistantTestCase{
+public class CAForJSF2BeansTest extends ContentAssistantTestCase {
 	TestProjectProvider provider = null;
 	boolean makeCopy = true;
 	private static final String PROJECT_NAME = "JSF2Beans";
 	private static final String PAGE_NAME = "/src/test/beans/inputname.xhtml";
-	
+
 	public void setUp() throws Exception {
-		provider = new TestProjectProvider("org.jboss.tools.jsf.base.test", null, PROJECT_NAME, makeCopy); 
+		provider = new TestProjectProvider("org.jboss.tools.jsf.base.test",
+				null, PROJECT_NAME, makeCopy);
 		project = provider.getProject();
 	}
 
 	protected void tearDown() throws Exception {
-		if(provider != null) {
+		if (provider != null) {
 			provider.dispose();
 		}
 	}
-	
+
 	/**
 	 * JBIDE-5941
 	 */
-	public void testCAForJSF2Beans(){
-		String[] proposals = {
-			"mybean1", "mybean2"
-		};
+	public void testCAForJSF2Beans() {
+		String[] proposals = { "mybean1", "mybean2" };
 
 		checkProposals(PAGE_NAME, "#{myb}", 5, proposals, false);
+
+	}
+
+	public void testCAForMethodParameters() {
+		String[] beans = {"testA", "testB"};
+		String[] methodA = {"testB.addA()"};
+		String[] methodB = {"testA.addB()"};
+
+		String text = "#{testA.addB(test";
+		checkProposals(PAGE_NAME, text, text.length(), beans, false);
+
+		text = "#{testA.addB(testB.add";
+		checkProposals(PAGE_NAME, text, text.length(), methodA, false);
+
+		text = "testA.addB(testB.addA(test";
+		checkProposals(PAGE_NAME, text, text.length(), beans, false);
+
+		text = "testA.addB(testB.addA(testA.add";
+		checkProposals(PAGE_NAME, text, text.length(), methodB, false);
+
+		text = "testA.addB(testB.addA(testA.addB(test";
+		checkProposals(PAGE_NAME, text, text.length(), beans, false);
 
 	}
 }
