d8cdc361d7698a62a25da6561d5c4dbbdaccccae
Viacheslav Kabanovich <scabanovich@exadel.com>
2012-04-20 00:38:12 +0000

    JBIDE-11477 https://issues.jboss.org/browse/JBIDE-11477 Test is added.
    git-svn-id: http://svn.jboss.org/repos/jbosstools/trunk@40368 a97e2381-89e5-4abb-bab3-167db6db766c

---
+++ jsf/tests/org.jboss.tools.jsf.base.test/projects/JSF2Beans/src/test/beans/TestA.java
@@ -0,0 +1,8 @@
+package test.beans;
+
+import javax.faces.bean.ManagedBean;
+
+@ManagedBean
+public class TestA {
+	public void addB(TestB b) {}
+}
---
+++ jsf/tests/org.jboss.tools.jsf.base.test/projects/JSF2Beans/src/test/beans/TestB.java
@@ -0,0 +1,8 @@
+package test.beans;
+
+import javax.faces.bean.ManagedBean;
+
+@ManagedBean
+public class TestB {
+	public void addA(TestA a) {}
+}
