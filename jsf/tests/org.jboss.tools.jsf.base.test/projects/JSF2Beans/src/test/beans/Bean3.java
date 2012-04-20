f08cc630192169804722918b49a7db4c446befaf
Viacheslav Kabanovich <scabanovich@exadel.com>
2012-04-20 18:42:08 +0000

    JBIDE-11510 https://issues.jboss.org/browse/JBIDE-11510 Test is added.
    git-svn-id: http://svn.jboss.org/repos/jbosstools/trunk@40391 a97e2381-89e5-4abb-bab3-167db6db766c

--- jsf/tests/org.jboss.tools.jsf.base.test/projects/JSF2Beans/src/test/beans/Bean3.java
+++ jsf/tests/org.jboss.tools.jsf.base.test/projects/JSF2Beans/src/test/beans/Bean3.java
@@ -4,5 +4,13 @@ import javax.faces.bean.ManagedBean;
 
 @ManagedBean(name="mybean2")
 public class Bean3 {
+	
+	public String get100() {
+		return "100";
+	}
+
+	public String get101() {
+		return "101";
+	}
 
 }
