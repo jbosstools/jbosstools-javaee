fc89e5d1e60b262dd0fbce6c4ee0db4d7455d16d
Jaroslav Jankovič <jjankovi@redhat.com>
2012-07-05 10:03:25 +0000

    new test project for seam3 bot test added
    git-svn-id: http://svn.jboss.org/repos/jbosstools/trunk@42401 a97e2381-89e5-4abb-bab3-167db6db766c

---
+++ cdi/tests/org.jboss.tools.cdi.seam3.bot.test/resources/projects/seamConfigInjectOpenOn/src/test/Report.java
@@ -0,0 +1,12 @@
+package test;
+
+import javax.inject.Inject;
+
+
+public class Report {
+
+	public @Inject Resource path1;
+	
+	public @Inject @Q1 Resource path2;
+	
+}
---
+++ cdi/tests/org.jboss.tools.cdi.seam3.bot.test/resources/projects/seamConfigInjectOpenOn/src/test/Resource.java
@@ -0,0 +1,7 @@
+package test;
+
+public class Resource {
+	
+	public String path;
+
+}
