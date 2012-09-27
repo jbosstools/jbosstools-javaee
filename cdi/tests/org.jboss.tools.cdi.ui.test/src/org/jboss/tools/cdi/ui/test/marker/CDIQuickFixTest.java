7f1dea0cb8aa4bcf2e3c31b5f8df7db38d569c64
Daniel Azarov <daniel@exadel.com>
2012-09-27 00:02:00 +0000

    Configure Problem Severity should open project settings if they are enabled for the project https://issues.jboss.org/browse/JBIDE-12538
    git-svn-id: http://svn.jboss.org/repos/jbosstools/trunk@44074 a97e2381-89e5-4abb-bab3-167db6db766c

--- cdi/tests/org.jboss.tools.cdi.ui.test/src/org/jboss/tools/cdi/ui/test/marker/CDIQuickFixTest.java
+++ cdi/tests/org.jboss.tools.cdi.ui.test/src/org/jboss/tools/cdi/ui/test/marker/CDIQuickFixTest.java
@@ -2,6 +2,7 @@ package org.jboss.tools.cdi.ui.test.marker;
 
 import org.eclipse.core.runtime.CoreException;
 import org.jboss.tools.cdi.core.test.tck.TCKTest;
+import org.jboss.tools.cdi.internal.core.validation.CDICoreValidator;
 import org.jboss.tools.cdi.internal.core.validation.CDIValidationErrorManager;
 import org.jboss.tools.cdi.ui.marker.AddAnnotationMarkerResolution;
 import org.jboss.tools.cdi.ui.marker.AddLocalBeanMarkerResolution;
@@ -18,19 +19,13 @@ import org.jboss.tools.common.base.test.QuickFixTestUtil;
 public class CDIQuickFixTest extends TCKTest {
 	private QuickFixTestUtil util = new QuickFixTestUtil();
 	
-//	private static boolean isSuspendedValidationDefaultValue;
-//	public void setUp() throws Exception {
-//		super.setUp();
-//		isSuspendedValidationDefaultValue = ValidationFramework.getDefault().isSuspended();
-//		ValidationFramework.getDefault().suspendAllValidation(false);
-//		tckProject.refreshLocal(IResource.DEPTH_INFINITE, new NullProgressMonitor());
-//		tckProject.build(IncrementalProjectBuilder.FULL_BUILD, new NullProgressMonitor());
-//	}
-//	
-//	public void tearDown() throws Exception {
-//		ValidationFramework.getDefault().suspendAllValidation(isSuspendedValidationDefaultValue);
-//		super.tearDown();
-//	}
+	public void setUp() throws Exception {
+		super.setUp();
+		
+		// register Preference Info
+		new CDICoreValidator();
+	}
+	
 	
 	public void testMakeFieldStatic() throws CoreException {
 		util.checkProposal(tckProject,
@@ -38,7 +33,9 @@ public class CDIQuickFixTest extends TCKTest {
 				"JavaSource/org/jboss/jsr299/tck/tests/jbt/quickfixes/MakeFieldStatic.qfxresult",
 				"@Produces",
 				CDIValidationErrorManager.ILLEGAL_PRODUCER_FIELD_IN_SESSION_BEAN_ID,
-				MakeFieldStaticMarkerResolution.class, true);
+				MakeFieldStaticMarkerResolution.class,
+				true,
+				CDICoreValidator.PROBLEM_TYPE);
 	}
 
 	public void testAddLocalBeanResolution() throws CoreException {
@@ -47,7 +44,10 @@ public class CDIQuickFixTest extends TCKTest {
 				"JavaSource/org/jboss/jsr299/tck/tests/jbt/quickfixes/AddLocalBean.qfxresult",
 				"@Produces",
 				CDIValidationErrorManager.ILLEGAL_PRODUCER_METHOD_IN_SESSION_BEAN_ID,
-				AddLocalBeanMarkerResolution.class, true);
+				AddLocalBeanMarkerResolution.class,
+				true,
+				CDICoreValidator.PROBLEM_TYPE);
+
 	}
 	
 	public void testMakeProducerMethodPublicResolution() throws CoreException {
@@ -56,8 +56,10 @@ public class CDIQuickFixTest extends TCKTest {
 				"JavaSource/org/jboss/jsr299/tck/tests/jbt/quickfixes/MakeMethodPublic.qfxresult",
 				"@Produces",
 				CDIValidationErrorManager.ILLEGAL_PRODUCER_METHOD_IN_SESSION_BEAN_ID,
-				MakeMethodPublicMarkerResolution.class, true);
-	}
+				MakeMethodPublicMarkerResolution.class,
+				true,
+				CDICoreValidator.PROBLEM_TYPE);
+}
 	
 	
 	public void testAddSerializableInterfaceResolution() throws CoreException{
@@ -66,7 +68,9 @@ public class CDIQuickFixTest extends TCKTest {
 				"JavaSource/org/jboss/jsr299/tck/tests/jbt/quickfixes/AddSerializable.qfxresult",
 				"AddSerializable",
 				CDIValidationErrorManager.NOT_PASSIVATION_CAPABLE_BEAN_ID,
-				AddSerializableInterfaceMarkerResolution.class, true);
+				AddSerializableInterfaceMarkerResolution.class,
+				true,
+				CDICoreValidator.PROBLEM_TYPE);
 	}
 	
 	public void testAddRetentionToQualifierResolution() throws CoreException{
@@ -75,7 +79,9 @@ public class CDIQuickFixTest extends TCKTest {
 				"JavaSource/org/jboss/jsr299/tck/tests/jbt/quickfixes/AddRetention.qfxresult",
 				"AddRetention",
 				CDIValidationErrorManager.MISSING_RETENTION_ANNOTATION_IN_QUALIFIER_TYPE_ID,
-				AddRetentionAnnotationMarkerResolution.class, true);
+				AddRetentionAnnotationMarkerResolution.class,
+				true,
+				CDICoreValidator.PROBLEM_TYPE);
 	}
 	
 	public void testChangeRetentionToQualifierResolution() throws CoreException{
@@ -84,7 +90,9 @@ public class CDIQuickFixTest extends TCKTest {
 				"JavaSource/org/jboss/jsr299/tck/tests/jbt/quickfixes/ChangeAnnotation.qfxresult",
 				"@Retention(value = null)",
 				CDIValidationErrorManager.MISSING_RETENTION_ANNOTATION_IN_QUALIFIER_TYPE_ID,
-				ChangeAnnotationMarkerResolution.class, true);
+				ChangeAnnotationMarkerResolution.class,
+				true,
+				CDICoreValidator.PROBLEM_TYPE);
 	}
 	
 	public void testAddTargetToScopeResolution() throws CoreException{
@@ -93,7 +101,9 @@ public class CDIQuickFixTest extends TCKTest {
 				"JavaSource/org/jboss/jsr299/tck/tests/jbt/quickfixes/AddTarget.qfxresult",
 				"AddTarget",
 				CDIValidationErrorManager.MISSING_TARGET_ANNOTATION_IN_SCOPE_TYPE_ID,
-				AddTargetAnnotationMarkerResolution.class, true);
+				AddTargetAnnotationMarkerResolution.class,
+				true,
+				CDICoreValidator.PROBLEM_TYPE);
 	}
 	
 	public void testAddNonbindingToAnnotationMemberOfQualifierResolution() throws CoreException{
@@ -102,7 +112,9 @@ public class CDIQuickFixTest extends TCKTest {
 				"JavaSource/org/jboss/jsr299/tck/tests/jbt/quickfixes/AddAnnotation.qfxresult",
 				"abc",
 				CDIValidationErrorManager.MISSING_NONBINDING_FOR_ANNOTATION_VALUE_IN_QUALIFIER_TYPE_MEMBER_ID,
-				AddAnnotationMarkerResolution.class, true);
+				AddAnnotationMarkerResolution.class,
+				true,
+				CDICoreValidator.PROBLEM_TYPE);
 	}
 	
 	public void testDeleteDisposesAnnotationFromParameterResolution() throws CoreException{
@@ -111,7 +123,9 @@ public class CDIQuickFixTest extends TCKTest {
 				"JavaSource/org/jboss/jsr299/tck/tests/jbt/quickfixes/DeleteAnnotation.qfxresult",
 				"@Inject",
 				CDIValidationErrorManager.CONSTRUCTOR_PARAMETER_ANNOTATED_DISPOSES_ID,
-				DeleteAnnotationMarkerResolution.class, true);
+				DeleteAnnotationMarkerResolution.class,
+				true,
+				CDICoreValidator.PROBLEM_TYPE);
 	}
 	
 	public void testCreateBeanClassResolution() throws CoreException{
@@ -120,7 +134,9 @@ public class CDIQuickFixTest extends TCKTest {
 				"JavaSource/org/jboss/jsr299/tck/tests/jbt/quickfixes/NonExistingAlternative.qfxresult",
 				"com.acme.NonExistingClass",
 				CDIValidationErrorManager.UNKNOWN_ALTERNATIVE_BEAN_CLASS_NAME_ID,
-				CreateCDIElementMarkerResolution.class, false);
+				CreateCDIElementMarkerResolution.class,
+				true,
+				CDICoreValidator.PROBLEM_TYPE);
 	}
 
 	public void testCreateStereotypeResolution() throws CoreException{
@@ -129,7 +145,9 @@ public class CDIQuickFixTest extends TCKTest {
 				"JavaSource/org/jboss/jsr299/tck/tests/jbt/quickfixes/NonExistingStereotype.qfxresult",
 				"com.acme.NotExistingStereotype",
 				CDIValidationErrorManager.UNKNOWN_ALTERNATIVE_ANNOTATION_NAME_ID,
-				CreateCDIElementMarkerResolution.class, false);
+				CreateCDIElementMarkerResolution.class,
+				false,
+				CDICoreValidator.PROBLEM_TYPE);
 	}
 	
 	public void testCreateDecoratorResolution() throws CoreException{
@@ -138,7 +156,9 @@ public class CDIQuickFixTest extends TCKTest {
 				"JavaSource/org/jboss/jsr299/tck/tests/jbt/quickfixes/NonExistingDecorator.qfxresult",
 				"com.acme.NonExistingDecoratorClass",
 				CDIValidationErrorManager.UNKNOWN_DECORATOR_BEAN_CLASS_NAME_ID,
-				CreateCDIElementMarkerResolution.class, false);
+				CreateCDIElementMarkerResolution.class,
+				false,
+				CDICoreValidator.PROBLEM_TYPE);
 	}
 
 	public void testCreateInterceptorResolution() throws CoreException{
@@ -147,6 +167,8 @@ public class CDIQuickFixTest extends TCKTest {
 				"JavaSource/org/jboss/jsr299/tck/tests/jbt/quickfixes/NonExistingInterceptor.qfxresult",
 				"com.acme.NonExistingInterceptorClass",
 				CDIValidationErrorManager.UNKNOWN_INTERCEPTOR_CLASS_NAME_ID,
-				CreateCDIElementMarkerResolution.class, false);
+				CreateCDIElementMarkerResolution.class,
+				false,
+				CDICoreValidator.PROBLEM_TYPE);
 	}
 }
