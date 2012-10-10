0640674883b59208dbc134207c08bc5d26f7b351
Jaroslav Jankovič <jjankovi@redhat.com>
2012-10-10 09:43:47 +0000

    removing two struts tests from stable suite - they are not stable yet
    git-svn-id: http://svn.jboss.org/repos/jbosstools/trunk@44404 a97e2381-89e5-4abb-bab3-167db6db766c

--- struts/tests/org.jboss.tools.struts.ui.bot.test/src/org/jboss/tools/struts/ui/bot/test/JenkinsTestSuite.java
+++ struts/tests/org.jboss.tools.struts.ui.bot.test/src/org/jboss/tools/struts/ui/bot/test/JenkinsTestSuite.java
@@ -2,15 +2,13 @@ package org.jboss.tools.struts.ui.bot.test;
 
 import org.jboss.tools.struts.ui.bot.test.smoke.CreateNewStrutsProjectTest;
 import org.jboss.tools.struts.ui.bot.test.smoke.ImportStrutsProjectTest;
-import org.jboss.tools.struts.ui.bot.test.smoke.RenameStrutsConfigXmlFile;
-import org.jboss.tools.struts.ui.bot.test.smoke.RenameTldFile;
 import org.jboss.tools.struts.ui.bot.test.smoke.RunStrutsProjectOnServer;
 import org.jboss.tools.ui.bot.ext.RequirementAwareSuite;
 import org.junit.runner.RunWith;
 import org.junit.runners.Suite.SuiteClasses;
 
 /**
- * Suite of tests executed on jenkins slave * 
+ * Suite of tests executed on jenkins slave 
  * @author jjankovi
  *
  */
@@ -19,11 +17,12 @@ import org.junit.runners.Suite.SuiteClasses;
 	CreateNewStrutsProjectTest.class,
 	RunStrutsProjectOnServer.class,
 	ImportStrutsProjectTest.class,
-	RenameStrutsConfigXmlFile.class,
-	RenameTldFile.class,
+	
 	
 	/** Not stable yet
 	
+	RenameStrutsConfigXmlFile.class,
+	RenameTldFile.class,
 	AddRemoveStrutsCapabilities.class,
 	TutorialTest.class,
 	
