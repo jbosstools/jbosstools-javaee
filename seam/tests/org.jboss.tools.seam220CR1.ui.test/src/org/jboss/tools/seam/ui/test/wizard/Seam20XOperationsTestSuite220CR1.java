354994349b54cd8048bcf82c65cddb88341471cf
Alexey Kazakov <akazakov@exadel.com>
2013-03-14 12:17:33 -0700

    JBIDE-13759 Reorganize Seam tests to avoid having too many tests plugins
    Removed tests for Seam 2.0.x, 2.1.x, 2.2.0.x. So we now have tests for Seam 1.2.1.EAP, FP, 2.1.2.GA, 2.2.1.GA, 2.3.0.Final only.

--- seam/tests/org.jboss.tools.seam220CR1.ui.test/src/org/jboss/tools/seam/ui/test/wizard/Seam20XOperationsTestSuite220CR1.java
+++
@@ -1,5 +0,0 @@
-package org.jboss.tools.seam.ui.test.wizard;
-
-public class Seam20XOperationsTestSuite220CR1 extends Seam20XOperationsTestSuite201GABase {
-
-}
--- seam/tests/org.jboss.tools.seam220GA.ui.test/src/org/jboss/tools/seam/ui/test/wizard/Seam20XOperationsTestSuite220GA.java
+++
@@ -1,5 +0,0 @@
-package org.jboss.tools.seam.ui.test.wizard;
-
-public class Seam20XOperationsTestSuite220GA extends Seam20XOperationsTestSuite201GABase {
-
-}
