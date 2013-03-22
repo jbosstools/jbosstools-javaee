198ddd10c716d793f3cbfca6c23fa21f0665218b
Viacheslav Kabanovich <scabanovich@exadel.com>
2013-03-22 10:41:23 -0700

    JBIDE-13837 KB Builder does complete scanning of content of every jar file
    Test is added.

---
+++ jsf/tests/org.jboss.tools.jsf.ui.test/src/org/jboss/tools/jsf/jsp/ca/test/CAForFaceletTaglibInJSFImplJarTest.java
@@ -0,0 +1,31 @@
+package org.jboss.tools.jsf.jsp.ca.test;
+
+import org.jboss.tools.jst.jsp.test.ca.ContentAssistantTestCase;
+import org.jboss.tools.test.util.TestProjectProvider;
+
+public class CAForFaceletTaglibInJSFImplJarTest extends ContentAssistantTestCase{
+	TestProjectProvider provider = null;
+	private static final String PROJECT_NAME = "testJSF2Project";
+	private static final String PAGE_NAME = "/WebContent/pages/inputname.xhtml";
+	
+	public void setUp() throws Exception {
+		provider = new TestProjectProvider("org.jboss.tools.jsf.ui.test", null, PROJECT_NAME, true); 
+		project = provider.getProject();
+	}
+
+	protected void tearDown() throws Exception {
+		if(provider != null) {
+			provider.dispose();
+		}
+	}
+	
+	public void testCAForJSFCore(){
+		String[] proposals = {
+			"f:ajax"
+		};
+
+		checkProposals(PAGE_NAME, "<f:a />", 4, proposals, false);
+
+	}
+
+}
