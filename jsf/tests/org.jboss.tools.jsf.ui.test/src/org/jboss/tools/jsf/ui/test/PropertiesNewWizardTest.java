dcb711cefbd0370a93e97fa088533d0cafa7c5c0
vrubezhny <vrubezhny@exadel.com>
2013-04-17 16:42:01 +0400

    JBIDE-14029 org.jboss.tools.jsf.ui.test.PropertiesNewWizardTest failure
    JUnit Test is fixed
    
    	modified:   src/org/jboss/tools/jsf/ui/test/PropertiesNewWizardTest.java
    	modified:   src/org/jboss/tools/jsf/ui/test/WizardTest.java

--- jsf/tests/org.jboss.tools.jsf.ui.test/src/org/jboss/tools/jsf/ui/test/PropertiesNewWizardTest.java
+++ jsf/tests/org.jboss.tools.jsf.ui.test/src/org/jboss/tools/jsf/ui/test/PropertiesNewWizardTest.java
@@ -1,8 +1,17 @@
+/*******************************************************************************
+ * Copyright (c) 2007-2013 Red Hat, Inc.
+ * Distributed under license by Red Hat, Inc. All rights reserved.
+ * This program is made available under the terms of the
+ * Eclipse Public License v1.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributor:
+ *     Red Hat, Inc. - initial API and implementation
+ ******************************************************************************/
 package org.jboss.tools.jsf.ui.test;
 
 import org.eclipse.core.resources.IResource;
 import org.jboss.tools.test.util.JobUtils;
-
 
 public class PropertiesNewWizardTest extends WizardTest {
 	public PropertiesNewWizardTest(){
@@ -22,7 +31,9 @@ public class PropertiesNewWizardTest extends WizardTest {
 	}
 	
 	public void testPropertiesFileNewWizardValidation2() {
-		validateFolderAndName();
+		// JBIDE-14029: Since testPropertiesFileNewWizardResults() test already creates the property file "aaa.properties"
+		// we cannot use the same default file name ("aaa"). The possibility to specify a different file name is added
+		validateFolderAndName("aaa2"); 
 	}
 	
 	public void testPropertiesFileNewWizardResults() {
