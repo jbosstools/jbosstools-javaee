b14287150bbf771be82efeec779c1c2bc9fe3201
vrubezhny <vrubezhny@exadel.com>
2013-10-02 00:20:35 +0400

    JBIDE-15489 Broken content assist for complex ELs
    JUnit Test test is added for the issue.

--- jsf/tests/org.jboss.tools.jsf.test/src/org/jboss/tools/jsf/kb/test/JsfKbAllTests.java
+++ jsf/tests/org.jboss.tools.jsf.test/src/org/jboss/tools/jsf/kb/test/JsfKbAllTests.java
@@ -1,5 +1,5 @@
 /******************************************************************************* 
- * Copyright (c) 2010 Red Hat, Inc. 
+ * Copyright (c) 2010-2013 Red Hat, Inc. 
  * Distributed under license by Red Hat, Inc. All rights reserved. 
  * This program is made available under the terms of the 
  * Eclipse Public License v1.0 which accompanies this distribution, 
@@ -38,6 +38,11 @@ public class JsfKbAllTests {
 				new String[]{"projects/utility", "projects/webapp"},
 				new String[]{"utility", "webapp"});
 		suiteAll.addTest(testSetup);
+		suiteAll.addTest(new ProjectImportTestSetup(new TestSuite(
+				PageProcesorInELDetectionTest.class), "org.jboss.tools.jsf.base.test", //$NON-NLS-1$
+				new String[] { "projects/jsf2pr" }, //$NON-NLS-1$
+				new String[] { "jsf2pr" })); //$NON-NLS-1$
+
 		return suiteAll;
 	}
 }
