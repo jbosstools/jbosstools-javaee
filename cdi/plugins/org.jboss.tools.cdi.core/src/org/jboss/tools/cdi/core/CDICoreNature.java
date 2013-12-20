391a9b7794371b1d275c243cbeab4ec2134644ca
Viacheslav Kabanovich <scabanovich@exadel.com>
2013-12-20 08:39:05 -0800

    JBIDE-15373 Check all code run by builders for thread safety
    Mistake in the previous commit is fixed.

--- cdi/plugins/org.jboss.tools.cdi.core/src/org/jboss/tools/cdi/core/CDICoreNature.java
+++ cdi/plugins/org.jboss.tools.cdi.core/src/org/jboss/tools/cdi/core/CDICoreNature.java
@@ -275,7 +275,7 @@ public class CDICoreNature implements IProjectNature {
 		synchronized(this) {
 			array = usedBy.toArray(new CDICoreNature[0]);
 		}
-		for (CDICoreNature n: usedBy) {
+		for (CDICoreNature n: array) {
 			if(!result.containsKey(n) || result.get(n).intValue() < level) {
 				result.put(n, level);
 				n.getAllDependentProjects(result, level + 1);
