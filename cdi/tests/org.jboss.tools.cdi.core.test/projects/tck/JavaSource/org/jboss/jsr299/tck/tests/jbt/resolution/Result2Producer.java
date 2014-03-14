31f9f7a3e70d382d743511a3a7ec7d751d2200b1
Viacheslav Kabanovich <scabanovich@exadel.com>
2014-03-14 12:28:25 -0700

    JBIDE-16759 Provide assignability of raw bean type to parameterized required type
    Tests are added.

---
+++ cdi/tests/org.jboss.tools.cdi.core.test/projects/tck/JavaSource/org/jboss/jsr299/tck/tests/jbt/resolution/Result2Producer.java
@@ -0,0 +1,11 @@
+package org.jboss.jsr299.tck.tests.jbt.resolution;
+
+import javax.enterprise.inject.Produces;
+
+public class Result2Producer {
+
+	@Produces
+	public Result2 createResult2() {
+		return null;
+	}
+}
