a0e3e241c769abc793e000bbfa95dc9b77e6ee68
Alexey Kazakov <akazakov@exadel.com>
2014-04-24 16:56:08 -0700

    JBIDE-17182 org.jboss.tools.jsf.test tests failure on Windows

--- jsf/tests/org.jboss.tools.jsf.test/src/org/jboss/tools/jsf/test/refactoring/ELVariableRefactoringTest.java
+++ jsf/tests/org.jboss.tools.jsf.test/src/org/jboss/tools/jsf/test/refactoring/ELVariableRefactoringTest.java
@@ -16,7 +16,6 @@ import org.eclipse.core.resources.IFile;
 import org.eclipse.core.resources.IProject;
 import org.eclipse.core.runtime.CoreException;
 import org.jboss.tools.common.base.test.AbstractRefactorTest;
-import org.jboss.tools.common.util.FileUtil;
 import org.jboss.tools.jsf.el.refactoring.RenameELVariableProcessor;
 import org.jboss.tools.test.util.ProjectImportTestSetup;
 
@@ -26,6 +25,7 @@ public class ELVariableRefactoringTest extends AbstractRefactorTest {
 
 	private static final String NEW_NAME = "cust";
 	private static final int NAME_LEN = 4;
+	private static final String OLD_NAME = "user";
 	
 	public ELVariableRefactoringTest(){
 		super("EL Variable Refactoring Test");
@@ -41,49 +41,41 @@ public class ELVariableRefactoringTest extends AbstractRefactorTest {
 		
 		IFile sourceFile = project.getProject().getFile("/WebContent/pages/hello.jsp");
 		
-		String sourceFileContent = FileUtil.getContentFromEditorOrFile(sourceFile);
-		
-		int position = sourceFileContent.indexOf("user.name");
-		
 		TestChangeStructure structure = new TestChangeStructure(project.getProject(), "/WebContent/pages/hello.jsp");
-		TestTextChange change = new TestTextChange(position, NAME_LEN, NEW_NAME);
+		TestTextChange change = new TestTextChange("user.name", NAME_LEN, NEW_NAME);
 		structure.addTextChange(change);
 		
-		position = sourceFileContent.indexOf("user.name", position+1);
-		
-		change = new TestTextChange(position, NAME_LEN, NEW_NAME);
+		change = new TestTextChange("user.name", NAME_LEN, NEW_NAME);
 		structure.addTextChange(change);
 		list.add(structure);
 
 		structure = new TestChangeStructure(project, "/WebContent/WEB-INF/faces-config.xml");
-		change = new TestTextChange(1875, NAME_LEN, NEW_NAME);
+		change = new TestTextChange(OLD_NAME, NAME_LEN, NEW_NAME);
 		structure.addTextChange(change);
 		list.add(structure);
 
 		structure = new TestChangeStructure(project, "/WebContent/pages/inputUserName.jsp");
-		change = new TestTextChange(494, NAME_LEN, NEW_NAME);
+		change = new TestTextChange(OLD_NAME, NAME_LEN, NEW_NAME);
 		structure.addTextChange(change);
 		list.add(structure);
 		
 		structure = new TestChangeStructure(project, "/WebContent/pages/el.jsp");
-		change = new TestTextChange(83, NAME_LEN, NEW_NAME);
+		change = new TestTextChange(OLD_NAME, NAME_LEN, NEW_NAME);
 		structure.addTextChange(change);
 		list.add(structure);
 
 		structure = new TestChangeStructure(project, "/WebContent/testElRevalidation.xhtml");
-		change = new TestTextChange(601, NAME_LEN, NEW_NAME);
+		change = new TestTextChange(OLD_NAME, NAME_LEN, NEW_NAME);
 		structure.addTextChange(change);
 		list.add(structure);
 
 		sourceFile = project.getProject().getFile("/WebContent/pages/syntaxErrors.xhtml");
-		sourceFileContent = FileUtil.getContentFromEditorOrFile(sourceFile);
-		position = sourceFileContent.indexOf("user");
 		structure = new TestChangeStructure(project, "/WebContent/pages/syntaxErrors.xhtml");
-		change = new TestTextChange(position, NAME_LEN, NEW_NAME);
+		change = new TestTextChange(OLD_NAME, NAME_LEN, NEW_NAME);
 		structure.addTextChange(change);
 		list.add(structure);
 
-		RenameELVariableProcessor processor = new RenameELVariableProcessor(sourceFile, "user");
+		RenameELVariableProcessor processor = new RenameELVariableProcessor(sourceFile, OLD_NAME);
 		processor.setNewName(NEW_NAME);
 
 		checkRename(processor, list);
