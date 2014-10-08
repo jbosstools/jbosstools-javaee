c7a07c9a31955213c2bcdfedd022abdbfafa677f
Viacheslav Kabanovich <scabanovich@exadel.com>
2014-10-08 16:58:09 -0700

    JBIDE-18492 Incorrect Qualifier Target validation

--- cdi/tests/org.jboss.tools.cdi.core.test/projects/tck1.1/JavaSource/org/jboss/jsr299/tck/tests/jbt/validation/annotations/qualifier/HairyTargetOk.java
+++ cdi/tests/org.jboss.tools.cdi.core.test/projects/tck1.1/JavaSource/org/jboss/jsr299/tck/tests/jbt/validation/annotations/qualifier/HairyTargetOk.java
@@ -1,4 +1,4 @@
-package org.jboss.jsr299.tck.tests.jbt.validation.annotations.qualifier.broken;
+package org.jboss.jsr299.tck.tests.jbt.validation.annotations.qualifier;
 
 import static java.lang.annotation.ElementType.FIELD;
 import static java.lang.annotation.ElementType.PARAMETER;
